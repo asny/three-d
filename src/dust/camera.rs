582bf2cbc6c0c126f64f3d256a0adcecdd9a1dca
Asger <asger.nyman@alexandra.dk>
2018-04-13 14:29:04 +0200

    More camera functionality

--- src/dust/camera.rs
+++ src/dust/camera.rs
@@ -1,6 +1,6 @@
 use gl;
 use glm;
-use glm::ext::look_at;
+use glm::ext::*;
 
 #[derive(Debug)]
 pub enum Error {
@@ -35,8 +35,13 @@ impl Camera
         self.direction = direction;
     }
 
-    fn get_view(&self)
+    fn get_view(&self) -> glm::Matrix4<f32>
     {
-        glm::ext::look_at(self.position, self.position + self.direction, glm::vec3(0., 1., 0.));
+        glm::ext::look_at(self.position, self.position + self.direction, glm::vec3(0., 1., 0.))
+    }
+
+    fn get_projection(&self) -> glm::Matrix4<f32>
+    {
+        glm::ext::perspective(glm::radians(45.0), (self.width as f32)/(self.height as f32), self.z_near, self.z_far)
     }
 }
