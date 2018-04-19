d23de0ae5a0bbb9428c41f44d7a99f6096dd224d
Asger <asger.nyman@alexandra.dk>
2018-04-19 11:23:44 +0200

    Add custom attributes to mesh

--- src/dust/mesh.rs
+++ src/dust/mesh.rs
@@ -1,11 +1,15 @@
 use std::rc::Rc;
+use std::collections::HashMap;
+use std::string::String;
 
 #[derive(Debug)]
 pub enum Error {
+    FailedToFindCustomAttribute {message: String}
 }
 
 pub struct Mesh {
-    positions: Vec<f32>
+    positions: Vec<f32>,
+    attributes: HashMap<String, Vec<f32>>
 }
 
 
@@ -13,11 +17,21 @@ impl Mesh
 {
     pub fn create(positions: Vec<f32>) -> Result<Rc<Mesh>, Error>
     {
-        Ok(Rc::new(Mesh { positions }))
+        Ok(Rc::new(Mesh { positions, attributes: HashMap::new() }))
     }
 
     pub fn positions(&self) -> &Vec<f32>
     {
         &self.positions
+    }
+
+    pub fn add_custom_attribute(&mut self, name: &str, attribute: Vec<f32>)
+    {
+        self.attributes.insert(String::from(name), attribute);
+    }
+
+    pub fn get_custom_attribute(&self, name: &str) -> Result<&Vec<f32>, Error>
+    {
+        self.attributes.get(name).ok_or(Error::FailedToFindCustomAttribute {message: format!("Could not find attribute {}", name)})
     }
 }
