74cbce5774f43ad3fef777b9d3b252baf3f950a9
Asger <asger.nyman@alexandra.dk>
2018-04-23 14:21:36 +0200

    Made stuff private if possible

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,21 @@
 pub extern crate gl;
 pub extern crate gust;
 pub use gust::glm;
-pub use gust::*;
+pub use gust::mesh;
 
-pub mod loader;
-pub mod model;
+mod loader;
+mod model;
+mod buffer;
+mod shader;
+mod utility;
+
 pub mod material;
 pub mod program;
-pub mod shader;
-pub mod utility;
-pub mod camera;
-pub mod scene;
 pub mod input;
-pub mod buffer;
 pub mod state;
 
+pub mod camera;
+pub mod scene;
+
+#[cfg(target_os = "emscripten")]
 pub mod emscripten;
