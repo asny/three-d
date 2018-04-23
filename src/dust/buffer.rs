67d05cd3d2d15f3d62f0a1e7239d75331565212b
Asger <asger.nyman@alexandra.dk>
2018-04-23 11:06:58 +0200

    Use a bind function which only binds another buffer if necessary

--- src/dust/buffer.rs
+++ src/dust/buffer.rs
@@ -20,19 +20,33 @@ impl VertexBuffer
         unsafe {
             gl.GenBuffers(1, &mut id);
         }
-        Ok(VertexBuffer{gl: gl.clone(), id: id})
+        let buffer = VertexBuffer{gl: gl.clone(), id };
+        buffer.bind();
+        Ok(buffer)
     }
 
     pub fn fill_with(&self, data: &Vec<f32>)
     {
+        self.bind();
         unsafe {
-            self.gl.BindBuffer(gl::ARRAY_BUFFER, self.id);
             self.gl.BufferData(
                 gl::ARRAY_BUFFER, // target
                 (data.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr, // size of data in bytes
                 data.as_ptr() as *const gl::types::GLvoid, // pointer to data
                 gl::STATIC_DRAW, // usage
             );
+        }
+    }
+
+    fn bind(&self)
+    {
+        unsafe {
+            static mut CURRENTLY_USED: gl::types::GLuint = 1000000 as u32;
+            if self.id != CURRENTLY_USED
+            {
+                self.gl.BindBuffer(gl::ARRAY_BUFFER, self.id);
+                CURRENTLY_USED = self.id;
+            }
         }
     }
 }
