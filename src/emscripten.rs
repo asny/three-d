7429171b92260de9deb87b5c61abf4c451443294
Asger <asger.nyman@alexandra.dk>
2018-04-26 15:22:36 +0200

    Something is working

--- src/emscripten.rs
+++ src/emscripten.rs
@@ -4,7 +4,7 @@ pub mod emscripten {
     use std::cell::RefCell;
     use std::ptr::null_mut;
     use std::os::raw::c_void;
-    use emscripten_sys::{emscripten_set_main_loop};
+    use emscripten_sys::{emscripten_set_main_loop, emscripten_async_wget, emscripten_async_wget_data};
 
     thread_local!(static MAIN_LOOP_CALLBACK: RefCell<*mut c_void> = RefCell::new(null_mut()));
 
@@ -22,4 +22,53 @@ pub mod emscripten {
             });
         }
     }
+
+    thread_local!(static ON_LOAD_CALLBACK: RefCell<*mut c_void> = RefCell::new(null_mut()));
+
+    pub fn async_wget_data<F, E>(name: &str, on_load: F, on_error: E) where F: FnMut(String), E: FnOnce(String)
+    {
+        ON_LOAD_CALLBACK.with(|log| {
+            *log.borrow_mut() = &on_load as *const _ as *mut c_void;
+        });
+
+        unsafe {
+            let out = 0 as *const ::std::os::raw::c_char;
+            emscripten_async_wget(name.as_ptr() as *const ::std::os::raw::c_char,
+                                       out,
+                                       Some(str_callback_wrapper::<F>),
+                                       Some(str_callback_wrapper::<F>));
+        }
+
+        unsafe extern "C" fn str_callback_wrapper<F>(arg1: *const ::std::os::raw::c_char) where F: FnMut(String)
+        {
+            ON_LOAD_CALLBACK.with(|z| {
+                let closure = *z.borrow_mut() as *mut F;
+                (*closure)(String::from("Hej"));
+            });
+        }
+    }
+
+    /*thread_local!(static ON_LOAD_CALLBACK: RefCell<*mut c_void> = RefCell::new(null_mut()));
+
+    pub fn async_wget_data<F>(name: &str, on_load: F, on_error: F) where F: FnOnce(String)
+    {
+        ON_LOAD_CALLBACK.with(|log| {
+            *log.borrow_mut() = &on_load as *const _ as *mut c_void;
+        });
+
+        unsafe {
+            emscripten_async_wget_data(name.as_ptr() as *const ::std::os::raw::c_char,
+                                       0 as *mut ::std::os::raw::c_void,
+                                       Some(on_load_wrapper::<F>),
+                                       on_error);
+        }
+
+        unsafe extern "C" fn on_load_wrapper<F>(arg1: *mut ::std::os::raw::c_void, arg2: *mut ::std::os::raw::c_void, arg3: ::std::os::raw::c_int) where F: FnOnce(String)
+        {
+            ON_LOAD_CALLBACK.with(|z| {
+                let closure = *z.borrow_mut() as *mut F;
+                (*closure)(String::from("Hej"));
+            });
+        }
+    }*/
 }
--- src/renderer.rs
+++ src/renderer.rs
@@ -14,8 +14,19 @@ pub fn set_main_loop<F>(mut main_loop: F) where F: FnMut()
     loop { main_loop(); }
 }
 
+#[cfg(target_os = "emscripten")]
+pub fn load<F>(name: &str, on_load: F) where F: FnMut(String)
+{
+    let on_error = |data: String| {
+
+        println!("Error {}", data);
+
+    };
+    emscripten::async_wget_data(name, on_load, on_error);
+}
+
 #[cfg(not(target_os = "emscripten"))]
-pub fn load<F>(name: &str, mut on_load: F) where F: FnOnce(String)
+pub fn load<F>(name: &str, mut on_load: F) where F: FnMut(String)
 {
     use loader;
     let data = loader::load_string(name).unwrap();
