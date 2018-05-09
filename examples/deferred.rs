6988b032122ba50685ad48b1d3d66d91dc814f8d
Asger <asger.nyman@alexandra.dk>
2018-05-09 10:19:31 +0200

    Added deferred example

---
+++ examples/deferred.rs
@@ -0,0 +1,83 @@
+extern crate sdl2;
+extern crate dust;
+
+mod materials;
+
+use std::process;
+
+use sdl2::event::{Event};
+use sdl2::keyboard::Keycode;
+
+use dust::*;
+
+fn main() {
+    let ctx = sdl2::init().unwrap();
+    let video_ctx = ctx.video().unwrap();
+
+    #[cfg(target_os = "macos")] // Use OpenGL 4.1 since that is the newest version supported on macOS
+    {
+        let gl_attr = video_ctx.gl_attr();
+        gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+        gl_attr.set_context_version(4, 1);
+    }
+
+    let width: usize = 900;
+    let height: usize = 700;
+    let window = video_ctx
+        .window("Dust", width as u32, height as u32)
+        .opengl()
+        .position_centered()
+        .resizable()
+        .build()
+        .unwrap();
+
+    let _gl_context = window.gl_create_context().unwrap();
+    let gl = gl::Gl::load_with(|s| video_ctx.gl_get_proc_address(s) as *const std::os::raw::c_void);
+
+    // Scene
+    let mut scene = scene::Scene::create().unwrap();
+
+    // Camera
+    let mut camera = camera::Camera::create(&gl, glm::vec3(5.0, 5.0, 5.0), glm::vec3(0.0, 0.0, 0.0), width, height).unwrap();
+
+    let mut mesh = gust::loader::load_obj("/examples/assets/models/box.obj").unwrap();
+    let material = materials::texture_material::TextureMaterial::create(&gl).unwrap();
+    scene.add_model(&gl, mesh, material).unwrap();
+
+    unsafe {
+        gl.ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
+
+    let rendertarget = rendertarget::Rendertarget::create(&gl, width, height);
+
+    // set up event handling
+    let mut events = ctx.event_pump().unwrap();
+
+    // main loop
+    let main_loop = || {
+        for event in events.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown {keycode: Some(Keycode::Escape), ..} => {
+                    process::exit(1);
+                },
+                Event::MouseMotion {xrel, yrel, mousestate, .. } => {
+                    if mousestate.left()
+                    {
+                        eventhandler::rotate(&mut camera, xrel, yrel);
+                    }
+                },
+                Event::MouseWheel {y, .. } => {
+                    eventhandler::zoom(&mut camera, y);
+                },
+                _ => {}
+            }
+        }
+
+        // draw
+        camera.draw(&scene).unwrap();
+
+        window.gl_swap_window();
+    };
+
+    renderer::set_main_loop(main_loop);
+}
--- src/camera.rs
+++ src/camera.rs
@@ -20,21 +20,21 @@ pub struct Camera {
     target: glm::Vec3,
     z_near: f32,
     z_far: f32,
-    width: u32,
-    height: u32,
+    width: usize,
+    height: usize,
 }
 
 
 impl Camera
 {
-    pub fn create(gl: &gl::Gl, position: glm::Vec3, target: glm::Vec3, width: u32, height: u32) -> Result<Camera, Error>
+    pub fn create(gl: &gl::Gl, position: glm::Vec3, target: glm::Vec3, width: usize, height: usize) -> Result<Camera, Error>
     {
         let mut camera = Camera { gl: gl.clone(), position, target, z_near: 0.1, z_far: 1000.0, width: width, height: height };
         camera.set_screen_size(width, height);
         Ok(camera)
     }
 
-    pub fn set_screen_size(&mut self, width: u32, height: u32)
+    pub fn set_screen_size(&mut self, width: usize, height: usize)
     {
         unsafe {
             self.gl.Viewport(0, 0, width as i32, height as i32);
