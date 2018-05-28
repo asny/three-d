db1668456e854496c9a7a03678b21aabb5197bdc
Asger Nyman Christiansen <asgernyman@gmail.com>
2018-05-28 16:08:35 +0200

    Changed texture settings for color target texture

--- src/core/texture.rs
+++ src/core/texture.rs
@@ -46,10 +46,10 @@ impl Texture2D
 
         bind(&texture.gl, texture.id, texture.target);
         unsafe {
-            gl.TexParameteri(texture.target, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
-            gl.TexParameteri(texture.target, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
-            gl.TexParameteri(texture.target, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
-            gl.TexParameteri(texture.target, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+            gl.TexParameteri(texture.target, gl::TEXTURE_MIN_FILTER, gl::NEAREST as i32);
+            gl.TexParameteri(texture.target, gl::TEXTURE_MAG_FILTER, gl::NEAREST as i32);
+            gl.TexParameteri(texture.target, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32);
+            gl.TexParameteri(texture.target, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32);
 
             gl.TexImage2D(texture.target,
                              0,
