57db75334707fe7814f861da4a0b58506b1757af
Asger Nyman Christiansen <asgernyman@gmail.com>
2018-06-14 10:03:32 +0200

    Added uv coordinates to cube

--- lib/Gust/src/models.rs
+++ lib/Gust/src/models.rs
@@ -55,7 +55,7 @@ pub fn create_cube() -> Result<mesh::Mesh, Error>
         glm::vec3(-1.0, 1.0, 1.0),
         glm::vec3(-1.0, -1.0, 1.0),
         glm::vec3(-1.0, 1.0, 1.0),
-        glm::vec3(-1.0, -1.0, -1.0),
+        glm::vec3(-1.0, -1.0, -1.0)
     ];
     let normals: Vec<glm::Vec3> = vec![
         glm::vec3(0.0, 1.0, 0.0),
@@ -100,5 +100,52 @@ pub fn create_cube() -> Result<mesh::Mesh, Error>
         glm::vec3(-1.0, 0.0, 0.0),
         glm::vec3(-1.0, 0.0, 0.0)
     ];
-    Ok(mesh::Mesh::create_with_normals(positions, normals)?)
+
+    let uvs: Vec<glm::Vec2> = vec![
+        glm::vec2(1.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(1.0, 1.0),
+        glm::vec2(0.0, 1.0),
+        glm::vec2(1.0, 1.0),
+        glm::vec2(0.0, 0.0),
+
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0),
+        glm::vec2(0.0, 0.0)
+    ];
+
+    let mut mesh = mesh::Mesh::create_with_normals(positions, normals)?;
+    mesh.add_custom_vec2_attribute("uv_coordinate", uvs)?;
+    Ok(mesh)
 }
