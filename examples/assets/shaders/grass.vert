39458caf59bfab302ca555edfde38da40476e93c
Asger Nyman Christiansen <asgernyman@gmail.com>
2018-08-21 19:52:11 +0200

    Enabled instanced arrays

--- examples/assets/shaders/grass.vert
+++ examples/assets/shaders/grass.vert
@@ -2,8 +2,9 @@ uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
 
 in vec3 position;
+in vec3 offset;
 
 void main()
 {
-  gl_Position = projectionMatrix * viewMatrix * vec4(position, 1.0);
+  gl_Position = projectionMatrix * viewMatrix * vec4(position + offset, 1.0);
 }
--- examples/scene_objects/grass.rs
+++ examples/scene_objects/grass.rs
@@ -4,9 +4,7 @@ use glm::*;
 use gust;
 
 use dust::{traits, camera};
-use dust::core::{program, surface, state};
-
-use num_traits::identities::One;
+use dust::core::{buffer, program, surface, state};
 
 pub struct Grass {
     program: program::Program,
@@ -22,13 +20,19 @@ impl Grass
         let mut model = surface::TriangleSurface::create_without_adding_attributes(gl, &mesh)?;
         model.add_attributes(&[&mesh.positions].to_vec(), &program)?;
 
+        let offsets = [0.0,0.0,0.0, 10.0,0.0,10.0];
+
+        let mut buffer = buffer::VertexBuffer::create(gl).unwrap();
+        buffer.fill_with(&offsets.to_vec());
+
+        program.set_used();
+        program.setup_attribute("offset", 3, 3, 0, 1)?;
+
         Ok(Grass { program, model })
     }
 
     pub fn render(&self, camera: &camera::Camera) -> Result<(), traits::Error>
     {
-        let transformation = Matrix4::one();
-
         self.program.cull(state::CullType::NONE);
 
         self.program.add_uniform_mat4("viewMatrix", &camera.get_view())?;
