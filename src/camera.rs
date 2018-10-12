c654ac74cc0804870741b99682b177f52b6d2631
Asger <asger.nyman@alexandra.dk>
2018-10-12 11:58:50 +0200

    set_target_screen_size

--- src/camera.rs
+++ src/camera.rs
@@ -7,6 +7,7 @@ pub trait Camera
     fn position(&self) -> &Vec3;
     fn target(&self) -> &Vec3;
     fn set_view(&mut self, position: Vec3, target: Vec3);
+    fn set_target_screen_size(&mut self, width: usize, height: usize);
 }
 
 struct BaseCamera {
@@ -18,8 +19,7 @@ struct BaseCamera {
 
 pub struct PerspectiveCamera {
     base: BaseCamera,
-    width: usize,
-    height: usize
+    aspect: f32
 }
 
 
@@ -27,7 +27,7 @@ impl PerspectiveCamera
 {
     pub fn new(position: Vec3, target: Vec3, width: usize, height: usize) -> PerspectiveCamera
     {
-        PerspectiveCamera { base: BaseCamera { position, target, z_near: 0.1, z_far: 1000.0 }, width, height }
+        PerspectiveCamera { base: BaseCamera { position, target, z_near: 0.1, z_far: 1000.0 }, aspect: (width as f32)/(height as f32) }
     }
 }
 
@@ -40,7 +40,7 @@ impl Camera for PerspectiveCamera
 
     fn get_projection(&self) -> Mat4
     {
-        Mat4::new_perspective((self.width as f32)/(self.height as f32), 0.25 * ::std::f32::consts::PI, self.base.z_near, self.base.z_far)
+        Mat4::new_perspective(self.aspect, 0.25 * ::std::f32::consts::PI, self.base.z_near, self.base.z_far)
     }
 
     fn position(&self) -> &Vec3
@@ -58,18 +58,24 @@ impl Camera for PerspectiveCamera
         self.base.position = position;
         self.base.target = target;
     }
+
+    fn set_target_screen_size(&mut self, width: usize, height: usize)
+    {
+        self.aspect = (width as f32)/(height as f32);
+    }
 }
 
 pub struct ShadowCamera {
     base: BaseCamera,
-    radius: usize
+    half_width: usize,
+    half_height: usize
 }
 
 impl ShadowCamera
 {
     pub fn new(position: Vec3, target: Vec3, radius: usize) -> ShadowCamera
     {
-        ShadowCamera { base: BaseCamera { position, target, z_near: 0.1, z_far: 1000.0 }, radius }
+        ShadowCamera { base: BaseCamera { position, target, z_near: 0.1, z_far: 1000.0 }, half_width: radius, half_height: radius }
     }
 }
 
@@ -83,7 +89,7 @@ impl Camera for ShadowCamera
 
     fn get_projection(&self) -> Mat4
     {
-        Mat4::new_orthographic(-(self.radius as f32), self.radius as f32, -(self.radius as f32), self.radius as f32, self.base.z_near, self.base.z_far)
+        Mat4::new_orthographic(-(self.half_width as f32), self.half_width as f32, -(self.half_height as f32), self.half_height as f32, self.base.z_near, self.base.z_far)
     }
 
     fn position(&self) -> &Vec3
@@ -100,5 +106,11 @@ impl Camera for ShadowCamera
     {
         self.base.position = position;
         self.base.target = target;
+    }
+
+    fn set_target_screen_size(&mut self, width: usize, height: usize)
+    {
+        self.half_width = width/2;
+        self.half_height = height/2;
     }
 }
