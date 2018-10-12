7a06783bd460c7a89120db1249842ad48783aca7
Asger <asger.nyman@alexandra.dk>
2018-10-12 11:26:41 +0200

    Added ShadowCamera

--- src/camera.rs
+++ src/camera.rs
@@ -11,11 +11,15 @@ pub trait Camera
     fn set_view(&mut self, position: Vec3, target: Vec3);
 }
 
-pub struct PerspectiveCamera {
-    position: Vec3,
-    target: Vec3,
+struct BaseCamera {
+    pub position: Vec3,
+    pub target: Vec3,
     z_near: f32,
-    z_far: f32,
+    z_far: f32
+}
+
+pub struct PerspectiveCamera {
+    base: BaseCamera,
     width: usize,
     height: usize
 }
@@ -25,7 +29,7 @@ impl PerspectiveCamera
 {
     pub fn new(position: Vec3, target: Vec3, width: usize, height: usize) -> PerspectiveCamera
     {
-        PerspectiveCamera { position, target, z_near: 0.1, z_far: 1000.0, width, height }
+        PerspectiveCamera { base: BaseCamera { position, target, z_near: 0.1, z_far: 1000.0 }, width, height }
     }
 }
 
@@ -33,22 +37,22 @@ impl Camera for PerspectiveCamera
 {
     fn get_view(&self) -> Mat4
     {
-        Mat4::look_at_rh(&na::Point::from_coordinates(self.position), &na::Point::from_coordinates(self.target), &vec3(0., 1., 0.))
+        Mat4::look_at_rh(&na::Point::from_coordinates(self.base.position), &na::Point::from_coordinates(self.base.target), &vec3(0., 1., 0.))
     }
 
     fn get_projection(&self) -> Mat4
     {
-        Mat4::new_perspective((self.width as f32)/(self.height as f32), 0.25 * ::std::f32::consts::PI, self.z_near, self.z_far)
+        Mat4::new_perspective((self.width as f32)/(self.height as f32), 0.25 * ::std::f32::consts::PI, self.base.z_near, self.base.z_far)
     }
 
     fn position(&self) -> &Vec3
     {
-        &self.position
+        &self.base.position
     }
 
     fn target(&self) -> &Vec3
     {
-        &self.target
+        &self.base.target
     }
 
     fn screen_width(&self) -> usize
@@ -63,7 +67,60 @@ impl Camera for PerspectiveCamera
 
     fn set_view(&mut self, position: Vec3, target: Vec3)
     {
-        self.position = position;
-        self.target = target;
+        self.base.position = position;
+        self.base.target = target;
+    }
+}
+
+pub struct ShadowCamera {
+    base: BaseCamera,
+    radius: usize
+}
+
+impl ShadowCamera
+{
+    pub fn new(position: Vec3, target: Vec3, radius: usize) -> ShadowCamera
+    {
+        ShadowCamera { base: BaseCamera { position, target, z_near: 0.1, z_far: 1000.0 }, radius }
+    }
+}
+
+impl Camera for ShadowCamera
+{
+    fn get_view(&self) -> Mat4
+    {
+        let up = (vec3(1.0, 0.0, 0.0).cross(&(self.base.target - self.base.position))).normalize();
+        Mat4::look_at_rh(&na::Point::from_coordinates(self.base.position), &na::Point::from_coordinates(self.base.target), &up)
+    }
+
+    fn get_projection(&self) -> Mat4
+    {
+        Mat4::new_orthographic(-(self.radius as f32), self.radius as f32, -(self.radius as f32), self.radius as f32, self.base.z_near, self.base.z_far)
+    }
+
+    fn position(&self) -> &Vec3
+    {
+        &self.base.position
+    }
+
+    fn target(&self) -> &Vec3
+    {
+        &self.base.target
+    }
+
+    fn screen_width(&self) -> usize
+    {
+        2 * self.radius
+    }
+
+    fn screen_height(&self) -> usize
+    {
+        2 * self.radius
+    }
+
+    fn set_view(&mut self, position: Vec3, target: Vec3)
+    {
+        self.base.position = position;
+        self.base.target = target;
     }
 }
