7b418f83ef196c1c78441af3b112bd68fa8d0b0a
Asger <asger.nyman@alexandra.dk>
2019-01-18 15:53:18 +0100

    Added window handler to examples to avoid duplicate code in the examples

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,29 +1,15 @@
 
 mod scene_objects;
-
-use std::process;
+mod window_handler;
 
 use dust::*;
-use glutin::dpi::*;
-use glutin::GlContext;
 
 fn main() {
 
-    let width: usize = 900;
-    let height: usize = 700;
-    let mut events_loop = glutin::EventsLoop::new();
-    let window = glutin::WindowBuilder::new()
-        .with_title("Hello, world!")
-        .with_dimensions(LogicalSize::new(width as f64, height as f64));
-    let context = glutin::ContextBuilder::new()
-        .with_vsync(true);
-    let gl_window = glutin::GlWindow::new(window, context, &events_loop).unwrap();
+    let mut window_handler = window_handler::WindowHandler::new_default("Hello, world!");
+    let (width, height) = window_handler.size();
 
-    unsafe {
-        gl_window.make_current().unwrap();
-    }
-
-    let gl = gl::Gl::load_with(|s| gl_window.get_proc_address(s) as *const std::os::raw::c_void);
+    let gl = gl::Gl::load_with(|s| window_handler.get_proc_address(s));
 
     // Screen
     let screen = screen::Screen {width, height};
@@ -38,28 +24,14 @@ fn main() {
     let model = scene_objects::triangle::Triangle::create(&gl);
 
     // main loop
-    let main_loop = || {
-        events_loop.poll_events(|event| {
-            match event {
-                glutin::Event::WindowEvent{ event, .. } => match event {
-                    glutin::WindowEvent::CloseRequested => process::exit(1),
-                    glutin::WindowEvent::Resized(logical_size) => {
-                        let dpi_factor = gl_window.get_hidpi_factor();
-                        gl_window.resize(logical_size.to_physical(dpi_factor));
-                    },
-                    _ => ()
-                },
-                _ => ()
-            }
-        });
+    loop {
+        window_handler.handle_events();
 
         // draw
         renderer.render_pass_begin();
 
         model.render(&camera);
 
-        gl_window.swap_buffers().unwrap();
+        window_handler.swap_buffers();
     };
-
-    renderer::set_main_loop(main_loop);
 }
---
+++ examples/window_handler.rs
@@ -0,0 +1,69 @@
+
+use glutin::*;
+
+pub struct WindowHandler
+{
+    gl_window: GlWindow,
+    events_loop: EventsLoop
+}
+
+impl WindowHandler
+{
+    pub fn new_default(title: &str) -> WindowHandler
+    {
+        let width: usize = 900;
+        let height: usize = 700;
+        let window = glutin::WindowBuilder::new()
+            .with_title(title)
+            .with_dimensions(glutin::dpi::LogicalSize::new(width as f64, height as f64));
+
+        WindowHandler::new(window)
+    }
+
+    pub fn new(window: WindowBuilder) -> WindowHandler
+    {
+        let events_loop = EventsLoop::new();
+
+        let context = ContextBuilder::new().with_vsync(true);
+
+        let gl_window = glutin::GlWindow::new(window, context, &events_loop).unwrap();
+
+        unsafe {
+            gl_window.make_current().unwrap();
+        }
+        WindowHandler {gl_window, events_loop}
+    }
+
+    pub fn size(&self) -> (usize, usize)
+    {
+        let size: (u32, u32) = self.gl_window.get_inner_size().unwrap().to_physical(self.gl_window.get_hidpi_factor()).into();
+        (size.0 as usize, size.1 as usize)
+    }
+
+    pub fn get_proc_address(&self, address: &str) -> *const std::os::raw::c_void
+    {
+        self.gl_window.get_proc_address(address) as *const std::os::raw::c_void
+    }
+
+    pub fn handle_events(&mut self)
+    {
+        self.events_loop.poll_events(|event| {
+            match event {
+                glutin::Event::WindowEvent{ event, .. } => match event {
+                    glutin::WindowEvent::CloseRequested => std::process::exit(1),
+                    glutin::WindowEvent::Resized(logical_size) => {
+                        //let dpi_factor = self.gl_window.get_hidpi_factor();
+                        //self.gl_window.resize(logical_size.to_physical(dpi_factor));
+                    },
+                    _ => ()
+                },
+                _ => ()
+            }
+        });
+    }
+
+    pub fn swap_buffers(&self)
+    {
+        self.gl_window.swap_buffers().unwrap();
+    }
+}
