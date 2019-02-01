453223e1ba814f24e2febdce66bc62000b14864d
Asger Nyman Christiansen <asgernyman@gmail.com>
2019-02-01 20:04:04 +0100

    Added www default index.html, index.js and webpack config

---
+++ examples/web/www/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        }),
+    ],
+    mode: 'development'
+};
