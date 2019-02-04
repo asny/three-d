1b7371318b65ebba8f8df95264f041e9179f8044
Asger <asger.nyman@alexandra.dk>
2019-02-04 15:56:47 +0100

    add_key_down_event_listener

--- lib/window/src/canvas.rs
+++ lib/window/src/canvas.rs
@@ -41,6 +41,7 @@ impl Window
         self.add_mouseup_event_listener(events.clone());
         self.add_mousemove_event_listener(events.clone());
         self.add_mousewheel_event_listener(events.clone());
+        self.add_key_down_event_listener(events.clone());
 
         *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
             callback(&(*events).borrow());
@@ -101,6 +102,18 @@ impl Window
             (*events).borrow_mut().push(Event::MouseWheel {delta: 0.02499999912 * event.delta_y() as f64});
         }) as Box<dyn FnMut(_)>);
         self.canvas.add_event_listener_with_callback("wheel", closure.as_ref().unchecked_ref()).unwrap();
+        closure.forget();
+    }
+
+    fn add_key_down_event_listener(&self, events: Rc<RefCell<Vec<Event>>>)
+    {
+        let closure = Closure::wrap(Box::new(move |event: web_sys::KeyboardEvent| {
+            if !event.default_prevented() {
+                (*events).borrow_mut().push(Event::Key {state: State::Pressed, kind: event.code()});
+                event.prevent_default();
+            }
+        }) as Box<dyn FnMut(_)>);
+        window().add_event_listener_with_callback("keydown", closure.as_ref().unchecked_ref()).unwrap();
         closure.forget();
     }
 
