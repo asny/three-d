9bca0c991f0faf49ea19e878013b28577af8da38
Asger <asger.nyman@alexandra.dk>
2019-02-04 16:00:04 +0100

    add_key_up_event_listener

--- lib/window/src/canvas.rs
+++ lib/window/src/canvas.rs
@@ -42,6 +42,7 @@ impl Window
         self.add_mousemove_event_listener(events.clone());
         self.add_mousewheel_event_listener(events.clone());
         self.add_key_down_event_listener(events.clone());
+        self.add_key_up_event_listener(events.clone());
 
         *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
             callback(&(*events).borrow());
@@ -114,6 +115,18 @@ impl Window
             }
         }) as Box<dyn FnMut(_)>);
         window().add_event_listener_with_callback("keydown", closure.as_ref().unchecked_ref()).unwrap();
+        closure.forget();
+    }
+
+    fn add_key_up_event_listener(&self, events: Rc<RefCell<Vec<Event>>>)
+    {
+        let closure = Closure::wrap(Box::new(move |event: web_sys::KeyboardEvent| {
+            if !event.default_prevented() {
+                (*events).borrow_mut().push(Event::Key {state: State::Released, kind: event.code()});
+                event.prevent_default();
+            }
+        }) as Box<dyn FnMut(_)>);
+        window().add_event_listener_with_callback("keyup", closure.as_ref().unchecked_ref()).unwrap();
         closure.forget();
     }
 
