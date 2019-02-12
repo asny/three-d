fc70052f92db1a5ea32c6405bb5064bbb940899c
Asger <asger.nyman@alexandra.dk>
2019-02-12 13:33:06 +0100

    Web error handling using console_error_panic_hook

--- examples/web/src/lib.rs
+++ examples/web/src/lib.rs
@@ -6,6 +6,7 @@ include!("../../hello_world.rs");
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue>
 {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     main();
     Ok(())
 }
