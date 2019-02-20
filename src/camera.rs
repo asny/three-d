d8e8799aaed243440dcf505104610fb32a2dbdff
Asger <asger.nyman@alexandra.dk>
2019-02-20 12:01:44 +0100

    Added Camera.view_direction_at()

--- src/camera.rs
+++ src/camera.rs
@@ -9,13 +9,15 @@ pub trait Camera
     fn up(&self) -> &Vec3;
     fn set_view(&mut self, position: Vec3, target: Vec3, up: Vec3);
     fn mirror_in_xz_plane(&mut self);
+    fn view_direction_at(&self, screen_coordinates: (f64, f64)) -> Vec3;
 }
 
 struct BaseCamera {
     position: Vec3,
     target: Vec3,
     up: Vec3,
-    view: Mat4
+    view: Mat4,
+    projection: Mat4
 }
 
 impl BaseCamera
@@ -35,18 +37,27 @@ impl BaseCamera
         self.view[1][1] = -self.view[1][1];
         self.view[1][2] = -self.view[1][2];
     }
+
+    pub fn view_direction_at(&self, screen_coordinates: (f64, f64)) -> Vec3
+    {
+        let mut v = self.view.clone();
+        v[3] = vec4(0.0, 0.0, 0.0, 1.0);
+        let m = (self.projection * v).invert().unwrap();
+        let screen_pos = vec4(2. * screen_coordinates.0 as f32 - 1., 1. - 2. * screen_coordinates.1 as f32, 1., 1.);
+        let ray_world = m * screen_pos;
+        vec3(ray_world.x, ray_world.y, ray_world.z).normalize()
+    }
 }
 
 pub struct PerspectiveCamera {
-    base: BaseCamera,
-    projection: Mat4
+    base: BaseCamera
 }
 
 impl PerspectiveCamera
 {
     pub fn new(position: Vec3, target: Vec3, up: Vec3, fovy: Degrees, aspect: f32, z_near: f32, z_far: f32) -> PerspectiveCamera
     {
-        let mut camera = PerspectiveCamera { base: BaseCamera {position, target, up, view: Mat4::identity()}, projection: Mat4::identity() };
+        let mut camera = PerspectiveCamera { base: BaseCamera {position, target, up, view: Mat4::identity(), projection: Mat4::identity()} };
         camera.set_view(position, target, up);
         camera.set_extent(fovy, aspect, z_near, z_far);
         camera
@@ -55,7 +66,7 @@ impl PerspectiveCamera
     pub fn set_extent(&mut self, fovy: Degrees, aspect: f32, z_near: f32, z_far: f32)
     {
         if z_near < 0.0 || z_near > z_far { panic!("Wrong perspective camera parameters") };
-        self.projection = perspective(fovy, aspect, z_near, z_far);
+        self.base.projection = perspective(fovy, aspect, z_near, z_far);
     }
 }
 
@@ -68,7 +79,7 @@ impl Camera for PerspectiveCamera
 
     fn get_projection(&self) -> &Mat4
     {
-        &self.projection
+        &self.base.projection
     }
 
     fn position(&self) -> &Vec3
@@ -95,18 +106,22 @@ impl Camera for PerspectiveCamera
     {
         self.base.mirror_in_xz_plane();
     }
+
+    fn view_direction_at(&self, screen_coordinates: (f64, f64)) -> Vec3
+    {
+        self.base.view_direction_at(screen_coordinates)
+    }
 }
 
 pub struct OrthographicCamera {
-    base: BaseCamera,
-    projection: Mat4
+    base: BaseCamera
 }
 
 impl OrthographicCamera
 {
     pub fn new(position: Vec3, target: Vec3, up: Vec3, width: f32, height: f32, depth: f32) -> OrthographicCamera
     {
-        let mut camera = OrthographicCamera { base: BaseCamera {position, target, up, view: Mat4::identity()}, projection: Mat4::identity() };
+        let mut camera = OrthographicCamera { base: BaseCamera {position, target, up, view: Mat4::identity(), projection: Mat4::identity()} };
         camera.set_view(position, target, up);
         camera.set_extent(width, height, depth);
         camera
@@ -114,7 +129,7 @@ impl OrthographicCamera
 
     fn set_extent(&mut self, width: f32, height: f32, depth: f32)
     {
-        self.projection = ortho(-0.5 * width, 0.5 * width, -0.5 * height, 0.5 * height, -0.5 * depth, 0.5 * depth);
+        self.base.projection = ortho(-0.5 * width, 0.5 * width, -0.5 * height, 0.5 * height, -0.5 * depth, 0.5 * depth);
     }
 }
 
@@ -127,7 +142,7 @@ impl Camera for OrthographicCamera
 
     fn get_projection(&self) -> &Mat4
     {
-        &self.projection
+        &self.base.projection
     }
 
     fn position(&self) -> &Vec3
@@ -153,5 +168,10 @@ impl Camera for OrthographicCamera
     fn mirror_in_xz_plane(&mut self)
     {
         self.base.mirror_in_xz_plane();
+    }
+
+    fn view_direction_at(&self, screen_coordinates: (f64, f64)) -> Vec3
+    {
+        self.base.view_direction_at(screen_coordinates)
     }
 }
