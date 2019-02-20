de9e3b829eca8db6b99b6189e844ffc4080d0451
Asger <asger.nyman@alexandra.dk>
2019-02-20 12:08:40 +0100

    Reuse matrix used for picking

--- src/camera.rs
+++ src/camera.rs
@@ -17,7 +17,8 @@ struct BaseCamera {
     target: Vec3,
     up: Vec3,
     view: Mat4,
-    projection: Mat4
+    projection: Mat4,
+    screen2ray: Mat4
 }
 
 impl BaseCamera
@@ -29,6 +30,7 @@ impl BaseCamera
         let dir = (target - position).normalize();
         self.up = dir.cross(up.normalize().cross(dir));
         self.view = Mat4::look_at(Point::from_vec(self.position), Point::from_vec(self.target), self.up);
+        self.update_screen2ray();
     }
 
     pub fn mirror_in_xz_plane(&mut self)
@@ -36,16 +38,21 @@ impl BaseCamera
         self.view[1][0] = -self.view[1][0];
         self.view[1][1] = -self.view[1][1];
         self.view[1][2] = -self.view[1][2];
+        self.update_screen2ray();
     }
 
     pub fn view_direction_at(&self, screen_coordinates: (f64, f64)) -> Vec3
     {
+        let screen_pos = vec4(2. * screen_coordinates.0 as f32 - 1., 1. - 2. * screen_coordinates.1 as f32, 1., 1.);
+        let ray_world = self.screen2ray * screen_pos;
+        vec3(ray_world.x, ray_world.y, ray_world.z).normalize()
+    }
+
+    pub fn update_screen2ray(&mut self)
+    {
         let mut v = self.view.clone();
         v[3] = vec4(0.0, 0.0, 0.0, 1.0);
-        let m = (self.projection * v).invert().unwrap();
-        let screen_pos = vec4(2. * screen_coordinates.0 as f32 - 1., 1. - 2. * screen_coordinates.1 as f32, 1., 1.);
-        let ray_world = m * screen_pos;
-        vec3(ray_world.x, ray_world.y, ray_world.z).normalize()
+        self.screen2ray = (self.projection * v).invert().unwrap();
     }
 }
 
@@ -57,7 +64,7 @@ impl PerspectiveCamera
 {
     pub fn new(position: Vec3, target: Vec3, up: Vec3, fovy: Degrees, aspect: f32, z_near: f32, z_far: f32) -> PerspectiveCamera
     {
-        let mut camera = PerspectiveCamera { base: BaseCamera {position, target, up, view: Mat4::identity(), projection: Mat4::identity()} };
+        let mut camera = PerspectiveCamera { base: BaseCamera {position, target, up, view: Mat4::identity(), projection: Mat4::identity(), screen2ray: Mat4::identity()} };
         camera.set_view(position, target, up);
         camera.set_extent(fovy, aspect, z_near, z_far);
         camera
@@ -67,6 +74,7 @@ impl PerspectiveCamera
     {
         if z_near < 0.0 || z_near > z_far { panic!("Wrong perspective camera parameters") };
         self.base.projection = perspective(fovy, aspect, z_near, z_far);
+        self.base.update_screen2ray();
     }
 }
 
@@ -121,7 +129,7 @@ impl OrthographicCamera
 {
     pub fn new(position: Vec3, target: Vec3, up: Vec3, width: f32, height: f32, depth: f32) -> OrthographicCamera
     {
-        let mut camera = OrthographicCamera { base: BaseCamera {position, target, up, view: Mat4::identity(), projection: Mat4::identity()} };
+        let mut camera = OrthographicCamera { base: BaseCamera {position, target, up, view: Mat4::identity(), projection: Mat4::identity(), screen2ray: Mat4::identity()} };
         camera.set_view(position, target, up);
         camera.set_extent(width, height, depth);
         camera
@@ -130,6 +138,7 @@ impl OrthographicCamera
     fn set_extent(&mut self, width: f32, height: f32, depth: f32)
     {
         self.base.projection = ortho(-0.5 * width, 0.5 * width, -0.5 * height, 0.5 * height, -0.5 * depth, 0.5 * depth);
+        self.base.update_screen2ray();
     }
 }
 
