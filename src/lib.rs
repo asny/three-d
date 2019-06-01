46860ebc219386ffbc4f1c12d93579a008e93bae
Asger Nyman Christiansen <asgernyman@gmail.com>
2019-06-01 16:30:05 +0200

    Rename deferred to renderer

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,11 @@
 pub mod objects;
 pub mod effects;
 pub mod light;
-pub mod deferred;
+pub mod renderer;
 
 pub use core;
 pub use core::*;
 pub use window;
 pub use crate::camera::Camera;
 pub use crate::camerahandler::CameraHandler;
-pub use crate::deferred::*;
+pub use crate::renderer::*;
