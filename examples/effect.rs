22b11fb363da6b0f12bc4ac121d2abe9e42b02d6
Asger Nyman Christiansen <asgernyman@gmail.com>
2019-06-26 09:12:30 +0200

    Fix effect example

--- examples/effect.rs
+++ examples/effect.rs
@@ -8,18 +8,13 @@ fn main() {
     let gl = window.gl();
 
     // Renderer
-    let renderer = DeferredPipeline::new(&gl, width, height, vec4(0.0, 0.0, 0.0, 1.0)).unwrap();
-
-    // Camera
-    let mut camera = Camera::new_perspective(&gl, vec3(5.0, 5.0, 5.0), vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0),
-                                                    degrees(45.0), width as f32 / height as f32, 0.1, 100.0);
+    let mut renderer = DeferredPipeline::new(&gl, width, height, vec4(0.0, 0.0, 0.0, 1.0)).unwrap();
 
     let mut monkey = objects::ShadedMesh::new_from_obj_source(&gl, include_str!("assets/models/suzanne.obj").to_string()).unwrap();
     monkey.color = vec3(0.5, 1.0, 0.5);
 
-    let ambient_light = crate::light::AmbientLight::new();
-    let mut light = dust::light::DirectionalLight::new(vec3(0.0, -1.0, 0.0));
-    light.base.intensity = 1.0;
+    renderer.directional_light(0).unwrap().set_direction(&vec3(0.0, -1.0, 0.0));
+    renderer.directional_light(0).unwrap().set_intensity(1.0);
 
     let mut fog_effect = effects::FogEffect::new(&gl).unwrap();
     fog_effect.color = vec3(0.9, 0.8, 0.8);
@@ -32,7 +27,7 @@ fn main() {
     window.render_loop(move |events, elapsed_time|
     {
         for event in events {
-            handle_camera_events(event, &mut camera_handler, &mut camera);
+            handle_camera_events(event, &mut camera_handler, &mut renderer.camera);
             match event {
                 Event::Key { state, kind } => {
                     if kind == "R" && *state == State::Pressed
@@ -47,17 +42,16 @@ fn main() {
 
         // draw
         // Geometry pass
-        renderer.geometry_pass_begin().unwrap();
-        let transformation = Mat4::identity();
-        monkey.render(&transformation, &camera);
+        renderer.geometry_pass(&|camera| {
+            let transformation = Mat4::identity();
+            monkey.render(&transformation, camera);
+        }).unwrap();
 
         // Light pass
-        renderer.light_pass_begin(&camera).unwrap();
-        renderer.shine_ambient_light(&ambient_light).unwrap();
-        renderer.shine_directional_light(&light).unwrap();
+        renderer.light_pass().unwrap();
 
         // Effect
-        fog_effect.apply(renderer.full_screen(), time as f32, &camera, renderer.geometry_pass_position_texture(), renderer.geometry_pass_depth_texture()).unwrap();
+        fog_effect.apply(renderer.full_screen(), time as f32, &renderer.camera, renderer.geometry_pass_position_texture(), renderer.geometry_pass_depth_texture()).unwrap();
         debug_effect.apply(renderer.full_screen(), renderer.geometry_pass_color_texture(), renderer.geometry_pass_position_texture(), renderer.geometry_pass_normal_texture(), renderer.geometry_pass_depth_texture()).unwrap();
     }).unwrap();
 }
