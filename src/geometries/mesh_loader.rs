6f0b34b667ce41a3c572f6eb61d34ae5f0a9f283
Asger Nyman Christiansen <asgernyman@gmail.com>
2019-12-29 11:19:23 +0100

    Added object type

--- src/geometries/mesh_loader.rs
+++ src/geometries/mesh_loader.rs
@@ -1,82 +1,70 @@
 use crate::geometries::mesh::Mesh;
+use crate::objects::MeshShader;
+use crate::object::*;
 use crate::buffer::*;
 use crate::core::Gl;
-use core::types::*;
 
 impl Mesh {
-    pub fn new_from_obj_source(gl: &Gl, source: String) -> Result<Mesh, Error>
+    pub fn new_from_obj_source(gl: &Gl, source: String) -> Result<Vec<Object>, Error>
     {
         let objs = wavefront_obj::obj::parse(source).unwrap();
-        let obj = objs.objects.first().unwrap();
 
-        let mut positions = Vec::new();
-        obj.vertices.iter().for_each(|v| {
-            positions.push(v.x as f32);
-            positions.push(v.y as f32);
-            positions.push(v.z as f32);
-        });
-        let mut normals = vec![0.0f32; positions.len() * 3];
-        let mut indices = Vec::new();
-        let has_normals = !obj.normals.is_empty();
-        for shape in obj.geometry.first().unwrap().shapes.iter() {
-            match shape.primitive {
-                wavefront_obj::obj::Primitive::Triangle(i0, i1, i2) => {
-                    indices.push(i0.0 as u32);
-                    indices.push(i1.0 as u32);
-                    indices.push(i2.0 as u32);
+        let mut objects = Vec::new();
+        for obj in objs.objects.iter() { // Objects consisting of several meshes with different materials
+            let mut object = Object::new(obj.name.to_owned());
 
-                    if has_normals {
-                        let mut normal = obj.normals[i0.2.unwrap()];
-                        normals[i0.0 * 3] = normal.x as f32;
-                        normals[i0.0 * 3 + 1] = normal.y as f32;
-                        normals[i0.0 * 3 + 2] = normal.z as f32;
+            let mut positions = Vec::new();
+            obj.vertices.iter().for_each(|v| {
+                positions.push(v.x as f32);
+                positions.push(v.y as f32);
+                positions.push(v.z as f32);
+            });
+            let mut normals = vec![0.0f32; positions.len() * 3];
+            let mut indices = Vec::new();
+            let has_normals = !obj.normals.is_empty();
 
-                        normal = obj.normals[i1.2.unwrap()];
-                        normals[i1.0 * 3] = normal.x as f32;
-                        normals[i1.0 * 3 + 1] = normal.y as f32;
-                        normals[i1.0 * 3 + 2] = normal.z as f32;
+            for mesh in obj.geometry.iter() { // All meshes with different materials
+                for primitive in mesh.shapes.iter() { // All triangles with same material
+                    match primitive.primitive {
+                        wavefront_obj::obj::Primitive::Triangle(i0, i1, i2) => {
+                            indices.push(i0.0 as u32);
+                            indices.push(i1.0 as u32);
+                            indices.push(i2.0 as u32);
 
-                        normal = obj.normals[i2.2.unwrap()];
-                        normals[i2.0 * 3] = normal.x as f32;
-                        normals[i2.0 * 3 + 1] = normal.y as f32;
-                        normals[i2.0 * 3 + 2] = normal.z as f32;
+                            if has_normals {
+                                let mut normal = obj.normals[i0.2.unwrap()];
+                                normals[i0.0 * 3] = normal.x as f32;
+                                normals[i0.0 * 3 + 1] = normal.y as f32;
+                                normals[i0.0 * 3 + 2] = normal.z as f32;
+
+                                normal = obj.normals[i1.2.unwrap()];
+                                normals[i1.0 * 3] = normal.x as f32;
+                                normals[i1.0 * 3 + 1] = normal.y as f32;
+                                normals[i1.0 * 3 + 2] = normal.z as f32;
+
+                                normal = obj.normals[i2.2.unwrap()];
+                                normals[i2.0 * 3] = normal.x as f32;
+                                normals[i2.0 * 3 + 1] = normal.y as f32;
+                                normals[i2.0 * 3 + 2] = normal.z as f32;
+                            }
+                        },
+                        _ => {}
                     }
-                },
-                _ => {}
-            }
-        }
-        if !has_normals {
-            //Compute normals
-            println!("{}", indices.len()/3);
-            println!("{}", positions.len()/3);
-            for face in 0..indices.len()/3 {
-                let index0 = indices[face*3] as usize;
-                let p0 = vec3(positions[index0*3], positions[index0*3+1], positions[index0*3+2]);
-                let index1 = indices[face*3 + 1] as usize;
-                let p1 = vec3(positions[index1*3], positions[index1*3+1], positions[index1*3+2]);
-                let index2 = indices[face*3 + 2] as usize;
-                let p2 = vec3(positions[index2*3], positions[index2*3+1], positions[index2*3+2]);
+                }
 
-                let normal = (p1 - p0).cross(p2 - p0);
-                normals[index0*3] += normal.x;
-                normals[index0*3+1] += normal.y;
-                normals[index0*3+2] += normal.z;
-                normals[index1*3] += normal.x;
-                normals[index1*3+1] += normal.y;
-                normals[index1*3+2] += normal.z;
-                normals[index2*3] += normal.x;
-                normals[index2*3+1] += normal.y;
-                normals[index2*3+2] += normal.z;
+
+                let mesh = if !has_normals {
+                    Self::new_with_computed_normals(&gl, &indices, &positions)?
+                } else {
+                    Self::new(&gl, &indices, &positions, &normals)?
+                };
+                object.add(mesh, MeshShader::new(gl).unwrap());
+
             }
 
-            for i in 0..normals.len()/3 {
-                let normal = vec3(normals[3*i], normals[3*i+1], normals[3*i+2]).normalize();
-                normals[3*i] = normal.x;
-                normals[3*i+1] = normal.y;
-                normals[3*i+2] = normal.z;
-            }
+            objects.push(object);
         }
 
-        Self::new(&gl, &indices, &positions, &normals)
+        Ok(objects)
     }
 }
