adcf0e70555921b6e6418a528ee61c2d823395c9
Asger Nyman Christiansen <asgernyman@gmail.com>
2020-02-14 15:40:26 +0100

    remove camera handler

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,10 @@ pub mod objects;
 pub mod effects;
 pub mod light;
 pub mod renderer;
-pub mod camerahandler;
 
 pub use core;
 pub use core::*;
 pub use window;
-pub use crate::camerahandler::CameraHandler;
 pub use crate::renderer::*;
 pub use crate::light::*;
 pub use crate::objects::*;
