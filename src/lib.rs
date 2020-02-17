cb60d141ed4e30e4d3b8b02e7ae38a8d6999e028
Asger Nyman Christiansen <asgernyman@gmail.com>
2020-02-17 19:13:46 +0100

    core-only feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,24 @@
 
 pub mod core;
-pub mod objects;
-pub mod effects;
-pub mod light;
+
+#[cfg(not(feature = "core-only"))]
 pub mod renderer;
+#[cfg(not(feature = "core-only"))]
+pub mod light;
+#[cfg(not(feature = "core-only"))]
+pub mod objects;
+#[cfg(not(feature = "core-only"))]
+pub mod effects;
 
 pub use window;
 
 pub use crate::core::*;
+
+#[cfg(not(feature = "core-only"))]
 pub use crate::renderer::*;
+#[cfg(not(feature = "core-only"))]
 pub use crate::light::*;
+#[cfg(not(feature = "core-only"))]
 pub use crate::objects::*;
+#[cfg(not(feature = "core-only"))]
+pub use crate::effects::*;
