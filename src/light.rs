10c64f8e70a84fe88119b667e2bf9d331aa12e6a
Asger Nyman Christiansen <asgernyman@gmail.com>
2020-03-29 21:03:15 +0200

    Do not use mip maps for shadow maps

--- src/light.rs
+++ src/light.rs
@@ -100,7 +100,7 @@ impl DirectionalLight {
         state::depth_write(&self.gl, true);
         state::depth_test(&self.gl, state::DepthTestType::LessOrEqual);
 
-        self.shadow_texture = Some(Texture2D::new_empty(&self.gl, texture_width, texture_height, Interpolation::Linear, Interpolation::Linear, Some(Interpolation::Linear),
+        self.shadow_texture = Some(Texture2D::new_empty(&self.gl, texture_width, texture_height, Interpolation::Linear, Interpolation::Linear, None,
                                                         Wrapping::ClampToEdge, Wrapping::ClampToEdge, Format::Depth32F).unwrap());
         RenderTarget::write_to_depth(&self.gl, 0, 0, texture_width, texture_height, Some(1.0),
             self.shadow_texture.as_ref(),
@@ -259,7 +259,7 @@ impl SpotLight {
         state::depth_write(&self.gl, true);
         state::depth_test(&self.gl, state::DepthTestType::LessOrEqual);
 
-        self.shadow_texture = Some(Texture2D::new_empty(&self.gl, texture_size, texture_size, Interpolation::Linear, Interpolation::Linear, Some(Interpolation::Linear),
+        self.shadow_texture = Some(Texture2D::new_empty(&self.gl, texture_size, texture_size, Interpolation::Linear, Interpolation::Linear, None,
                                                         Wrapping::ClampToEdge, Wrapping::ClampToEdge, Format::Depth32F).unwrap());
         RenderTarget::write_to_depth(&self.gl, 0, 0, texture_size, texture_size, Some(1.0),
             self.shadow_texture.as_ref(), &|| render_scene(self.shadow_camera.as_ref().unwrap())).unwrap();
