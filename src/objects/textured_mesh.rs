6abd96347fd7fe4e44cdf8983c68c4f8f2510920
Asger Nyman Christiansen <asgernyman@gmail.com>
2020-10-29 10:21:19 +0100

    TexturedMesh from file and CPUMesh functionality

--- src/objects/textured_mesh.rs
+++ src/objects/textured_mesh.rs
@@ -1,11 +1,13 @@
 
 use crate::*;
+use std::rc::Rc;
+use std::cell::RefCell;
 
 pub struct TexturedMesh {
     program: program::Program,
     pub position_buffer: VertexBuffer,
     pub normal_buffer: VertexBuffer,
-    pub uv_buffer: Option<VertexBuffer>,
+    pub uv_buffer: VertexBuffer,
     pub index_buffer: ElementBuffer,
     pub texture: texture::Texture2D,
     pub diffuse_intensity: f32,
@@ -19,7 +21,7 @@ impl TexturedMesh
     {
         let position_buffer = VertexBuffer::new_with_static_f32(gl, positions)?;
         let normal_buffer = VertexBuffer::new_with_static_f32(gl, normals)?;
-        let uv_buffer = if uvs.len() == 0 {None} else { Some(VertexBuffer::new_with_static_f32(gl, uvs)?) };
+        let uv_buffer = VertexBuffer::new_with_static_f32(gl, uvs)?;
         let index_buffer = ElementBuffer::new_with_u32(gl, indices)?;
 
         let program = program::Program::from_source(&gl,
@@ -30,6 +32,29 @@ impl TexturedMesh
             diffuse_intensity: 0.5, specular_intensity: 0.2, specular_power: 6.0 })
     }
 
+    pub fn from_file(gl: &Gl, path: &'static str, texture: texture::Texture2D) -> Rc<RefCell<Self>> {
+        Self::from_file_with_mapping(gl, path, texture, |_| {})
+    }
+
+    pub fn from_file_with_mapping<F: 'static>(gl: &Gl, path: &'static str, texture: texture::Texture2D, mapping: F) -> Rc<RefCell<Self>>
+        where F: Fn(CPUMesh)
+    {
+        let m = Rc::new(RefCell::new(Self::new(gl, &[], &[], &[], &[], texture).unwrap()));
+        let clone = m.clone();
+        CPUMesh::from_file_with_mapping(path, move |cpu_mesh| {
+            m.borrow_mut().index_buffer.fill_with_u32(&cpu_mesh.indices);
+            m.borrow_mut().position_buffer.fill_with_static_f32(&cpu_mesh.positions);
+            m.borrow_mut().normal_buffer.fill_with_static_f32(&cpu_mesh.normals);
+            m.borrow_mut().uv_buffer.fill_with_static_f32(&cpu_mesh.uvs);
+            mapping(cpu_mesh);
+        });
+        clone
+    }
+
+    pub fn from_cpu_mesh(gl: &Gl, cpu_mesh: &CPUMesh, texture: texture::Texture2D) -> Result<Self, Error> {
+        Self::new(gl, &cpu_mesh.indices, &cpu_mesh.positions, &cpu_mesh.normals, &cpu_mesh.uvs, texture)
+    }
+
     pub fn render(&self, transformation: &Mat4, camera: &camera::Camera)
     {
         self.program.add_uniform_float("diffuse_intensity", &self.diffuse_intensity).unwrap();
@@ -38,20 +63,13 @@ impl TexturedMesh
 
         self.program.use_texture(&self.texture,"tex").unwrap();
 
-        if let Some(ref buffer) = self.uv_buffer
-        {
-            self.program.use_attribute_vec3_float(buffer, "uv_coordinates").unwrap();
-            self.program.add_uniform_int("use_uvs", &1).unwrap();
-        }
-        else {
-            self.program.add_uniform_int("use_uvs", &0).unwrap();
-        }
-
+        self.program.add_uniform_int("use_uvs", &(if self.uv_buffer.count() > 0 {1} else {0})).unwrap();
         self.program.add_uniform_mat4("modelMatrix", &transformation).unwrap();
         self.program.use_uniform_block(camera.matrix_buffer(), "Camera");
 
         self.program.add_uniform_mat4("normalMatrix", &transformation.invert().unwrap().transpose()).unwrap();
 
+        self.program.use_attribute_vec3_float(&self.uv_buffer, "uv_coordinates").unwrap();
         self.program.use_attribute_vec3_float(&self.position_buffer, "position").unwrap();
         self.program.use_attribute_vec3_float(&self.normal_buffer, "normal").unwrap();
 
