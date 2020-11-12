ff095ba8719001b5918faf641dc63ce13549e175
Asger Nyman Christiansen <asgernyman@gmail.com>
2020-11-12 21:51:04 +0100

    Loader returns a map path -> bytes

--- src/io.rs
+++ src/io.rs
@@ -1,60 +1,56 @@
 
+use std::collections::HashMap;
 use std::rc::Rc;
 use std::cell::RefCell;
 use log::info;
 
-pub struct Loader {
-    pub loads: Rc<RefCell<Vec<Load>>>
-}
+type Ref<T> = Rc<RefCell<T>>;
+type Loads = Ref<HashMap<String, Ref<Vec<u8>>>>;
 
-pub struct Load {
-    is_loaded: Rc<RefCell<bool>>,
-    pub bytes: Rc<RefCell<Vec<u8>>>
+pub struct Loader {
+    loads: Loads
 }
 
 impl Loader {
 
     pub fn new() -> Self {
-        Self { loads: Rc::new(RefCell::new(Vec::new()))}
+        Self { loads: Rc::new(RefCell::new(HashMap::new()))}
     }
 
     pub fn load_cpu_mesh(&mut self, path: &'static str)
     {
-        let load = Load {
-            is_loaded: Rc::new(RefCell::new(false)),
-            bytes: Rc::new(RefCell::new(Vec::new()))
-        };
+        let bytes = Rc::new(RefCell::new(Vec::new()));
 
-        Self::load_file(path, load.is_loaded.clone(), load.bytes.clone());
+        Self::load_file(path,bytes.clone());
 
-        self.loads.borrow_mut().push(load);
+        self.loads.borrow_mut().insert(path.to_string(), bytes);
 
     }
 
     pub fn wait_all<F>(&mut self, callback: F)
-        where F: 'static + FnOnce(&[u8])
+        where F: 'static + FnOnce(Loads)
     {
         Self::wait(self.loads.clone(), callback);
     }
 
-    fn wait<F>(meshes: Rc<RefCell<Vec<Load>>>, callback: F)
-        where F: 'static + FnOnce(&[u8])
+    fn wait<F>(loads: Loads, callback: F)
+        where F: 'static + FnOnce(Loads)
     {
         info!("Wait");
         Self::sleep(1000, move || {
 
             let mut is_loading = false;
-            for mesh in meshes.borrow_mut().iter_mut() {
-                if !*mesh.is_loaded.borrow() {
-                    is_loading = true;
+            for bytes in loads.borrow_mut().values() {
+                if let Ok(b) = bytes.try_borrow() {
+                    is_loading = b.len() == 0;
                 }
             }
             info!("Is loading: {}", is_loading);
 
             if is_loading {
-                Self::wait(meshes, callback);
+                Self::wait(loads, callback);
             } else {
-                callback(meshes.borrow()[0].bytes.borrow().as_ref());
+                callback(loads);
             }
         });
     }
@@ -79,22 +75,21 @@ impl Loader {
     }
 
     #[cfg(not(target_arch = "wasm32"))]
-    fn load_file(path: &'static str, is_loaded: Rc<RefCell<bool>>, bytes: Rc<RefCell<Vec<u8>>>)
+    fn load_file(path: &'static str, bytes: Rc<RefCell<Vec<u8>>>)
     {
         let mut file = std::fs::File::open(path).unwrap();
         use std::io::prelude::*;
         file.read_to_end(&mut bytes.borrow_mut()).unwrap();
-        *is_loaded.borrow_mut() = true;
     }
 
     #[cfg(target_arch = "wasm32")]
-    fn load_file(path: &'static str, is_loaded: Rc<RefCell<bool>>, bytes: Rc<RefCell<Vec<u8>>>)
+    fn load_file(path: &'static str, bytes: Rc<RefCell<Vec<u8>>>)
     {
-        wasm_bindgen_futures::spawn_local(Self::load(path, is_loaded, bytes));
+        wasm_bindgen_futures::spawn_local(Self::load(path, bytes));
     }
 
     #[cfg(target_arch = "wasm32")]
-    async fn load(url: &'static str, is_loaded: Rc<RefCell<bool>>, bytes: Rc<RefCell<Vec<u8>>>)
+    async fn load(url: &'static str, bytes: Rc<RefCell<Vec<u8>>>)
     {
         use wasm_bindgen::prelude::*;
         use wasm_bindgen::JsCast;
@@ -115,7 +110,6 @@ impl Loader {
         // Convert this other `Promise` into a rust `Future`.
         let data: JsValue = JsFuture::from(resp.array_buffer().unwrap()).await.unwrap();
         *bytes.borrow_mut() = js_sys::Uint8Array::new(&data).to_vec();
-        *is_loaded.borrow_mut() = true;
     }
 }
 
