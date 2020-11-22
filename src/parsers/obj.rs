3fc5f55558ec524ee84358362da91dcbd5b7e4d0
Asger Nyman Christiansen <asgernyman@gmail.com>
2020-11-22 20:36:32 +0100

    Obj::parse is indexed if possible

--- src/parsers/obj.rs
+++ src/parsers/obj.rs
@@ -1,5 +1,6 @@
 
 use crate::cpu_mesh::CPUMesh;
+use std::collections::HashMap;
 
 pub struct Obj {
 
@@ -18,52 +19,72 @@ impl Obj {
                 let mut positions = Vec::new();
                 let mut normals = Vec::new();
                 let mut uvs = Vec::new();
-                //let mut indices = Vec::new();
+                let mut indices = Vec::new();
 
-                let mut map = |i: wavefront_obj::obj::VTNIndex| {
-                    //indices.push((start_index + i.0) as u32);
+                let mut map: HashMap<usize, usize> = HashMap::new();
 
-                    let position = obj.vertices[i.0];
-                    positions.push(position.x as f32);
-                    positions.push(position.y as f32);
-                    positions.push(position.z as f32);
+                let mut process = |i: wavefront_obj::obj::VTNIndex| {
 
-                    if let Some(index) = i.1 {
+                    let mut index = map.get(&i.0).map(|v| *v);
 
-                        //println!("{:?} -> {}",index*2, i.0*2);
-                        //uvs_out[start_index + i.0*2] = uvs[index*3];
-                        //uvs_out[start_index + i.0*2 + 1] = uvs[index*3+1];
-                        let uvw = obj.tex_vertices[index];
-                        uvs.push(uvw.u as f32);
-                        uvs.push(uvw.v as f32);
+                    let uvw = i.1.map(|tex_index| obj.tex_vertices[tex_index]);
+                    let normal = i.2.map(|normal_index| obj.normals[normal_index]);
+
+                    if let Some(ind) = index {
+                        if let Some(tex) = uvw {
+                            if ((uvs[ind*2] - tex.u as f32) as f32).abs() > std::f32::EPSILON ||
+                                ((uvs[ind*2+1] - tex.v as f32) as f32).abs() > std::f32::EPSILON {
+                                index = None;
+                            }
+                        }
+                        if let Some(n) = normal {
+                            if ((normals[ind*3] - n.x as f32) as f32).abs() > std::f32::EPSILON ||
+                                ((normals[ind*3+1] - n.y as f32) as f32).abs() > std::f32::EPSILON ||
+                                ((normals[ind*3+2] - n.z as f32) as f32).abs() > std::f32::EPSILON {
+                                index = None;
+                            }
+                        }
                     }
-                    if let Some(index) = i.2 {
 
-                        //println!("{:?} -> {}", index*3 , i.0*3);
-                        let normal = obj.normals[index];
-                        normals.push(normal.x as f32);
-                        normals.push(normal.y as f32);
-                        normals.push(normal.z as f32);
+                    if index.is_none() {
+                        index = Some(positions.len() / 3);
+                        map.insert(i.0, index.unwrap());
+                        let position = obj.vertices[i.0];
+                        positions.push(position.x as f32);
+                        positions.push(position.y as f32);
+                        positions.push(position.z as f32);
+
+                        if let Some(tex) = uvw {
+                            uvs.push(tex.u as f32);
+                            uvs.push(tex.v as f32);
+                        }
+                        if let Some(n) = normal {
+                            normals.push(n.x as f32);
+                            normals.push(n.y as f32);
+                            normals.push(n.z as f32);
+                        }
                     }
+
+                    indices.push(index.unwrap() as u32);
                 };
                 for shape in mesh.shapes.iter() { // All triangles with same material
                     match shape.primitive {
                         wavefront_obj::obj::Primitive::Triangle(i0, i1, i2) => {
-                            map(i0);
-                            map(i1);
-                            map(i2);
-
+                            process(i0);
+                            process(i1);
+                            process(i2);
                         },
                         _ => {}
                     }
                 }
 
-                //println!("indices: {:?}", indices.len());
+                println!("indices: {:?}", indices.len());
                 println!("pos: {}", positions.len());
                 println!("nor: {}", normals.len());
                 println!("uvs: {}", uvs.len());
                 cpu_meshes.push(CPUMesh {
                     positions,
+                    indices: Some(indices),
                     normals: Some(normals),
                     uvs: Some(uvs),
                     .. Default::default()
