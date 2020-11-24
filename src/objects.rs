405ab64f6c83ec4573800a7ce186448a14cf5855
Asger Nyman Christiansen <asgernyman@gmail.com>
2020-11-24 08:45:19 +0100

    Combine TexturedMesh and Mesh (again)

--- src/objects.rs
+++ src/objects.rs
@@ -1,13 +1,11 @@
 pub mod vertices;
 pub mod edges;
 pub mod mesh;
-pub mod textured_mesh;
 pub mod skybox;
 pub mod imposter;
 
 pub use crate::objects::vertices::*;
 pub use crate::objects::edges::*;
 pub use crate::objects::mesh::*;
-pub use crate::objects::textured_mesh::*;
 pub use crate::objects::skybox::*;
 pub use crate::objects::imposter::*;
--- src/objects/mesh.rs
+++ src/objects/mesh.rs
@@ -1,12 +1,18 @@
 
 use crate::*;
+use std::rc::Rc;
+
+pub enum ColorSource {
+    Color(Vec3),
+    Texture((Rc<Texture2D>, Option<VertexBuffer>))
+}
 
 pub struct Mesh {
     program: program::Program,
     position_buffer: VertexBuffer,
     normal_buffer: VertexBuffer,
     index_buffer: Option<ElementBuffer>,
-    pub color: Vec3,
+    pub color: ColorSource,
     pub diffuse_intensity: f32,
     pub specular_intensity: f32,
     pub specular_power: f32
@@ -14,21 +20,34 @@ pub struct Mesh {
 
 impl Mesh
 {
-    pub fn from_cpu_mesh(gl: &Gl, cpu_mesh: &CPUMesh) -> Result<Self, Error> {
+    pub fn from_cpu_mesh(gl: &Gl, cpu_mesh: &CPUMesh) -> Result<Self, Error>
+    {
         let position_buffer = VertexBuffer::new_with_static_f32(gl, &cpu_mesh.positions)?;
         let normal_buffer = VertexBuffer::new_with_static_f32(gl,
               cpu_mesh.normals.as_ref().ok_or(Error::FailedToCreateMesh {message:
               "Cannot create a mesh without normals. Consider calling compute_normals on the CPUMesh before creating the mesh.".to_string()})?)?;
-        let index_buffer = if let Some(ref ind) = cpu_mesh.indices {
-            Some(ElementBuffer::new_with_u32(gl, ind)?)
-        } else {None};
+        let index_buffer = if let Some(ref ind) = cpu_mesh.indices { Some(ElementBuffer::new_with_u32(gl, ind)?) } else {None};
+
+
+        let color = if let Some(ref img) = cpu_mesh.texture {
+            let uv_buffer = if let Some(ref uvs) = cpu_mesh.uvs { Some(VertexBuffer::new_with_static_f32(gl, uvs)?) } else {None};
+            use image::GenericImageView;
+            let texture = Rc::new(texture::Texture2D::new_with_u8(&gl,Interpolation::Linear, Interpolation::Linear,
+                                                                  Some(Interpolation::Linear),Wrapping::Repeat, Wrapping::Repeat,
+                                                                  img.width(), img.height(), &img.to_bytes()).unwrap());
+            ColorSource::Texture((texture, uv_buffer))
+        } else {
+            ColorSource::Color(cpu_mesh.color.map(|(r, g, b)| vec3(r, g, b)).unwrap_or(vec3(1.0, 1.0, 1.0)))
+        };
 
         let program = program::Program::from_source(&gl,
                                                     include_str!("shaders/mesh_shaded.vert"),
-                                                    include_str!("shaders/shaded.frag"))?;
+                                                    match color {
+                                                        ColorSource::Color(_) => include_str!("shaders/shaded.frag"),
+                                                        ColorSource::Texture(_) => include_str!("shaders/textured.frag")
+                                                    })?;
 
-        Ok(Self { index_buffer, position_buffer, normal_buffer, program,
-            color: cpu_mesh.color.map(|(r, g, b)| vec3(r, g, b)).unwrap_or(vec3(1.0, 1.0, 1.0)),
+        Ok(Self { index_buffer, position_buffer, normal_buffer, program, color,
             diffuse_intensity: cpu_mesh.diffuse_intensity.unwrap_or(0.5),
             specular_intensity: cpu_mesh.specular_intensity.unwrap_or(0.2),
             specular_power: cpu_mesh.specular_power.unwrap_or(6.0) })
@@ -39,12 +58,26 @@ impl Mesh
         self.program.add_uniform_float("diffuse_intensity", &self.diffuse_intensity).unwrap();
         self.program.add_uniform_float("specular_intensity", &self.specular_intensity).unwrap();
         self.program.add_uniform_float("specular_power", &self.specular_power).unwrap();
-        self.program.add_uniform_vec3("color", &self.color).unwrap();
 
         self.program.add_uniform_mat4("modelMatrix", &transformation).unwrap();
         self.program.use_uniform_block(camera.matrix_buffer(), "Camera");
-
         self.program.add_uniform_mat4("normalMatrix", &transformation.invert().unwrap().transpose()).unwrap();
+
+        match self.color {
+            ColorSource::Color(ref color) => {
+                self.program.add_uniform_vec3("color", color).unwrap();
+            },
+            ColorSource::Texture((ref texture, ref uv_buffer)) => {
+
+                self.program.use_texture(texture.as_ref(),"tex").unwrap();
+                if let Some(uv_buffer) = uv_buffer {
+                    self.program.add_uniform_int("use_uvs", &1).unwrap();
+                    self.program.use_attribute_vec2_float(uv_buffer, "uv_coordinates").unwrap();
+                } else {
+                    self.program.add_uniform_int("use_uvs", &0).unwrap();
+                }
+            }
+        }
 
         self.program.use_attribute_vec3_float(&self.position_buffer, "position").unwrap();
         self.program.use_attribute_vec3_float(&self.normal_buffer, "normal").unwrap();
