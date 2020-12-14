69d2f27c7c87860306173a71b4358e7dbea67740
Asger Nyman Christiansen <asgernyman@gmail.com>
2020-12-14 21:29:10 +0100

    Mesh render functions returns a result

--- src/objects/mesh.rs
+++ src/objects/mesh.rs
@@ -37,7 +37,7 @@ impl Mesh
             use image::GenericImageView;
             ColorSource::Texture(Rc::new(texture::Texture2D::new_with_u8(&gl, Interpolation::Linear, Interpolation::Linear,
                                                                   Some(Interpolation::Linear), Wrapping::Repeat, Wrapping::Repeat,
-                                                                  img.width(), img.height(), &img.to_bytes()).unwrap()))
+                                                                  img.width(), img.height(), &img.to_bytes())?))
         } else {
             ColorSource::Color(cpu_mesh.color.map(|(r, g, b, a)| vec4(r, g, b, a)).unwrap_or(vec4(1.0, 1.0, 1.0, 1.0)))
         };
@@ -53,51 +53,53 @@ impl Mesh
         &self.name
     }
 
-    pub fn render_with_lighting(&self, transformation: &Mat4, camera: &camera::Camera, light: &DirectionalLight)
+    pub fn render_with_lighting(&self, transformation: &Mat4, camera: &camera::Camera, light: &DirectionalLight) -> Result<(), Error>
     {
         let program = match self.color {
             ColorSource::Color(_) => self.program_color.as_ref(),
             ColorSource::Texture(_) => self.program_texture.as_ref()
         };
-        program.add_uniform_vec3("eyePosition", &camera.position()).unwrap();
-        program.use_texture(light.shadow_map(), "shadowMap").unwrap();
+        program.add_uniform_vec3("eyePosition", &camera.position())?;
+        program.use_texture(light.shadow_map(), "shadowMap")?;
         program.use_uniform_block(light.buffer(), "DirectionalLightUniform");
-        self.render_internal(program, transformation, camera);
+        self.render_internal(program, transformation, camera)?;
+        Ok(())
     }
 
-    fn render_internal(&self, program: &Program, transformation: &Mat4, camera: &camera::Camera)
+    fn render_internal(&self, program: &Program, transformation: &Mat4, camera: &camera::Camera) -> Result<(), Error>
     {
-        program.add_uniform_float("diffuse_intensity", &self.diffuse_intensity).unwrap();
-        program.add_uniform_float("specular_intensity", &self.specular_intensity).unwrap();
-        program.add_uniform_float("specular_power", &self.specular_power).unwrap();
+        program.add_uniform_float("diffuse_intensity", &self.diffuse_intensity)?;
+        program.add_uniform_float("specular_intensity", &self.specular_intensity)?;
+        program.add_uniform_float("specular_power", &self.specular_power)?;
 
-        program.add_uniform_mat4("modelMatrix", &transformation).unwrap();
+        program.add_uniform_mat4("modelMatrix", &transformation)?;
         program.use_uniform_block(camera.matrix_buffer(), "Camera");
-        program.add_uniform_mat4("normalMatrix", &transformation.invert().unwrap().transpose()).unwrap();
+        program.add_uniform_mat4("normalMatrix", &transformation.invert().unwrap().transpose())?;
 
         match self.color {
             ColorSource::Color(ref color) => {
-                program.add_uniform_vec4("color", color).unwrap();
+                program.add_uniform_vec4("color", color)?;
             },
             ColorSource::Texture(ref texture) => {
-                program.use_texture(texture.as_ref(),"tex").unwrap();
+                program.use_texture(texture.as_ref(),"tex")?;
                 if let Some(ref uv_buffer) = self.uv_buffer {
-                    program.add_uniform_int("use_uvs", &1).unwrap();
-                    program.use_attribute_vec2_float(uv_buffer, "uv_coordinates").unwrap();
+                    program.add_uniform_int("use_uvs", &1)?;
+                    program.use_attribute_vec2_float(uv_buffer, "uv_coordinates")?;
                 } else {
-                    program.add_uniform_int("use_uvs", &0).unwrap();
+                    program.add_uniform_int("use_uvs", &0)?;
                 }
             }
         }
 
-        program.use_attribute_vec3_float(&self.position_buffer, "position").unwrap();
-        program.use_attribute_vec3_float(&self.normal_buffer, "normal").unwrap();
+        program.use_attribute_vec3_float(&self.position_buffer, "position")?;
+        program.use_attribute_vec3_float(&self.normal_buffer, "normal")?;
 
         if let Some(ref index_buffer) = self.index_buffer {
             program.draw_elements(index_buffer);
         } else {
             program.draw_arrays(self.position_buffer.count() as u32/3);
         }
+        Ok(())
     }
 }
 
@@ -125,12 +127,13 @@ impl DeferredMesh {
         &mut self.mesh
     }
 
-    pub fn render(&self, transformation: &Mat4, camera: &camera::Camera)
+    pub fn render_geometry(&self, transformation: &Mat4, camera: &camera::Camera) -> Result<(), Error>
     {
         let program = match self.mesh.color {
             ColorSource::Color(_) => self.program_deferred_color.as_ref(),
             ColorSource::Texture(_) => self.program_deferred_texture.as_ref()
         };
-        self.mesh.render_internal(program, transformation, camera);
+        self.mesh.render_internal(program, transformation, camera)?;
+        Ok(())
     }
 }
