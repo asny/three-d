b8e0aa88b2f2bd27d25d80bea4a54370f54685e7
Asger Nyman Christiansen <asgernyman@gmail.com>
2020-12-19 21:16:21 +0100

    Fix wireframe example

--- examples/wireframe/main.rs
+++ examples/wireframe/main.rs
@@ -16,12 +16,14 @@ fn main() {
     let mut camera = Camera::new_perspective(&gl, scene_center + scene_radius * vec3(0.6, 0.3, 1.0).normalize(), scene_center, vec3(0.0, 1.0, 0.0),
                                                 degrees(45.0), width as f32 / height as f32, 0.1, 1000.0);
 
-    Loader::load(&["./examples/assets/models/suzanne.3d"], move |loaded| {
-        let mut cpu_mesh = ThreeD::parse(loaded, "./examples/assets/models/suzanne.3d").unwrap().remove(0);
-        cpu_mesh.diffuse_intensity = Some(0.2);
-        cpu_mesh.specular_intensity = Some(0.4);
-        cpu_mesh.specular_power = Some(20.0);
-        let model = renderer.new_mesh(&cpu_mesh).unwrap();
+    Loader::load(&["./examples/assets/suzanne.3d"], move |loaded| {
+        let (mut meshes, mut materials)  = ThreeD::parse(loaded, "./examples/assets/suzanne.3d").unwrap();
+        let cpu_mesh = meshes.remove(0);
+        let mut cpu_material = materials.remove(0);
+        cpu_material.diffuse_intensity = Some(0.2);
+        cpu_material.specular_intensity = Some(0.4);
+        cpu_material.specular_power = Some(20.0);
+        let model = renderer.new_mesh(&cpu_mesh, &renderer.new_material(&cpu_material).unwrap()).unwrap();
 
         let mut edges = renderer.new_cylinder_instances(cpu_mesh.indices.as_ref().unwrap(), &cpu_mesh.positions, 0.007).unwrap();
         edges.diffuse_intensity = 0.8;
@@ -35,14 +37,16 @@ fn main() {
         vertices.specular_power = 5.0;
         vertices.color = vec4(0.9, 0.2, 0.2, 1.0);
 
-        let plane = renderer.new_mesh(&CPUMesh {
-            positions: vec!(-10000.0, 0.0, 10000.0, 10000.0, 0.0, 10000.0, 0.0, 0.0, -10000.0),
-            normals: Some(vec![0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0]),
-            diffuse_intensity: Some(0.2),
-            specular_intensity: Some(0.4),
-            specular_power: Some(20.0),
-            ..Default::default()
-        }).unwrap();
+        let plane = renderer.new_mesh(
+            &CPUMesh {
+                positions: vec!(-10000.0, -1.0, 10000.0, 10000.0, -1.0, 10000.0, 0.0, -1.0, -10000.0),
+                normals: Some(vec![0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0]),
+                ..Default::default()},
+            &Material {color_source: ColorSource::Color(vec4(1.0, 1.0, 1.0, 1.0)),
+                diffuse_intensity: 0.2,
+                specular_intensity: 0.4,
+                specular_power: 20.0, ..Default::default()}
+        ).unwrap();
 
         let mut spot_light0 = SpotLight::new(&gl, 0.6, &vec3(1.0, 1.0, 1.0), &vec3(5.0, 7.0, 5.0),
                                              &vec3(-1.0, -1.0, -1.0), 25.0, 0.1, 0.001, 0.0001).unwrap();
