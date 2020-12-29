3793109bfd6148c38ca0739ebac0e0a7be76c6b4
Asger Nyman Christiansen <asgernyman@gmail.com>
2020-12-29 16:57:06 +0100

    Remove cylinder instance

--- src/phong.rs
+++ src/phong.rs
@@ -5,10 +5,8 @@ pub use crate::material::*;
 pub mod renderer;
 pub use crate::renderer::*;
 
-pub mod cylinder_instances;
 pub mod mesh;
 pub mod imposter;
 
-pub use crate::cylinder_instances::*;
 pub use crate::mesh::*;
 pub use crate::imposter::*;
