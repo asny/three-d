058609f77ac0736be619404feb498dceae9c840e
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-01-12 14:53:28 +0100

    Split forward and deferred pipeline into two files

--- src/phong.rs
+++ src/phong.rs
@@ -2,8 +2,11 @@
 pub mod material;
 pub use crate::material::*;
 
-pub mod renderer;
-pub use crate::renderer::*;
+pub mod forward_pipeline;
+pub use crate::forward_pipeline::*;
+
+pub mod deferred_pipeline;
+pub use crate::deferred_pipeline::*;
 
 pub mod mesh;
 pub use crate::mesh::*;
