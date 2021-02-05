7ff3bb46ec763a69bac096b157dedf7e4b34a37e
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-02-05 19:01:59 +0100

    Moved phong forward instanced mesh to own file

--- src/phong/forward_instanced_mesh.rs
+++ src/phong/forward_instanced_mesh.rs
@@ -3,43 +3,36 @@ use crate::core::*;
 use crate::objects::*;
 use crate::phong::*;
 
-pub struct PhongForwardMesh {
+pub struct PhongForwardInstancedMesh {
     gl: Context,
     pub name: String,
-    mesh: Mesh,
+    mesh: InstancedMesh,
     pub material: PhongMaterial
 }
 
-impl PhongForwardMesh
+impl PhongForwardInstancedMesh
 {
-    pub fn new(gl: &Context, cpu_mesh: &CPUMesh, material: &PhongMaterial) -> Result<Self, Error>
+    pub fn new(gl: &Context, transformations: &[Mat4], cpu_mesh: &CPUMesh, material: &PhongMaterial) -> Result<Self, Error>
     {
         if cpu_mesh.normals.is_none() {
             Err(Error::FailedToCreateMesh {message:
               "Cannot create a mesh without normals. Consider calling compute_normals on the CPUMesh before creating the mesh.".to_string()})?
         }
+        let mesh = InstancedMesh::new(gl, transformations, cpu_mesh)?;
         unsafe {
-            MESH_COUNT += 1;
+            INSTANCED_MESH_COUNT += 1;
         }
         Ok(Self {
             gl: gl.clone(),
             name: cpu_mesh.name.clone(),
-            mesh: Mesh::new(gl, cpu_mesh)?,
+            mesh,
             material: material.clone()
         })
     }
 
-    pub fn new_meshes(gl: &Context, cpu_meshes: &[CPUMesh], materials: &[PhongMaterial]) -> Result<Vec<Self>, Error>
+    pub fn update_transformations(&mut self, transformations: &[Mat4])
     {
-        let mut meshes = Vec::new();
-        for cpu_mesh in cpu_meshes {
-            let material = cpu_mesh.material_name.as_ref().map(|material_name|
-                materials.iter().filter(|m| &m.name == material_name).last()
-                .map(|m| m.clone()).unwrap_or_else(|| PhongMaterial::default()))
-                .unwrap_or_else(|| PhongMaterial::default());
-            meshes.push(Self::new(gl,cpu_mesh, &material)?);
-        }
-        Ok(meshes)
+        self.mesh.update_transformations(transformations);
     }
 
     pub fn render_depth(&self, render_states: RenderStates, viewport: Viewport, transformation: &Mat4, camera: &camera::Camera) -> Result<(), Error>
@@ -52,20 +45,20 @@ impl PhongForwardMesh
         let program = match self.material.color_source {
             ColorSource::Color(_) => {
                 unsafe {
-                    if PROGRAM_COLOR_AMBIENT.is_none()
+                    if INSTANCED_PROGRAM_COLOR_AMBIENT.is_none()
                     {
-                        PROGRAM_COLOR_AMBIENT = Some(Mesh::create_program(&self.gl, include_str!("shaders/colored_forward_ambient.frag"))?);
+                        INSTANCED_PROGRAM_COLOR_AMBIENT = Some(InstancedMesh::create_program(&self.gl, include_str!("shaders/colored_forward_ambient.frag"))?);
                     }
-                    PROGRAM_COLOR_AMBIENT.as_ref().unwrap()
+                    INSTANCED_PROGRAM_COLOR_AMBIENT.as_ref().unwrap()
                 }
             },
             ColorSource::Texture(_) => {
                 unsafe {
-                    if PROGRAM_TEXTURE_AMBIENT.is_none()
+                    if INSTANCED_PROGRAM_TEXTURE_AMBIENT.is_none()
                     {
-                        PROGRAM_TEXTURE_AMBIENT = Some(Mesh::create_program(&self.gl,include_str!("shaders/textured_forward_ambient.frag"))?);
+                        INSTANCED_PROGRAM_TEXTURE_AMBIENT = Some(InstancedMesh::create_program(&self.gl, include_str!("shaders/textured_forward_ambient.frag"))?);
                     }
-                    PROGRAM_TEXTURE_AMBIENT.as_ref().unwrap()
+                    INSTANCED_PROGRAM_TEXTURE_AMBIENT.as_ref().unwrap()
                 }
             }
         };
@@ -82,7 +75,7 @@ impl PhongForwardMesh
                 program.use_texture(texture.as_ref(),"tex")?;
             }
         }
-        self.mesh.render(program, render_states, viewport,transformation, camera)
+        self.mesh.render(program, render_states, viewport, transformation, camera)
     }
 
     pub fn render_with_ambient_and_directional(&self, render_states: RenderStates, viewport: Viewport, transformation: &Mat4, camera: &camera::Camera, ambient_light: &AmbientLight, directional_light: &DirectionalLight) -> Result<(), Error>
@@ -90,29 +83,28 @@ impl PhongForwardMesh
         let program = match self.material.color_source {
             ColorSource::Color(_) => {
                 unsafe {
-                    if PROGRAM_COLOR_AMBIENT_DIRECTIONAL.is_none()
+                    if INSTANCED_PROGRAM_COLOR_AMBIENT_DIRECTIONAL.is_none()
                     {
-                        PROGRAM_COLOR_AMBIENT_DIRECTIONAL = Some(Mesh::create_program(&self.gl, &format!("{}\n{}",
+                        INSTANCED_PROGRAM_COLOR_AMBIENT_DIRECTIONAL = Some(InstancedMesh::create_program(&self.gl, &format!("{}\n{}",
                                                                                       &include_str!("shaders/light_shared.frag"),
                                                                                       &include_str!("shaders/colored_forward_ambient_directional.frag")))?);
                     }
-                    PROGRAM_COLOR_AMBIENT_DIRECTIONAL.as_ref().unwrap()
+                    INSTANCED_PROGRAM_COLOR_AMBIENT_DIRECTIONAL.as_ref().unwrap()
                 }
             },
             ColorSource::Texture(_) => {
                 unsafe {
-                    if PROGRAM_TEXTURE_AMBIENT_DIRECTIONAL.is_none()
+                    if INSTANCED_PROGRAM_TEXTURE_AMBIENT_DIRECTIONAL.is_none()
                     {
-                        PROGRAM_TEXTURE_AMBIENT_DIRECTIONAL = Some(Mesh::create_program(&self.gl, &format!("{}\n{}",
+                        INSTANCED_PROGRAM_TEXTURE_AMBIENT_DIRECTIONAL = Some(InstancedMesh::create_program(&self.gl, &format!("{}\n{}",
                                                                                     include_str!("shaders/light_shared.frag"),
                                                                                     include_str!("shaders/textured_forward_ambient_directional.frag")))?)
                     }
-                    PROGRAM_TEXTURE_AMBIENT_DIRECTIONAL.as_ref().unwrap()
+                    INSTANCED_PROGRAM_TEXTURE_AMBIENT_DIRECTIONAL.as_ref().unwrap()
                 }
             }
         };
         program.add_uniform_vec3("ambientColor", &(ambient_light.color * ambient_light.intensity))?;
-
         program.add_uniform_vec3("eyePosition", &camera.position())?;
         program.use_texture(directional_light.shadow_map(), "shadowMap")?;
         program.use_uniform_block(directional_light.buffer(), "DirectionalLightUniform");
@@ -136,23 +128,23 @@ impl PhongForwardMesh
     }
 }
 
-impl Drop for PhongForwardMesh {
+impl Drop for PhongForwardInstancedMesh {
 
     fn drop(&mut self) {
         unsafe {
-            MESH_COUNT -= 1;
-            if MESH_COUNT == 0 {
-                PROGRAM_COLOR_AMBIENT = None;
-                PROGRAM_COLOR_AMBIENT_DIRECTIONAL = None;
-                PROGRAM_TEXTURE_AMBIENT = None;
-                PROGRAM_TEXTURE_AMBIENT_DIRECTIONAL = None;
+            INSTANCED_MESH_COUNT -= 1;
+            if INSTANCED_MESH_COUNT == 0 {
+                INSTANCED_PROGRAM_COLOR_AMBIENT = None;
+                INSTANCED_PROGRAM_COLOR_AMBIENT_DIRECTIONAL = None;
+                INSTANCED_PROGRAM_TEXTURE_AMBIENT = None;
+                INSTANCED_PROGRAM_TEXTURE_AMBIENT_DIRECTIONAL = None;
             }
         }
     }
 }
 
-static mut PROGRAM_COLOR_AMBIENT: Option<Program> = None;
-static mut PROGRAM_COLOR_AMBIENT_DIRECTIONAL: Option<Program> = None;
-static mut PROGRAM_TEXTURE_AMBIENT: Option<Program> = None;
-static mut PROGRAM_TEXTURE_AMBIENT_DIRECTIONAL: Option<Program> = None;
-static mut MESH_COUNT: u32 = 0;
+static mut INSTANCED_PROGRAM_COLOR_AMBIENT: Option<Program> = None;
+static mut INSTANCED_PROGRAM_COLOR_AMBIENT_DIRECTIONAL: Option<Program> = None;
+static mut INSTANCED_PROGRAM_TEXTURE_AMBIENT: Option<Program> = None;
+static mut INSTANCED_PROGRAM_TEXTURE_AMBIENT_DIRECTIONAL: Option<Program> = None;
+static mut INSTANCED_MESH_COUNT: u32 = 0;
