0d887de0502ded77025f557921c6dd398b6524aa
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-02-05 19:04:37 +0100

    Moved phong deferred instanced mesh to own file

--- src/phong/deferred_instanced_mesh.rs
+++ src/phong/deferred_instanced_mesh.rs
@@ -3,23 +3,23 @@ use crate::core::*;
 use crate::objects::*;
 use crate::phong::*;
 
-pub struct PhongDeferredMesh {
+pub struct PhongDeferredInstancedMesh {
     gl: Context,
     pub name: String,
-    mesh: Mesh,
+    mesh: InstancedMesh,
     pub material: PhongMaterial
 }
 
-impl PhongDeferredMesh {
-
-    pub fn new(gl: &Context, cpu_mesh: &CPUMesh, material: &PhongMaterial) -> Result<Self, Error>
+impl PhongDeferredInstancedMesh
+{
+    pub fn new(gl: &Context, transformations: &[Mat4], cpu_mesh: &CPUMesh, material: &PhongMaterial) -> Result<Self, Error>
     {
         if cpu_mesh.normals.is_none() {
             Err(Error::FailedToCreateMesh {message:
               "Cannot create a mesh without normals. Consider calling compute_normals on the CPUMesh before creating the mesh.".to_string()})?
         }
-        let mesh = Mesh::new(gl, cpu_mesh)?;
-        unsafe {MESH_COUNT += 1;}
+        let mesh = InstancedMesh::new(gl, transformations, cpu_mesh)?;
+        unsafe {INSTANCED_MESH_COUNT += 1;}
         Ok(Self {
             gl: gl.clone(),
             name: cpu_mesh.name.clone(),
@@ -28,17 +28,9 @@ impl PhongDeferredMesh {
         })
     }
 
-    pub fn new_meshes(gl: &Context, cpu_meshes: &[CPUMesh], materials: &[PhongMaterial]) -> Result<Vec<Self>, Error>
+    pub fn update_transformations(&mut self, transformations: &[Mat4])
     {
-        let mut meshes = Vec::new();
-        for cpu_mesh in cpu_meshes {
-            let material = cpu_mesh.material_name.as_ref().map(|material_name|
-                materials.iter().filter(|m| &m.name == material_name).last()
-                .map(|m| m.clone()).unwrap_or_else(|| PhongMaterial::default()))
-                .unwrap_or_else(|| PhongMaterial::default());
-            meshes.push(Self::new(gl,cpu_mesh, &material)?);
-        }
-        Ok(meshes)
+        self.mesh.update_transformations(transformations);
     }
 
     pub fn render_depth(&self, render_states: RenderStates, viewport: Viewport, transformation: &Mat4, camera: &camera::Camera) -> Result<(), Error>
@@ -51,24 +43,24 @@ impl PhongDeferredMesh {
         let program = match self.material.color_source {
             ColorSource::Color(_) => {
                 unsafe {
-                    if PROGRAM_COLOR.is_none()
+                    if INSTANCED_PROGRAM_COLOR.is_none()
                     {
-                        PROGRAM_COLOR = Some(Mesh::create_program(&self.gl, &format!("{}\n{}",
+                        INSTANCED_PROGRAM_COLOR = Some(InstancedMesh::create_program(&self.gl, &format!("{}\n{}",
                                                              include_str!("shaders/deferred_objects_shared.frag"),
                                                              include_str!("shaders/colored_deferred.frag")))?);
                     }
-                    PROGRAM_COLOR.as_ref().unwrap()
+                    INSTANCED_PROGRAM_COLOR.as_ref().unwrap()
                 }
             },
             ColorSource::Texture(_) => {
                 unsafe {
-                    if PROGRAM_TEXTURE.is_none()
+                    if INSTANCED_PROGRAM_TEXTURE.is_none()
                     {
-                        PROGRAM_TEXTURE = Some(Mesh::create_program(&self.gl, &format!("{}\n{}",
+                        INSTANCED_PROGRAM_TEXTURE = Some(InstancedMesh::create_program(&self.gl, &format!("{}\n{}",
                                                              include_str!("shaders/deferred_objects_shared.frag"),
                                                              include_str!("shaders/textured_deferred.frag")))?);
                     }
-                    PROGRAM_TEXTURE.as_ref().unwrap()
+                    INSTANCED_PROGRAM_TEXTURE.as_ref().unwrap()
                 }
             }
         };
@@ -92,19 +84,23 @@ impl PhongDeferredMesh {
     }
 }
 
-impl Drop for PhongDeferredMesh {
+impl Drop for PhongDeferredInstancedMesh {
 
     fn drop(&mut self) {
         unsafe {
-            MESH_COUNT -= 1;
-            if MESH_COUNT == 0 {
-                PROGRAM_COLOR = None;
-                PROGRAM_TEXTURE = None;
+            INSTANCED_MESH_COUNT -= 1;
+            if INSTANCED_MESH_COUNT == 0 {
+                INSTANCED_PROGRAM_COLOR = None;
+                INSTANCED_PROGRAM_TEXTURE = None;
             }
         }
     }
 }
 
-static mut PROGRAM_COLOR: Option<Program> = None;
-static mut PROGRAM_TEXTURE: Option<Program> = None;
-static mut MESH_COUNT: u32 = 0;
+static mut INSTANCED_PROGRAM_COLOR: Option<Program> = None;
+static mut INSTANCED_PROGRAM_TEXTURE: Option<Program> = None;
+static mut INSTANCED_MESH_COUNT: u32 = 0;
+
+fn bind_material(program: &Program, material: &PhongMaterial, has_uvs: bool) -> Result<(), Error> {
+    Ok(())
+}
