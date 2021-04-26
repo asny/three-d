e7aff86c58640d3f79670ed5c14eeb649a33175f
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-04-26 19:39:59 +0200

    Made Axes cloneable

--- src/object/axes.rs
+++ src/object/axes.rs
@@ -8,6 +8,7 @@ use crate::object::*;
 /// Three arrows indicating the three main axes; the x-axis (red), the y-axis (green) and the z-axis (blue).
 /// Used for easily debugging where objects are placed in the 3D world.
 ///
+#[derive(Clone)]
 pub struct Axes {
     x: Mesh,
     y: Mesh,
