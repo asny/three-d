2ab515af3bc37061f694720266541c9abaea1d60
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-05-02 21:29:06 +0200

    Fix build

--- src/camera/camera.rs
+++ src/camera/camera.rs
@@ -399,7 +399,7 @@ impl Camera {
     ///
     /// Use this buffer in your [Program](crate::Program) like this `program.use_uniform_block(camera.uniform_buffer(), "Camera");` and add the following to your shader code:
     ///
-    /// ```
+    /// ```ignore
     /// layout (std140) uniform Camera
     /// {
     ///     mat4 viewProjection;
