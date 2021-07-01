8c93762be31ad34202d55267a240670ff529671b
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-07-01 21:01:27 +0200

    Improvements to Color type

--- src/math/math.rs
+++ src/math/math.rs
@@ -14,11 +14,11 @@ pub type Point = Point3<f32>;
 pub type Degrees = Deg<f32>;
 pub type Radians = Rad<f32>;
 
-pub fn vec2(x: f32, y: f32) -> Vec2 {
+pub const fn vec2(x: f32, y: f32) -> Vec2 {
     Vector2::new(x, y)
 }
 
-pub fn vec3(x: f32, y: f32, z: f32) -> Vec3 {
+pub const fn vec3(x: f32, y: f32, z: f32) -> Vec3 {
     Vector3::new(x, y, z)
 }
 
