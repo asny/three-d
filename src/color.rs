7cb7367695c6c641fac0077c0549a1b3a491005b
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-07-01 21:01:27 +0200

    Improvements to Color type

--- src/color.rs
+++ src/color.rs
@@ -1,35 +1,43 @@
-use crate::vec4;
-use crate::Vec4;
+use crate::math::*;
 
-/// Newtype for color vec.
+/// Represents a color composed of a red, green and blue component.
+/// In addition, the alpha value determines the how transparent the color is (0 is fully transparent and 255 is fully opaque).
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 pub struct Color {
+    /// Red component
     pub r: u8,
+    /// Green component
     pub g: u8,
+    /// Blue component
     pub b: u8,
+    /// Alpha component
     pub a: u8,
 }
 
 impl Color {
+    /// Opaque red
     pub const RED: Color = Color {
         r: 255,
         g: 0,
         b: 0,
-        a: 0,
+        a: 255,
     };
+    /// Opaque green
     pub const GREEN: Color = Color {
         r: 0,
         g: 255,
         b: 0,
-        a: 0,
+        a: 255,
     };
+    /// Opaque blue
     pub const BLUE: Color = Color {
         r: 0,
         g: 0,
         b: 255,
-        a: 0,
+        a: 255,
     };
 
-    /// Conver to [`Vec4`] by mapping each component to range `0.0..=1.0`.
+    /// Convert to [`Vec4`] by mapping each component to range `0.0..=1.0`.
     pub fn to_vec4(&self) -> Vec4 {
         vec4(
             self.r as f32 / 255.0,
