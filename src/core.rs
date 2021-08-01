717b6f56c11dbb99a85273b3152b36afb7d4860f
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-08-01 09:21:24 +0200

    core::buffer module

--- src/core.rs
+++ src/core.rs
@@ -13,17 +13,8 @@ pub use render_states::*;
 pub mod texture;
 pub use texture::*;
 
-mod element_buffer;
-#[doc(inline)]
-pub use element_buffer::*;
-
-mod vertex_buffer;
-#[doc(inline)]
-pub use vertex_buffer::*;
-
-mod uniform_buffer;
-#[doc(inline)]
-pub use uniform_buffer::*;
+pub mod buffer;
+pub use buffer::*;
 
 mod render_target;
 #[doc(inline)]
---
+++ src/core/buffer.rs
@@ -0,0 +1,11 @@
+mod element_buffer;
+#[doc(inline)]
+pub use element_buffer::*;
+
+mod vertex_buffer;
+#[doc(inline)]
+pub use vertex_buffer::*;
+
+mod uniform_buffer;
+#[doc(inline)]
+pub use uniform_buffer::*;
