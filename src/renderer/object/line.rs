f48feaf5fd1e035dabe2233f9d3fbe2583e4d5fb
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-08-31 10:01:37 +0200

    Use vec2 for pixel positions in line

--- src/renderer/object/line.rs
+++ src/renderer/object/line.rs
@@ -4,18 +4,13 @@ use crate::renderer::*;
 pub struct Line {
     model: Model2D,
     context: Context,
-    pixel0: (f32, f32),
-    pixel1: (f32, f32),
+    pixel0: Vec2,
+    pixel1: Vec2,
     width: f32,
 }
 
 impl Line {
-    pub fn new(
-        context: &Context,
-        pixel0: (f32, f32),
-        pixel1: (f32, f32),
-        width: f32,
-    ) -> Result<Self> {
+    pub fn new(context: &Context, pixel0: Vec2, pixel1: Vec2, width: f32) -> Result<Self> {
         let mut mesh = CPUMesh::square();
         mesh.transform(&(Mat4::from_scale(0.5) * Mat4::from_translation(vec3(1.0, 0.0, 0.0))));
         let mut line = Self {
@@ -29,11 +24,11 @@ impl Line {
         Ok(line)
     }
 
-    pub fn end_point0(&self) -> (f32, f32) {
+    pub fn end_point0(&self) -> Vec2 {
         self.pixel0
     }
 
-    pub fn end_point1(&self) -> (f32, f32) {
+    pub fn end_point1(&self) -> Vec2 {
         self.pixel1
     }
 
@@ -42,7 +37,7 @@ impl Line {
     /// The pixel coordinates must be in physical pixels, where (viewport.x, viewport.y) indicate the top left corner of the viewport
     /// and (viewport.x + viewport.width, viewport.y + viewport.height) indicate the bottom right corner.
     ///
-    pub fn set_endpoints(&mut self, pixel0: (f32, f32), pixel1: (f32, f32)) {
+    pub fn set_endpoints(&mut self, pixel0: Vec2, pixel1: Vec2) {
         self.pixel0 = pixel0;
         self.pixel1 = pixel1;
         self.update();
@@ -54,15 +49,14 @@ impl Line {
     }
 
     fn update(&mut self) {
-        let dx = self.pixel1.0 - self.pixel0.0;
-        let dy = self.pixel1.1 - self.pixel0.1;
+        let dx = self.pixel1.x - self.pixel0.x;
+        let dy = self.pixel1.y - self.pixel0.y;
         let length = (dx * dx + dy * dy).sqrt();
-
         let c = dx / length;
         let s = dy / length;
         let rot = Mat3::new(c, s, 0.0, -s, c, 0.0, 0.0, 0.0, 1.0);
         self.model.set_transformation(
-            Mat3::from_translation(vec2(self.pixel0.0, self.pixel0.1))
+            Mat3::from_translation(self.pixel0)
                 * rot
                 * Mat3::from_nonuniform_scale(length, self.width),
         );
