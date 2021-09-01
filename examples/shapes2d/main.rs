d24afc9b864d1ba5221a553060f66bf6a8398ed8
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-09-01 08:32:32 +0200

    Move line to shapes2d

--- examples/shapes2d/main.rs
+++ examples/shapes2d/main.rs
@@ -14,22 +14,42 @@ fn main() {
     let mut rectangle =
         Rectangle::new(&context, vec2(200.0, 200.0), degrees(45.0), 100.0, 200.0).unwrap();
     let mut circle = Circle::new(&context, vec2(500.0, 500.0), 200.0).unwrap();
+    let mut line = Line::new(
+        &context,
+        vec2(0.0, 0.0),
+        vec2(
+            window.viewport().unwrap().width as f32,
+            window.viewport().unwrap().height as f32,
+        ),
+        5.0,
+    )
+    .unwrap();
+
     window
         .render_loop(move |frame_input: FrameInput| {
             for event in frame_input.events.iter() {
                 match event {
                     Event::MousePress {
-                        button, position, ..
+                        button,
+                        position,
+                        modifiers,
+                        ..
                     } => {
                         let pos = vec2(
                             (frame_input.device_pixel_ratio * position.0) as f32,
                             (frame_input.device_pixel_ratio * position.1) as f32,
                         );
-                        if *button == MouseButton::Left {
+                        if *button == MouseButton::Left && !modifiers.ctrl {
                             rectangle.set_center(pos);
                         }
-                        if *button == MouseButton::Right {
+                        if *button == MouseButton::Right && !modifiers.ctrl {
                             circle.set_center(pos);
+                        }
+                        if *button == MouseButton::Left && modifiers.ctrl {
+                            line.set_endpoints(pos, line.end_point1());
+                        }
+                        if *button == MouseButton::Right && modifiers.ctrl {
+                            line.set_endpoints(line.end_point0(), pos);
                         }
                     }
                     _ => {}
@@ -39,6 +59,7 @@ fn main() {
                 &context,
                 ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0),
                 || {
+                    line.render_with_color(Color::GREEN, frame_input.viewport)?;
                     rectangle.render_with_color(Color::RED, frame_input.viewport)?;
                     circle.render_with_color(Color::BLUE, frame_input.viewport)?;
                     Ok(())
