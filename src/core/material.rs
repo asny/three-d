ebe2f07520d7f90f42adba35e0d01115591dc346
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-09-09 18:22:00 +0200

    CPUMaterial::occlusion_metallic_roughness_texture + docs #131

--- src/core/material.rs
+++ src/core/material.rs
@@ -20,13 +20,23 @@ pub struct CPUMaterial {
     pub metallic: f32,
     /// A value in the range `[0..1]` specifying how rough the material surface is.
     pub roughness: f32,
-    /// Texture containing the metallic and roughness parameters.
+    /// Texture containing the occlusion, metallic and roughness parameters.
+    /// The occlusion values are sampled from the red channel, metallic from the blue channel and the roughness from the green channel.
+    /// Is sometimes in two textures, see [Self::occlusion_texture] and [Self::metallic_roughness_texture].
+    pub occlusion_metallic_roughness_texture: Option<CPUTexture<u8>>,
+    /// Texture containing the metallic and roughness parameters which are multiplied with the [Self::metallic] and [Self::roughness] values in the shader.
+    /// The metallic values are sampled from the blue channel and the roughness from the green channel.
+    /// Can be combined with occlusion into one texture, see [Self::occlusion_metallic_roughness_texture].
     pub metallic_roughness_texture: Option<CPUTexture<u8>>,
-
+    /// A scalar multiplier controlling the amount of occlusion applied from the [Self::occlusion_texture]. A value of 0.0 means no occlusion. A value of 1.0 means full occlusion.
     pub occlusion_strength: f32,
+    /// An occlusion map. Higher values indicate areas that should receive full indirect lighting and lower values indicate no indirect lighting.
+    /// The occlusion values are sampled from the red channel.
+    /// Can be combined with metallic and roughness into one texture, see [Self::occlusion_metallic_roughness_texture].
     pub occlusion_texture: Option<CPUTexture<u8>>,
-
+    /// A scalar multiplier applied to each normal vector of the [Self::normal_texture].
     pub normal_scale: f32,
+    /// A tangent space normal map, also known as bump map.
     pub normal_texture: Option<CPUTexture<u8>>,
 }
 
@@ -36,19 +46,20 @@ impl Default for CPUMaterial {
             name: "default".to_string(),
             albedo: Color::WHITE,
             albedo_texture: None,
+            occlusion_metallic_roughness_texture: None,
             metallic_roughness_texture: None,
+            occlusion_texture: None,
             metallic: 0.0,
             roughness: 1.0,
+            occlusion_strength: 1.0,
             normal_texture: None,
             normal_scale: 1.0,
-            occlusion_texture: None,
-            occlusion_strength: 1.0,
         }
     }
 }
 
 ///
-/// A material used for shading an object.
+/// A GPU-side material used for shading an object.
 ///
 #[derive(Clone)]
 pub struct Material {
@@ -62,19 +73,25 @@ pub struct Material {
     pub metallic: f32,
     /// A value in the range `[0..1]` specifying how rough the material surface is.
     pub roughness: f32,
-    /// Texture containing the metallic and roughness parameters.
+    /// Texture containing the metallic and roughness parameters which are multiplied with the [Self::metallic] and [Self::roughness] values in the shader.
+    /// The metallic values are sampled from the blue channel and the roughness from the green channel.
     pub metallic_roughness_texture: Option<Rc<Texture2D>>,
-
+    /// A scalar multiplier controlling the amount of occlusion applied from the [Self::occlusion_texture]. A value of 0.0 means no occlusion. A value of 1.0 means full occlusion.
     pub occlusion_strength: f32,
+    /// An occlusion map. Higher values indicate areas that should receive full indirect lighting and lower values indicate no indirect lighting.
+    /// The occlusion values are sampled from the red channel.
     pub occlusion_texture: Option<Rc<Texture2D>>,
-
+    /// A scalar multiplier applied to each normal vector of the [Self::normal_texture].
     pub normal_scale: f32,
+    /// A tangent space normal map, also known as bump map.
     pub normal_texture: Option<Rc<Texture2D>>,
 }
 
 impl Material {
     ///
-    /// Constructor.
+    /// Moves the material information from the [CPUMaterial] to the GPU.
+    /// If the input contains an [CPUMaterial::occlusion_metallic_roughness_texture], this texture is used for both
+    /// [Material::metallic_roughness_texture] and [Material::occlusion_texture] while any [CPUMaterial::metallic_roughness_texture] or [CPUMaterial::occlusion_texture] are ignored.
     ///
     pub fn new(context: &Context, cpu_material: &CPUMaterial) -> Result<Self> {
         let albedo_texture = if let Some(ref cpu_texture) = cpu_material.albedo_texture {
@@ -83,17 +100,25 @@ impl Material {
             None
         };
         let metallic_roughness_texture =
-            if let Some(ref cpu_texture) = cpu_material.metallic_roughness_texture {
+            if let Some(ref cpu_texture) = cpu_material.occlusion_metallic_roughness_texture {
+                Some(Rc::new(Texture2D::new(&context, cpu_texture)?))
+            } else {
+                if let Some(ref cpu_texture) = cpu_material.metallic_roughness_texture {
+                    Some(Rc::new(Texture2D::new(&context, cpu_texture)?))
+                } else {
+                    None
+                }
+            };
+        let occlusion_texture = if cpu_material.occlusion_metallic_roughness_texture.is_some() {
+            metallic_roughness_texture.clone()
+        } else {
+            if let Some(ref cpu_texture) = cpu_material.occlusion_texture {
                 Some(Rc::new(Texture2D::new(&context, cpu_texture)?))
             } else {
                 None
-            };
-        let normal_texture = if let Some(ref cpu_texture) = cpu_material.normal_texture {
-            Some(Rc::new(Texture2D::new(&context, cpu_texture)?))
-        } else {
-            None
+            }
         };
-        let occlusion_texture = if let Some(ref cpu_texture) = cpu_material.occlusion_texture {
+        let normal_texture = if let Some(ref cpu_texture) = cpu_material.normal_texture {
             Some(Rc::new(Texture2D::new(&context, cpu_texture)?))
         } else {
             None
