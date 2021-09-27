4bd3d9c74e8605bce20cd4261c87a65e6e70b26e
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-09-27 12:58:11 +0200

    ForwardPipeline render_pass

--- src/renderer/forward_pipeline.rs
+++ src/renderer/forward_pipeline.rs
@@ -22,6 +22,7 @@ impl ForwardPipeline {
     /// Render the objects with the given surface materials and the given set of lights.
     /// Must be called in a render target render function, for example in the callback function of [Screen::write].
     ///
+    #[deprecated = "Use render_pass instead"]
     pub fn light_pass(
         &self,
         camera: &Camera,
@@ -31,28 +32,19 @@ impl ForwardPipeline {
         spot_lights: &[&SpotLight],
         point_lights: &[&PointLight],
     ) -> Result<()> {
-        for (object, material) in objects {
-            if in_frustum(camera, object) {
-                object.render_forward(
-                    *material,
-                    camera,
-                    &Lights {
-                        ambient_light: ambient_light.map(|l| l.clone()),
-                        directional_lights: directional_lights
-                            .iter()
-                            .map(|l| (*l).clone())
-                            .collect(),
-                        spot_lights: spot_lights.iter().map(|l| (*l).clone()).collect(),
-                        point_lights: point_lights.iter().map(|l| (*l).clone()).collect(),
-                    },
-                )?;
-            }
-        }
-
-        Ok(())
+        self.render_pass(
+            camera,
+            objects,
+            &Lights {
+                ambient_light: ambient_light.map(|l| l.clone()),
+                directional_lights: directional_lights.iter().map(|l| (*l).clone()).collect(),
+                spot_lights: spot_lights.iter().map(|l| (*l).clone()).collect(),
+                point_lights: point_lights.iter().map(|l| (*l).clone()).collect(),
+            },
+        )
     }
 
-    pub fn light_pass2(
+    pub fn render_pass(
         &self,
         camera: &Camera,
         objects: &[(&dyn Object, &dyn ForwardMaterial)],
