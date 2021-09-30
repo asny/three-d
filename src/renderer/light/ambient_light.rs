0680ecbdd7ab590b547e92c8501440c055f94cb8
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-09-30 11:12:20 +0200

    impl Light for AmbientLight

--- src/renderer/light/ambient_light.rs
+++ src/renderer/light/ambient_light.rs
@@ -1,4 +1,5 @@
-use crate::Color;
+use crate::core::*;
+use crate::renderer::*;
 
 ///
 /// A light which shines equally on all parts of any surface.
@@ -7,6 +8,26 @@ use crate::Color;
 pub struct AmbientLight {
     pub color: Color,
     pub intensity: f32,
+}
+
+impl Light for AmbientLight {
+    fn shader_source(&self, i: u32) -> String {
+        format!(
+        "
+            uniform vec3 ambientColor{};
+            vec3 calculate_lighting{}(vec3 surface_color, vec3 position, vec3 normal, float metallic, float roughness, float occlusion)
+            {{
+                occlusion * ambientColor * mix(surface_color, vec3(0.0), metallic);
+            }}
+        
+        ", i, i)
+    }
+    fn bind(&self, program: &Program, _camera: &Camera, i: u32) -> Result<()> {
+        program.use_uniform_vec3(
+            &format!("ambientColor{}", i),
+            &(self.color.to_vec3() * self.intensity),
+        )
+    }
 }
 
 impl Default for AmbientLight {
