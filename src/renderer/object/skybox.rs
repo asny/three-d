859142f05d3aacf79dfdc8daba7ac1cdb4943bc1
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-10-12 16:43:01 +0200

    Skybox does not implement Object

--- src/renderer/object/skybox.rs
+++ src/renderer/object/skybox.rs
@@ -46,22 +46,8 @@ impl Skybox {
     pub fn texture(&self) -> &TextureCubeMap {
         &self.texture
     }
-}
 
-impl Cullable for Skybox {
-    fn in_frustum(&self, _camera: &Camera) -> bool {
-        true
-    }
-}
-
-impl Cullable for &Skybox {
-    fn in_frustum(&self, camera: &Camera) -> bool {
-        (*self).in_frustum(camera)
-    }
-}
-
-impl Drawable for Skybox {
-    fn render(&self, camera: &Camera, _lights: &Lights) -> Result<()> {
+    pub fn render(&self, camera: &Camera) -> Result<()> {
         let render_states = RenderStates {
             depth_test: DepthTest::LessOrEqual,
             cull: Cull::Front,
@@ -80,12 +66,3 @@ impl Drawable for Skybox {
         Ok(())
     }
 }
-
-impl Drawable for &Skybox {
-    fn render(&self, camera: &Camera, lights: &Lights) -> Result<()> {
-        (*self).render(camera, lights)
-    }
-}
-
-impl Object for Skybox {}
-impl Object for &Skybox {}
