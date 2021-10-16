5a95ebdd800ba67d01e585875e402e615bfda1b0
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-10-16 20:45:34 +0200

    Fix web examples

--- examples/mandelbrot/src/lib.rs
+++ examples/mandelbrot/src/lib.rs
@@ -3,7 +3,7 @@ include!("../main.rs");
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
-pub fn start() -> ThreeDResult<(), JsValue> {
+pub fn start() -> Result<(), JsValue> {
     console_log::init_with_level(log::Level::Debug).unwrap();
 
     use log::info;
