1dc1f07c62c43fd0bbbabfae172e65dfc2c6fedc
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-10-18 08:19:57 +0200

    Shapes example

---
+++ examples/shapes/main.rs
@@ -0,0 +1,60 @@
+use three_d::*;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    let window = Window::new(WindowSettings {
+        title: "Shapes!".to_string(),
+        max_size: Some((1280, 720)),
+        ..Default::default()
+    })
+    .unwrap();
+    let context = window.gl().unwrap();
+
+    let mut camera = Camera::new_perspective(
+        &context,
+        window.viewport().unwrap(),
+        vec3(5.0, 2.0, 2.5),
+        vec3(0.0, 0.0, -0.5),
+        vec3(0.0, 1.0, 0.0),
+        degrees(45.0),
+        0.1,
+        1000.0,
+    )
+    .unwrap();
+    let mut control = OrbitControl::new(*camera.target(), 1.0, 100.0);
+
+    let sphere = Sphere::new(&context, vec3(0.0, 2.0, 1.0), 1.0).unwrap();
+    let axes = Axes::new(&context, 0.1, 1.0).unwrap();
+
+    window
+        .render_loop(move |mut frame_input: FrameInput| {
+            camera.set_viewport(frame_input.viewport).unwrap();
+            control
+                .handle_events(&mut camera, &mut frame_input.events)
+                .unwrap();
+
+            Screen::write(
+                &context,
+                ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0),
+                || {
+                    sphere.render(&camera, &Lights::default());
+                    axes.render(&camera, &Lights::default());
+                    Ok(())
+                },
+            )
+            .unwrap();
+
+            if args.len() > 1 {
+                // To automatically generate screenshots of the examples, can safely be ignored.
+                FrameOutput {
+                    screenshot: Some(args[1].clone().into()),
+                    exit: true,
+                    ..Default::default()
+                }
+            } else {
+                FrameOutput::default()
+            }
+        })
+        .unwrap();
+}
