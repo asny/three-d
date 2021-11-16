5c1e4faf09d2ce432c55f27a131aa6e69346a6de
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-11-16 17:54:08 +0100

    Use Loading in fog and texture examples

--- examples/fog/main.rs
+++ examples/fog/main.rs
@@ -25,44 +25,18 @@ fn main() {
     .unwrap();
     let mut control = FlyControl::new(0.05);
 
-    Loader::load(
+    // Skybox
+    let skybox = Loading::new_with_context(
+        &context,
         &[
-            "examples/assets/suzanne.obj",
-            "examples/assets/suzanne.mtl",
-            "examples/assets/skybox_evening/back.jpg",
-            "examples/assets/skybox_evening/front.jpg",
-            "examples/assets/skybox_evening/top.jpg",
-            "examples/assets/skybox_evening/left.jpg",
             "examples/assets/skybox_evening/right.jpg",
+            "examples/assets/skybox_evening/left.jpg",
+            "examples/assets/skybox_evening/top.jpg",
+            "examples/assets/skybox_evening/front.jpg",
+            "examples/assets/skybox_evening/back.jpg",
         ],
-        move |mut loaded| {
-            let (meshes, materials) = loaded.obj("examples/assets/suzanne.obj").unwrap();
-            let mut monkey_material = PhysicalMaterial::new(&context, &materials[0]).unwrap();
-            monkey_material.opaque_render_states.cull = Cull::Back;
-            let monkey = Model::new_with_material(&context, &meshes[0], monkey_material).unwrap();
-
-            let lights = Lights {
-                ambient: Some(AmbientLight {
-                    intensity: 0.4,
-                    color: Color::WHITE,
-                }),
-                directional: vec![DirectionalLight::new(
-                    &context,
-                    2.0,
-                    Color::WHITE,
-                    &vec3(-1.0, -1.0, -1.0),
-                )
-                .unwrap()],
-                ..Default::default()
-            };
-
-            // Fog
-            let mut fog_effect = FogEffect::new(&context).unwrap();
-            fog_effect.color = vec3(0.8, 0.8, 0.8);
-            let mut fog_enabled = true;
-
-            // Skybox
-            let skybox = Skybox::new(
+        move |context, mut loaded| {
+            Skybox::new(
                 &context,
                 &mut loaded
                     .cube_image(
@@ -75,63 +49,106 @@ fn main() {
                     )
                     .unwrap(),
             )
-            .unwrap();
-
-            // main loop
-            let mut depth_texture = None;
-            window
-                .render_loop(move |mut frame_input| {
-                    let mut change = frame_input.first_frame;
-                    change |= camera.set_viewport(frame_input.viewport).unwrap();
-                    change |= control
-                        .handle_events(&mut camera, &mut frame_input.events)
-                        .unwrap();
-
-                    for event in frame_input.events.iter() {
-                        match event {
-                            Event::KeyPress { kind, .. } => {
-                                if *kind == Key::F {
-                                    fog_enabled = !fog_enabled;
-                                    change = true;
-                                    println!("Fog: {:?}", fog_enabled);
-                                }
-                            }
-                            _ => {}
-                        }
-                    }
-
-                    // draw
-                    if change && fog_enabled {
-                        depth_texture =
-                            Some(pipeline.depth_pass_texture(&camera, &[&monkey]).unwrap());
-                    }
-
-                    Screen::write(&context, ClearState::default(), || {
-                        monkey.render(&camera, &lights)?;
-                        skybox.render(&camera)?;
-                        if fog_enabled {
-                            fog_effect.apply(
-                                &camera,
-                                depth_texture.as_ref().unwrap(),
-                                frame_input.accumulated_time as f32,
-                            )?;
-                        }
-                        Ok(())
-                    })
-                    .unwrap();
-
-                    if args.len() > 1 {
-                        // To automatically generate screenshots of the examples, can safely be ignored.
-                        FrameOutput {
-                            screenshot: Some(args[1].clone().into()),
-                            exit: true,
-                            ..Default::default()
-                        }
-                    } else {
-                        FrameOutput::default()
-                    }
-                })
-                .unwrap();
+            .unwrap()
         },
     );
+
+    let monkey = Loading::new_with_context(
+        &context,
+        &["examples/assets/suzanne.obj", "examples/assets/suzanne.mtl"],
+        move |context, mut loaded| {
+            let (meshes, materials) = loaded.obj("examples/assets/suzanne.obj").unwrap();
+            let mut monkey_material = PhysicalMaterial::new(&context, &materials[0]).unwrap();
+            monkey_material.opaque_render_states.cull = Cull::Back;
+            Model::new_with_material(&context, &meshes[0], monkey_material).unwrap()
+        },
+    );
+
+    let lights = Lights {
+        ambient: Some(AmbientLight {
+            intensity: 0.4,
+            color: Color::WHITE,
+        }),
+        directional: vec![DirectionalLight::new(
+            &context,
+            2.0,
+            Color::WHITE,
+            &vec3(-1.0, -1.0, -1.0),
+        )
+        .unwrap()],
+        ..Default::default()
+    };
+
+    // Fog
+    let mut fog_effect = FogEffect::new(&context).unwrap();
+    fog_effect.color = vec3(0.8, 0.8, 0.8);
+    let mut fog_enabled = true;
+
+    // main loop
+    let mut depth_texture = None;
+    let mut loaded = false;
+    window
+        .render_loop(move |mut frame_input| {
+            let mut change = frame_input.first_frame;
+            change |= camera.set_viewport(frame_input.viewport).unwrap();
+            change |= control
+                .handle_events(&mut camera, &mut frame_input.events)
+                .unwrap();
+            if !loaded && monkey.is_loaded() && skybox.is_loaded() {
+                change = true;
+                loaded = true;
+            }
+
+            for event in frame_input.events.iter() {
+                match event {
+                    Event::KeyPress { kind, .. } => {
+                        if *kind == Key::F {
+                            fog_enabled = !fog_enabled;
+                            change = true;
+                            println!("Fog: {:?}", fog_enabled);
+                        }
+                    }
+                    _ => {}
+                }
+            }
+
+            // draw
+            if change && fog_enabled {
+                if let Some(ref monkey) = *monkey.borrow() {
+                    depth_texture = Some(pipeline.depth_pass_texture(&camera, &[monkey]).unwrap());
+                }
+            }
+
+            Screen::write(&context, ClearState::default(), || {
+                if let Some(ref monkey) = *monkey.borrow() {
+                    monkey.render(&camera, &lights)?;
+                }
+                if let Some(ref skybox) = *skybox.borrow() {
+                    skybox.render(&camera)?;
+                }
+                if fog_enabled {
+                    if let Some(ref depth_texture) = depth_texture {
+                        fog_effect.apply(
+                            &camera,
+                            depth_texture,
+                            frame_input.accumulated_time as f32,
+                        )?;
+                    }
+                }
+                Ok(())
+            })
+            .unwrap();
+
+            if args.len() > 1 {
+                // To automatically generate screenshots of the examples, can safely be ignored.
+                FrameOutput {
+                    screenshot: Some(args[1].clone().into()),
+                    exit: true,
+                    ..Default::default()
+                }
+            } else {
+                FrameOutput::default()
+            }
+        })
+        .unwrap();
 }
--- examples/texture/main.rs
+++ examples/texture/main.rs
@@ -25,53 +25,49 @@ fn main() {
     .unwrap();
     let mut control = OrbitControl::new(*camera.target(), 1.0, 100.0);
 
-    Loader::load(
+    let skybox = Loading::new_with_context(
+        &context,
         &[
+            "examples/assets/skybox_evening/right.jpg",
+            "examples/assets/skybox_evening/left.jpg",
+            "examples/assets/skybox_evening/top.jpg",
+            "examples/assets/skybox_evening/front.jpg",
+            "examples/assets/skybox_evening/back.jpg",
+        ],
+        move |context, mut loaded| {
+            Skybox::new(
+                &context,
+                &mut loaded
+                    .cube_image("right", "left", "top", "top", "front", "back")
+                    .unwrap(),
+            )
+            .unwrap()
+        },
+    );
+
+    let objects = Loading::new_with_context(
+        &context,
+        &[
+            "examples/assets/test_texture.jpg",
             "examples/assets/PenguinBaseMesh.obj",
             "examples/assets/PenguinBaseMesh.mtl",
             "examples/assets/penguin.png",
-            "examples/assets/test_texture.jpg",
-            "examples/assets/skybox_evening/back.jpg",
-            "examples/assets/skybox_evening/front.jpg",
-            "examples/assets/skybox_evening/top.jpg",
-            "examples/assets/skybox_evening/left.jpg",
-            "examples/assets/skybox_evening/right.jpg",
         ],
-        move |mut loaded| {
+        move |context, mut loaded| {
             let mut box_object = Model::new_with_material(
                 &context,
                 &CPUMesh::cube(),
                 ColorMaterial {
                     texture: Some(std::rc::Rc::new(
-                        Texture2D::new(
-                            &context,
-                            &loaded.image("examples/assets/test_texture.jpg").unwrap(),
-                        )
-                        .unwrap(),
+                        Texture2D::new(&context, &loaded.image("test_texture").unwrap()).unwrap(),
                     )),
                     ..Default::default()
                 },
             )
             .unwrap();
             box_object.material.opaque_render_states.cull = Cull::Back;
-
-            let skybox = Skybox::new(
-                &context,
-                &mut loaded
-                    .cube_image(
-                        "examples/assets/skybox_evening/right.jpg",
-                        "examples/assets/skybox_evening/left.jpg",
-                        "examples/assets/skybox_evening/top.jpg",
-                        "examples/assets/skybox_evening/top.jpg",
-                        "examples/assets/skybox_evening/front.jpg",
-                        "examples/assets/skybox_evening/back.jpg",
-                    )
-                    .unwrap(),
-            )
-            .unwrap();
-
             let (penguin_cpu_meshes, penguin_cpu_materials) =
-                loaded.obj("examples/assets/PenguinBaseMesh.obj").unwrap();
+                loaded.obj("PenguinBaseMesh.obj").unwrap();
             let mut penguin_object = Model::new_with_material(
                 &context,
                 &penguin_cpu_meshes[0],
@@ -80,61 +76,71 @@ fn main() {
             .unwrap();
             penguin_object.set_transformation(Mat4::from_translation(vec3(0.0, 1.0, 0.5)));
             penguin_object.material.opaque_render_states.cull = Cull::Back;
-
-            let lights = Lights {
-                ambient: Some(AmbientLight {
-                    intensity: 0.4,
-                    color: Color::WHITE,
-                }),
-                directional: vec![DirectionalLight::new(
-                    &context,
-                    2.0,
-                    Color::WHITE,
-                    &vec3(0.0, -1.0, -1.0),
-                )
-                .unwrap()],
-                ..Default::default()
-            };
-
-            // main loop
-            window
-                .render_loop(move |mut frame_input| {
-                    let mut redraw = frame_input.first_frame;
-                    redraw |= camera.set_viewport(frame_input.viewport).unwrap();
-                    redraw |= control
-                        .handle_events(&mut camera, &mut frame_input.events)
-                        .unwrap();
-
-                    // draw
-                    if redraw {
-                        Screen::write(&context, ClearState::default(), || {
-                            pipeline.render_pass(
-                                &camera,
-                                &[&box_object as &dyn Object, &penguin_object],
-                                &lights,
-                            )?;
-                            skybox.render(&camera)?;
-                            Ok(())
-                        })
-                        .unwrap();
-                    }
-
-                    if args.len() > 1 {
-                        // To automatically generate screenshots of the examples, can safely be ignored.
-                        FrameOutput {
-                            screenshot: Some(args[1].clone().into()),
-                            exit: true,
-                            ..Default::default()
-                        }
-                    } else {
-                        FrameOutput {
-                            swap_buffers: redraw,
-                            wait_next_event: true,
-                            ..Default::default()
-                        }
-                    }
-                })
-                .unwrap();
+            (box_object, penguin_object)
         },
     );
+
+    let lights = Lights {
+        ambient: Some(AmbientLight {
+            intensity: 0.4,
+            color: Color::WHITE,
+        }),
+        directional: vec![DirectionalLight::new(
+            &context,
+            2.0,
+            Color::WHITE,
+            &vec3(0.0, -1.0, -1.0),
+        )
+        .unwrap()],
+        ..Default::default()
+    };
+
+    // main loop
+    let mut loaded = false;
+    window
+        .render_loop(move |mut frame_input| {
+            let mut redraw = frame_input.first_frame;
+            redraw |= camera.set_viewport(frame_input.viewport).unwrap();
+            redraw |= control
+                .handle_events(&mut camera, &mut frame_input.events)
+                .unwrap();
+            if !loaded && objects.is_loaded() && skybox.is_loaded() {
+                redraw = true;
+                loaded = true;
+            }
+
+            // draw
+            if redraw {
+                Screen::write(&context, ClearState::default(), || {
+                    if let Some((ref box_object, ref penguin_object)) = *objects.borrow() {
+                        pipeline.render_pass(
+                            &camera,
+                            &[box_object as &dyn Object, penguin_object],
+                            &lights,
+                        )?;
+                    }
+                    if let Some(ref skybox) = *skybox.borrow() {
+                        skybox.render(&camera)?;
+                    }
+                    Ok(())
+                })
+                .unwrap();
+            }
+
+            if args.len() > 1 {
+                // To automatically generate screenshots of the examples, can safely be ignored.
+                FrameOutput {
+                    screenshot: Some(args[1].clone().into()),
+                    exit: true,
+                    ..Default::default()
+                }
+            } else {
+                FrameOutput {
+                    swap_buffers: redraw,
+                    wait_next_event: true,
+                    ..Default::default()
+                }
+            }
+        })
+        .unwrap();
 }
