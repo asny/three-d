a9a8e0285ce73b6b1813ef92013a0983640d841f
Ranzear <Ranzear@gmail.com>
2021-12-02 22:37:50 -0800

    Support borderless mode for both window or maximized
    Centering an unmaximized borderless window will require a monitorhandle from glutin. Will leave for later when individual monitors can be exposed and selected.

--- src/window/settings.rs
+++ src/window/settings.rs
@@ -20,6 +20,10 @@ pub struct WindowSettings {
     /// On web, this can only be off (0) or on (>0).
     /// The actual number of samples depends on browser settings.
     pub multisamples: u8,
+    /// Borderless mode.
+    ///
+    /// No effect on web.
+    pub borderless: bool,
 }
 impl Default for WindowSettings {
     fn default() -> Self {
@@ -29,6 +33,7 @@ impl Default for WindowSettings {
             max_size: None,
             vsync: true,
             multisamples: 4,
+            borderless: false,
         }
     }
 }
