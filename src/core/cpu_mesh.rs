4ca8711232cc240a498f714d4d213c2a7d37c47c
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-12-03 16:55:06 +0100

    Simplified compute normal

--- src/core/cpu_mesh.rs
+++ src/core/cpu_mesh.rs
@@ -391,12 +391,42 @@ impl CPUMesh {
     /// It will override the current normals if they already exist.
     ///
     pub fn compute_normals(&mut self) {
-        self.normals = Some(
-            self.indices
-                .as_ref()
-                .map(|indices| compute_normals_with_indices(&indices.into_u32(), &self.positions))
-                .unwrap_or_else(|| compute_normals(&self.positions)),
-        );
+        let mut normals = vec![0.0f32; self.positions.len()];
+        let mut handle_triangle = |i0, i1, i2| {
+            let p0 = self.position(i0);
+            let p1 = self.position(i1);
+            let p2 = self.position(i2);
+            let normal = (p1 - p0).cross(p2 - p0);
+            normals[i0 * 3] += normal.x;
+            normals[i0 * 3 + 1] += normal.y;
+            normals[i0 * 3 + 2] += normal.z;
+            normals[i1 * 3] += normal.x;
+            normals[i1 * 3 + 1] += normal.y;
+            normals[i1 * 3 + 2] += normal.z;
+            normals[i2 * 3] += normal.x;
+            normals[i2 * 3 + 1] += normal.y;
+            normals[i2 * 3 + 2] += normal.z;
+        };
+        if let Some(ref indices) = self.indices {
+            let indices = indices.into_u32();
+            for face in 0..indices.len() / 3 {
+                let index0 = indices[face * 3] as usize;
+                let index1 = indices[face * 3 + 1] as usize;
+                let index2 = indices[face * 3 + 2] as usize;
+                handle_triangle(index0, index1, index2);
+            }
+        } else {
+            for face in 0..self.positions.len() / 9 {
+                handle_triangle(face * 3, face * 3 + 1, face * 3 + 2);
+            }
+        }
+        for i in 0..normals.len() / 3 {
+            let normal = vec3(normals[3 * i], normals[3 * i + 1], normals[3 * i + 2]).normalize();
+            normals[3 * i] = normal.x;
+            normals[3 * i + 1] = normal.y;
+            normals[3 * i + 2] = normal.z;
+        }
+        self.normals = Some(normals);
     }
 
     ///
@@ -586,90 +616,4 @@ impl CPUMesh {
         };
         Ok(())
     }
-}
-
-fn compute_normals_with_indices(indices: &[u32], positions: &[f32]) -> Vec<f32> {
-    let mut normals = vec![0.0f32; positions.len()];
-    for face in 0..indices.len() / 3 {
-        let index0 = indices[face * 3] as usize;
-        let p0 = vec3(
-            positions[index0 * 3],
-            positions[index0 * 3 + 1],
-            positions[index0 * 3 + 2],
-        );
-        let index1 = indices[face * 3 + 1] as usize;
-        let p1 = vec3(
-            positions[index1 * 3],
-            positions[index1 * 3 + 1],
-            positions[index1 * 3 + 2],
-        );
-        let index2 = indices[face * 3 + 2] as usize;
-        let p2 = vec3(
-            positions[index2 * 3],
-            positions[index2 * 3 + 1],
-            positions[index2 * 3 + 2],
-        );
-
-        let normal = (p1 - p0).cross(p2 - p0);
-        normals[index0 * 3] += normal.x;
-        normals[index0 * 3 + 1] += normal.y;
-        normals[index0 * 3 + 2] += normal.z;
-        normals[index1 * 3] += normal.x;
-        normals[index1 * 3 + 1] += normal.y;
-        normals[index1 * 3 + 2] += normal.z;
-        normals[index2 * 3] += normal.x;
-        normals[index2 * 3 + 1] += normal.y;
-        normals[index2 * 3 + 2] += normal.z;
-    }
-
-    for i in 0..normals.len() / 3 {
-        let normal = vec3(normals[3 * i], normals[3 * i + 1], normals[3 * i + 2]).normalize();
-        normals[3 * i] = normal.x;
-        normals[3 * i + 1] = normal.y;
-        normals[3 * i + 2] = normal.z;
-    }
-    normals
-}
-
-fn compute_normals(positions: &[f32]) -> Vec<f32> {
-    let mut normals = vec![0.0f32; positions.len()];
-    for face in 0..positions.len() / 9 {
-        let index0 = face * 3 as usize;
-        let p0 = vec3(
-            positions[index0 * 3],
-            positions[index0 * 3 + 1],
-            positions[index0 * 3 + 2],
-        );
-        let index1 = face * 3 + 1 as usize;
-        let p1 = vec3(
-            positions[index1 * 3],
-            positions[index1 * 3 + 1],
-            positions[index1 * 3 + 2],
-        );
-        let index2 = face * 3 + 2 as usize;
-        let p2 = vec3(
-            positions[index2 * 3],
-            positions[index2 * 3 + 1],
-            positions[index2 * 3 + 2],
-        );
-
-        let normal = (p1 - p0).cross(p2 - p0);
-        normals[index0 * 3] += normal.x;
-        normals[index0 * 3 + 1] += normal.y;
-        normals[index0 * 3 + 2] += normal.z;
-        normals[index1 * 3] += normal.x;
-        normals[index1 * 3 + 1] += normal.y;
-        normals[index1 * 3 + 2] += normal.z;
-        normals[index2 * 3] += normal.x;
-        normals[index2 * 3 + 1] += normal.y;
-        normals[index2 * 3 + 2] += normal.z;
-    }
-
-    for i in 0..normals.len() / 3 {
-        let normal = vec3(normals[3 * i], normals[3 * i + 1], normals[3 * i + 2]).normalize();
-        normals[3 * i] = normal.x;
-        normals[3 * i + 1] = normal.y;
-        normals[3 * i + 2] = normal.z;
-    }
-    normals
 }
