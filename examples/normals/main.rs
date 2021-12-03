8ee32f02b9f2eea2a220fb6fd5326281828b5ad2
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-12-03 15:39:10 +0100

    Normal test with loaded tangents

--- examples/normals/main.rs
+++ examples/normals/main.rs
@@ -12,14 +12,14 @@ fn main() {
     .unwrap();
     let context = window.gl().unwrap();
 
+    // Model source: https://github.com/KhronosGroup/glTF-Sample-Models/tree/master/2.0/
     let model = Loading::new(
         &context,
-        &["examples/assets/gltf/NormalTangentTest.glb"],
+        &["examples/assets/gltf/NormalTangentMirrorTest.glb"],
         move |context, mut loaded| {
-            let (mut cpu_meshes, cpu_materials) = loaded.gltf("NormalTangentTest.glb").unwrap();
+            let (mut cpu_meshes, cpu_materials) =
+                loaded.gltf("NormalTangentMirrorTest.glb").unwrap();
             let mut material = PhysicalMaterial::new(&context, &cpu_materials[0]).unwrap();
-            material.opaque_render_states.cull = Cull::Back;
-            cpu_meshes[0].compute_tangents();
             let mut model =
                 Model::new_with_material(&context, &cpu_meshes[0], material.clone()).unwrap();
             Ok(model)
@@ -43,10 +43,13 @@ fn main() {
             color: Color::WHITE,
             intensity: 0.4,
         }),
-        directional: vec![
-            DirectionalLight::new(&context, 2.0, Color::WHITE, &vec3(0.0, -1.0, 0.0)).unwrap(),
-            DirectionalLight::new(&context, 2.0, Color::WHITE, &vec3(0.0, -1.0, 0.0)).unwrap(),
-        ],
+        directional: vec![DirectionalLight::new(
+            &context,
+            2.0,
+            Color::WHITE,
+            &vec3(0.0, -1.0, 0.0),
+        )
+        .unwrap()],
         lighting_model: LightingModel::Cook(
             NormalDistributionFunction::TrowbridgeReitzGGX,
             GeometryFunction::SmithSchlickGGX,
@@ -62,31 +65,13 @@ fn main() {
                 .handle_events(&mut camera, &mut frame_input.events)
                 .unwrap();
 
-            let time = 0.001 * frame_input.accumulated_time;
-            let c = time.cos() as f32;
-            let s = time.sin() as f32;
-            lights.directional[0].set_direction(&vec3(-1.0 - c, -1.0, 1.0 + s));
-            lights.directional[1].set_direction(&vec3(1.0 + c, -1.0, -1.0 - s));
-
             // Draw
-            if let Some(Ok(ref model)) = *model.borrow() {
-                lights.directional[0]
-                    .generate_shadow_map(2.0, 1024, 1024, &[model])
-                    .unwrap();
-                lights.directional[1]
-                    .generate_shadow_map(2.0, 1024, 1024, &[model])
-                    .unwrap();
-            }
             Screen::write(
                 &context,
                 ClearState::color_and_depth(0.5, 0.5, 0.5, 1.0, 1.0),
                 || {
                     if let Some(Ok(ref model)) = *model.borrow() {
-                        model.render_forward(
-                            &NormalMaterial::from_physical_material(&model.material),
-                            &camera,
-                            &lights,
-                        )?;
+                        model.render(&camera, &lights)?;
                     }
                     Ok(())
                 },
