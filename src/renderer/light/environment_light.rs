efabdc637f9ea9a56e13be1d7cada8ef96015b84
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-12-15 17:50:50 +0100

    Environment light

--- src/renderer/light/environment_light.rs
+++ src/renderer/light/environment_light.rs
@@ -5,17 +5,84 @@ use crate::renderer::*;
 /// A light which shines equally on all parts of any surface.
 ///
 pub struct EnvironmentLight {
-    irradiance_map: TextureCubeMap,
+    irradiance_map: ColorTargetTextureCubeMap<f32>,
 }
 
 impl EnvironmentLight {
-    pub fn new<T: TextureDataType>(
-        context: &Context,
-        cpu_texture: &CPUTexture<T>,
-    ) -> ThreeDResult<Self> {
-        Ok(Self {
-            irradiance_map: TextureCubeMap::new(context, cpu_texture)?,
-        })
+    pub fn new(context: &Context, environment_map: &impl TextureCube) -> ThreeDResult<Self> {
+        let program = Program::from_source(
+            context,
+            include_str!("shaders/irradiance.vert"),
+            include_str!("shaders/irradiance.frag"),
+        )?;
+        let vertex_buffer = VertexBuffer::new_with_static(context, &CPUMesh::cube().positions)?;
+        let irradiance_map = ColorTargetTextureCubeMap::new(
+            &context,
+            512,
+            512,
+            Interpolation::Linear,
+            Interpolation::Linear,
+            None,
+            Wrapping::ClampToEdge,
+            Wrapping::ClampToEdge,
+            Wrapping::ClampToEdge,
+            Format::RGB,
+        )?;
+
+        let mut camera = Camera::new_perspective(
+            context,
+            Viewport::new_at_origo(512, 512),
+            vec3(0.0, 0.0, 0.0),
+            vec3(0.0, 0.0, -1.0),
+            vec3(0.0, 1.0, 0.0),
+            degrees(90.0),
+            0.1,
+            10.0,
+        )?;
+        for i in 0..6 {
+            match i {
+                0 => camera.set_view(
+                    vec3(0.0, 0.0, 0.0),
+                    vec3(1.0, 0.0, 0.0),
+                    vec3(0.0, -1.0, 0.0),
+                ),
+                1 => camera.set_view(
+                    vec3(0.0, 0.0, 0.0),
+                    vec3(-1.0, 0.0, 0.0),
+                    vec3(0.0, -1.0, 0.0),
+                ),
+                2 => camera.set_view(
+                    vec3(0.0, 0.0, 0.0),
+                    vec3(0.0, 1.0, 0.0),
+                    vec3(0.0, 0.0, 1.0),
+                ),
+                3 => camera.set_view(
+                    vec3(0.0, 0.0, 0.0),
+                    vec3(0.0, -1.0, 0.0),
+                    vec3(0.0, 0.0, -1.0),
+                ),
+                4 => camera.set_view(
+                    vec3(0.0, 0.0, 0.0),
+                    vec3(0.0, 0.0, 1.0),
+                    vec3(0.0, -1.0, 0.0),
+                ),
+                5 => camera.set_view(
+                    vec3(0.0, 0.0, 0.0),
+                    vec3(0.0, 0.0, -1.0),
+                    vec3(0.0, -1.0, 0.0),
+                ),
+                _ => unreachable!(),
+            }?;
+
+            program.use_uniform_block("Camera", camera.uniform_buffer());
+            program.use_texture_cube("environmentMap", environment_map)?;
+            program.use_attribute_vec3("position", &vertex_buffer)?;
+            irradiance_map.write(&[i], ClearState::default(), || {
+                program.draw_arrays(RenderStates::default(), camera.viewport(), 36);
+                Ok(())
+            })?;
+        }
+        Ok(Self { irradiance_map })
     }
 }
 
