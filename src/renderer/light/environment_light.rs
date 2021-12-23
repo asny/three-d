ab978a18562575b5998ead47c52da856fd2da398
Asger Nyman Christiansen <asgernyman@gmail.com>
2021-12-23 14:43:44 +0100

    Use write_to_all

--- src/renderer/light/environment_light.rs
+++ src/renderer/light/environment_light.rs
@@ -3,28 +3,12 @@ use crate::renderer::*;
 
 pub struct EnvironmentLight {
     irradiance_map: ColorTargetTextureCubeMap<f32>,
+    prefilter_map: ColorTargetTextureCubeMap<f32>,
 }
 
 impl EnvironmentLight {
     pub fn new(context: &Context, environment_map: &impl TextureCube) -> ThreeDResult<Self> {
-        let vertex_buffer = VertexBuffer::new_with_static(context, &CPUMesh::cube().positions)?;
-        let mut camera = Camera::new_perspective(
-            context,
-            Viewport::new_at_origo(1, 1),
-            vec3(0.0, 0.0, 0.0),
-            vec3(0.0, 0.0, -1.0),
-            vec3(0.0, 1.0, 0.0),
-            degrees(90.0),
-            0.1,
-            10.0,
-        )?;
-
         // Diffuse
-        let program = Program::from_source(
-            context,
-            include_str!("shaders/cubemap.vert"),
-            include_str!("shaders/irradiance.frag"),
-        )?;
         let irradiance_map = ColorTargetTextureCubeMap::new(
             context,
             32,
@@ -32,74 +16,48 @@ impl EnvironmentLight {
             Interpolation::Linear,
             Interpolation::Linear,
             Some(Interpolation::Linear),
-            Wrapping::Repeat,
-            Wrapping::Repeat,
-            Wrapping::Repeat,
+            Wrapping::ClampToEdge,
+            Wrapping::ClampToEdge,
+            Wrapping::ClampToEdge,
             Format::RGBA,
         )?;
-        Self::render_to_cube(
-            &program,
-            &irradiance_map,
-            &vertex_buffer,
-            &mut camera,
-            || program.use_texture_cube("environmentMap", environment_map),
+        irradiance_map.write_to_all(
+            ClearState::default(),
+            &format!(
+                "{}{}",
+                include_str!("../../core/shared.frag"),
+                include_str!("shaders/irradiance.frag")
+            ),
+            |program| program.use_texture_cube("environmentMap", environment_map),
         )?;
 
-        Ok(Self { irradiance_map })
-    }
+        // Prefilter
+        let prefilter_map = ColorTargetTextureCubeMap::new(
+            context,
+            32,
+            32,
+            Interpolation::Linear,
+            Interpolation::Linear,
+            Some(Interpolation::Linear),
+            Wrapping::ClampToEdge,
+            Wrapping::ClampToEdge,
+            Wrapping::ClampToEdge,
+            Format::RGBA,
+        )?;
+        prefilter_map.write_to_all(
+            ClearState::default(),
+            &format!(
+                "{}{}",
+                include_str!("../../core/shared.frag"),
+                include_str!("shaders/prefilter.frag")
+            ),
+            |program| program.use_texture_cube("environmentMap", environment_map),
+        )?;
 
-    fn render_to_cube<T: TextureDataType>(
-        program: &Program,
-        target: &ColorTargetTextureCubeMap<T>,
-        vertex_buffer: &VertexBuffer,
-        camera: &mut Camera,
-        use_uniforms: impl Fn() -> ThreeDResult<()>,
-    ) -> ThreeDResult<()> {
-        camera.set_viewport(Viewport::new_at_origo(target.width(), target.height()))?;
-        for i in 0..6 {
-            match i {
-                0 => camera.set_view(
-                    vec3(0.0, 0.0, 0.0),
-                    vec3(1.0, 0.0, 0.0),
-                    vec3(0.0, -1.0, 0.0),
-                ),
-                1 => camera.set_view(
-                    vec3(0.0, 0.0, 0.0),
-                    vec3(-1.0, 0.0, 0.0),
-                    vec3(0.0, -1.0, 0.0),
-                ),
-                2 => camera.set_view(
-                    vec3(0.0, 0.0, 0.0),
-                    vec3(0.0, 1.0, 0.0),
-                    vec3(0.0, 0.0, 1.0),
-                ),
-                3 => camera.set_view(
-                    vec3(0.0, 0.0, 0.0),
-                    vec3(0.0, -1.0, 0.0),
-                    vec3(0.0, 0.0, -1.0),
-                ),
-                4 => camera.set_view(
-                    vec3(0.0, 0.0, 0.0),
-                    vec3(0.0, 0.0, 1.0),
-                    vec3(0.0, -1.0, 0.0),
-                ),
-                5 => camera.set_view(
-                    vec3(0.0, 0.0, 0.0),
-                    vec3(0.0, 0.0, -1.0),
-                    vec3(0.0, -1.0, 0.0),
-                ),
-                _ => unreachable!(),
-            }?;
-
-            program.use_uniform_block("Camera", camera.uniform_buffer());
-            program.use_attribute_vec3("position", &vertex_buffer)?;
-            use_uniforms()?;
-            target.write(i, ClearState::default(), || {
-                program.draw_arrays(RenderStates::default(), camera.viewport(), 36);
-                Ok(())
-            })?;
-        }
-        Ok(())
+        Ok(Self {
+            irradiance_map,
+            prefilter_map,
+        })
     }
 }
 
