d7e2da6fc3677b0ce7fa237a2f34926b570e686a
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-01-11 12:41:17 +0100

    Avoid uniform buffers in directional light

--- src/renderer/light/directional_light.rs
+++ src/renderer/light/directional_light.rs
@@ -8,8 +8,11 @@ use crate::renderer::*;
 ///
 pub struct DirectionalLight {
     context: Context,
-    light_buffer: UniformBuffer,
     shadow_texture: Option<DepthTargetTexture2D>,
+    shadow_matrix: Mat4,
+    pub intensity: f32,
+    pub color: Color,
+    pub direction: Vec3,
 }
 
 impl DirectionalLight {
@@ -19,49 +22,18 @@ impl DirectionalLight {
         color: Color,
         direction: &Vec3,
     ) -> ThreeDResult<DirectionalLight> {
-        let mut light = DirectionalLight {
+        Ok(DirectionalLight {
             context: context.clone(),
-            light_buffer: UniformBuffer::new(context, &[3u32, 1, 3, 1, 16])?,
+            shadow_matrix: Mat4::identity(),
             shadow_texture: None,
-        };
-
-        light.set_intensity(intensity);
-        light.set_color(color);
-        light.set_direction(direction);
-        Ok(light)
-    }
-
-    pub fn set_color(&mut self, color: Color) {
-        self.light_buffer.update(0, &color.to_rgb_slice()).unwrap();
-    }
-
-    pub fn color(&self) -> Color {
-        let c = self.light_buffer.get(0).unwrap();
-        Color::from_rgb_slice(&[c[0], c[1], c[2]])
-    }
-
-    pub fn set_intensity(&mut self, intensity: f32) {
-        self.light_buffer.update(1, &[intensity]).unwrap();
-    }
-
-    pub fn intensity(&self) -> f32 {
-        self.light_buffer.get(1).unwrap()[0]
-    }
-
-    pub fn set_direction(&mut self, direction: &Vec3) {
-        self.light_buffer
-            .update(2, &direction.normalize().to_slice())
-            .unwrap();
-    }
-
-    pub fn direction(&self) -> Vec3 {
-        let d = self.light_buffer.get(2).unwrap();
-        vec3(d[0], d[1], d[2])
+            intensity,
+            color,
+            direction: *direction,
+        })
     }
 
     pub fn clear_shadow_map(&mut self) {
         self.shadow_texture = None;
-        self.light_buffer.update(3, &[0.0]).unwrap();
     }
 
     pub fn generate_shadow_map(
@@ -71,8 +43,7 @@ impl DirectionalLight {
         texture_height: u32,
         geometries: &[impl Geometry],
     ) -> ThreeDResult<()> {
-        let direction = self.direction();
-        let up = compute_up_direction(direction);
+        let up = compute_up_direction(self.direction);
 
         let viewport = Viewport::new_at_origo(texture_width, texture_height);
         let mut aabb = AxisAlignedBoundingBox::EMPTY;
@@ -83,7 +54,7 @@ impl DirectionalLight {
             return Ok(());
         }
         let target = aabb.center();
-        let position = target - direction * aabb.max().distance(aabb.min());
+        let position = target - self.direction * aabb.max().distance(aabb.min());
         let z_far = aabb.distance_max(&position);
         let z_near = aabb.distance(&position);
         let shadow_camera = Camera::new_orthographic(
@@ -96,8 +67,6 @@ impl DirectionalLight {
             z_near,
             z_far,
         )?;
-        self.light_buffer
-            .update(4, &shadow_matrix(&shadow_camera).to_slice())?;
 
         let mut shadow_texture = DepthTargetTexture2D::new(
             &self.context,
@@ -128,16 +97,11 @@ impl DirectionalLight {
             Ok(())
         })?;
         self.shadow_texture = Some(shadow_texture);
-        self.light_buffer.update(3, &[1.0])?;
         Ok(())
     }
 
     pub fn shadow_map(&self) -> Option<&DepthTargetTexture2D> {
         self.shadow_texture.as_ref()
-    }
-
-    pub fn buffer(&self) -> &UniformBuffer {
-        &self.light_buffer
     }
 }
 
@@ -146,38 +110,39 @@ impl Light for DirectionalLight {
         format!(
         "
             uniform sampler2D shadowMap{};
-            layout (std140) uniform LightUniform{}
-            {{
-                BaseLight base{};
-                vec3 direction{};
-                float shadowEnabled{};
-                mat4 shadowMVP{};
-            }};
+            uniform float shadowEnabled{};
+            uniform mat4 shadowMVP{};
+
+            uniform vec3 color{};
+            uniform vec3 direction{};
+
             vec3 calculate_lighting{}(vec3 surface_color, vec3 position, vec3 normal, vec3 view_direction, float metallic, float roughness, float occlusion)
             {{
-                if(base{}.intensity > 0.001) {{
-                    vec3 light_color = base{}.intensity * base{}.color;
-                    vec3 result = calculate_light(light_color, -direction{}, surface_color, view_direction, normal, metallic, roughness);
-                    if(shadowEnabled{} > 0.5) {{
-                        result *= calculate_shadow(shadowMap{}, shadowMVP{}, position);
-                    }}
-                    return result;
+                vec3 result = calculate_light(color{}, -direction{}, surface_color, view_direction, normal, metallic, roughness);
+                if(shadowEnabled{} > 0.5) {{
+                    result *= calculate_shadow(shadowMap{}, shadowMVP{}, position);
                 }}
-                else {{
-                    return vec3(0.0, 0.0, 0.0);
-                }}
+                return result;
             }}
         
-        ", i, i, i, i, i, i, i, i, i, i, i, i, i, i)
+        ", i, i, i, i, i, i, i, i, i, i, i)
     }
     fn use_uniforms(&self, program: &Program, i: u32) -> ThreeDResult<()> {
         if let Some(tex) = self.shadow_map() {
             program.use_texture(&format!("shadowMap{}", i), tex)?;
+            program.use_uniform_float(&format!("shadowEnabled{}", i), &1.0)?;
+            program.use_uniform_mat4(&format!("shadowMVP{}", i), &self.shadow_matrix)?;
         } else {
             self.context
                 .use_texture_dummy(&program, &format!("shadowMap{}", i))?;
+            program.use_uniform_float(&format!("shadowEnabled{}", i), &0.0)?;
+            program.use_uniform_mat4(&format!("shadowMVP{}", i), &self.shadow_matrix)?;
         }
-        program.use_uniform_block(&format!("LightUniform{}", i), self.buffer());
+        program.use_uniform_vec3(
+            &format!("color{}", i),
+            &(self.color.to_vec3() * self.intensity),
+        )?;
+        program.use_uniform_vec3(&format!("direction{}", i), &self.direction)?;
         Ok(())
     }
 }
