c6ad41081f1a94d31b96b70a82a40f2594206ece
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-01-14 20:35:06 +0100

    Use RGBA8 from the rgb crate internally for Color

--- src/core/color.rs
+++ src/core/color.rs
@@ -1,55 +1,33 @@
 use crate::core::math::*;
+use rgb::RGBA8;
 
 /// Represents a color composed of a red, green and blue component.
 /// In addition, the alpha value determines the how transparent the color is (0 is fully transparent and 255 is fully opaque).
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 pub struct Color {
-    /// Red component
-    pub r: u8,
-    /// Green component
-    pub g: u8,
-    /// Blue component
-    pub b: u8,
-    /// Alpha component
-    pub a: u8,
+    inner: RGBA8,
+}
+
+impl std::ops::Deref for Color {
+    type Target = RGBA8;
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl std::ops::DerefMut for Color {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner
+    }
 }
 
 impl Color {
     ///
-    /// Creates a new color with the given values.
-    ///
-    pub const fn new(r: u8, g: u8, b: u8, a: u8) -> Self {
-        Self { r, g, b, a }
-    }
-
-    ///
     /// Creates a new color with the given r, g and b values and an alpha value of 255.
     ///
     pub const fn new_opaque(r: u8, g: u8, b: u8) -> Self {
-        Self { r, g, b, a: 255 }
-    }
-
-    ///
-    /// Creates a new color from three float elements where each element are in the range `0.0..=1.0`.
-    ///
-    pub fn from_rgb_slice(rgba: &[f32; 3]) -> Self {
         Self {
-            r: (rgba[0] * 255.0) as u8,
-            g: (rgba[1] * 255.0) as u8,
-            b: (rgba[2] * 255.0) as u8,
-            ..Default::default()
-        }
-    }
-
-    ///
-    /// Creates a new color from four float elements where each element are in the range `0.0..=1.0`.
-    ///
-    pub fn from_rgba_slice(rgba: &[f32; 4]) -> Self {
-        Self {
-            r: (rgba[0] * 255.0) as u8,
-            g: (rgba[1] * 255.0) as u8,
-            b: (rgba[2] * 255.0) as u8,
-            a: (rgba[3] * 255.0) as u8,
+            inner: RGBA8 { r, g, b, a: 255 },
         }
     }
 
@@ -64,96 +42,56 @@ impl Color {
     /// Opaque black
     pub const BLACK: Color = Color::new_opaque(0, 0, 0);
 
+    ///
+    /// Creates a new color from three float elements where each element are in the range `0.0..=1.0`.
+    ///
+    #[deprecated = "use into() instead"]
+    pub fn from_rgb_slice(rgba: &[f32; 3]) -> Self {
+        rgba.into()
+    }
+
+    ///
+    /// Creates a new color from four float elements where each element are in the range `0.0..=1.0`.
+    ///
+    #[deprecated = "use into() instead"]
+    pub fn from_rgba_slice(rgba: &[f32; 4]) -> Self {
+        rgba.into()
+    }
+
     /// Convert to [`Vec3`] by mapping the red, green and blue component to the range `0.0..=1.0`.
+    #[deprecated = "use into() instead"]
     pub fn to_vec3(&self) -> Vec3 {
-        vec3(
-            self.r as f32 / 255.0,
-            self.g as f32 / 255.0,
-            self.b as f32 / 255.0,
-        )
+        (*self).into()
     }
 
     /// Convert to [`Vec4`] by mapping each component to the range `0.0..=1.0`.
+    #[deprecated = "use into() instead"]
     pub fn to_vec4(&self) -> Vec4 {
-        vec4(
-            self.r as f32 / 255.0,
-            self.g as f32 / 255.0,
-            self.b as f32 / 255.0,
-            self.a as f32 / 255.0,
-        )
+        (*self).into()
     }
 
     /// Convert to a slice by mapping the red, green and blue component to the range `0.0..=1.0`.
+    #[deprecated = "use into() instead"]
     pub fn to_rgb_slice(&self) -> [f32; 3] {
-        [
-            self.r as f32 / 255.0,
-            self.g as f32 / 255.0,
-            self.b as f32 / 255.0,
-        ]
+        (*self).into()
     }
 
     /// Convert to a slice by mapping each component to the range `0.0..=1.0`.
+    #[deprecated = "use into() instead"]
     pub fn to_rgba_slice(&self) -> [f32; 4] {
-        [
-            self.r as f32 / 255.0,
-            self.g as f32 / 255.0,
-            self.b as f32 / 255.0,
-            self.a as f32 / 255.0,
-        ]
-    }
-}
-
-impl From<[u8; 3]> for Color {
-    fn from(rgba: [u8; 3]) -> Self {
-        Self {
-            r: rgba[0],
-            g: rgba[1],
-            b: rgba[2],
-            a: 255,
-        }
-    }
-}
-
-impl From<&[u8; 3]> for Color {
-    fn from(rgba: &[u8; 3]) -> Self {
-        Self {
-            r: rgba[0],
-            g: rgba[1],
-            b: rgba[2],
-            a: 255,
-        }
-    }
-}
-
-impl From<[u8; 4]> for Color {
-    fn from(rgba: [u8; 4]) -> Self {
-        Self {
-            r: rgba[0],
-            g: rgba[1],
-            b: rgba[2],
-            a: rgba[3],
-        }
-    }
-}
-
-impl From<&[u8; 4]> for Color {
-    fn from(rgba: &[u8; 4]) -> Self {
-        Self {
-            r: rgba[0],
-            g: rgba[1],
-            b: rgba[2],
-            a: rgba[3],
-        }
+        (*self).into()
     }
 }
 
 impl From<[f32; 3]> for Color {
     fn from(rgba: [f32; 3]) -> Self {
         Self {
-            r: (rgba[0] * 255.0) as u8,
-            g: (rgba[1] * 255.0) as u8,
-            b: (rgba[2] * 255.0) as u8,
-            a: 255,
+            inner: RGBA8 {
+                r: (rgba[0] * 255.0) as u8,
+                g: (rgba[1] * 255.0) as u8,
+                b: (rgba[2] * 255.0) as u8,
+                a: 255,
+            },
         }
     }
 }
@@ -161,10 +99,12 @@ impl From<[f32; 3]> for Color {
 impl From<&[f32; 3]> for Color {
     fn from(rgba: &[f32; 3]) -> Self {
         Self {
-            r: (rgba[0] * 255.0) as u8,
-            g: (rgba[1] * 255.0) as u8,
-            b: (rgba[2] * 255.0) as u8,
-            a: 255,
+            inner: RGBA8 {
+                r: (rgba[0] * 255.0) as u8,
+                g: (rgba[1] * 255.0) as u8,
+                b: (rgba[2] * 255.0) as u8,
+                a: 255,
+            },
         }
     }
 }
@@ -172,10 +112,12 @@ impl From<&[f32; 3]> for Color {
 impl From<[f32; 4]> for Color {
     fn from(rgba: [f32; 4]) -> Self {
         Self {
-            r: (rgba[0] * 255.0) as u8,
-            g: (rgba[1] * 255.0) as u8,
-            b: (rgba[2] * 255.0) as u8,
-            a: (rgba[3] * 255.0) as u8,
+            inner: RGBA8 {
+                r: (rgba[0] * 255.0) as u8,
+                g: (rgba[1] * 255.0) as u8,
+                b: (rgba[2] * 255.0) as u8,
+                a: (rgba[3] * 255.0) as u8,
+            },
         }
     }
 }
@@ -183,10 +125,12 @@ impl From<[f32; 4]> for Color {
 impl From<&[f32; 4]> for Color {
     fn from(rgba: &[f32; 4]) -> Self {
         Self {
-            r: (rgba[0] * 255.0) as u8,
-            g: (rgba[1] * 255.0) as u8,
-            b: (rgba[2] * 255.0) as u8,
-            a: (rgba[3] * 255.0) as u8,
+            inner: RGBA8 {
+                r: (rgba[0] * 255.0) as u8,
+                g: (rgba[1] * 255.0) as u8,
+                b: (rgba[2] * 255.0) as u8,
+                a: (rgba[3] * 255.0) as u8,
+            },
         }
     }
 }
