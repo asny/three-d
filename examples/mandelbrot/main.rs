68e45134516afb86ccaa9ce360b6980deebe6c58
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-01-23 19:27:01 +0100

    Fix mandelbrot example

--- examples/mandelbrot/main.rs
+++ examples/mandelbrot/main.rs
@@ -5,14 +5,18 @@ use three_d::window::*;
 struct MandelbrotMaterial {}
 
 impl Material for MandelbrotMaterial {
-    fn fragment_shader_source(&self, _use_vertex_colors: bool, _lights: &Lights) -> String {
+    fn fragment_shader_source(
+        &self,
+        _use_vertex_colors: bool,
+        _lights: impl std::iter::IntoIterator<Item = impl Light>,
+    ) -> String {
         include_str!("../assets/shaders/mandelbrot.frag").to_string()
     }
     fn use_uniforms(
         &self,
         _program: &Program,
         _camera: &Camera,
-        _lights: &Lights,
+        _lights: impl std::iter::IntoIterator<Item = impl Light>,
     ) -> ThreeDResult<()> {
         Ok(())
     }
