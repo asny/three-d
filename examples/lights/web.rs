9e2f8d395b3593b83b2f7fa18ccb269efe1ba879
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-02-20 14:38:03 +0100

    Use async in lights example

--- examples/lights/web.rs
+++ examples/lights/web.rs
@@ -1,15 +1,15 @@
-include!("../main.rs");
+mod example;
 
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
-pub fn start() -> Result<(), JsValue> {
+pub async fn start() -> Result<(), JsValue> {
     console_log::init_with_level(log::Level::Debug).unwrap();
 
     use log::info;
     info!("Logging works!");
 
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-    main();
+    example::run(None).await;
     Ok(())
 }
