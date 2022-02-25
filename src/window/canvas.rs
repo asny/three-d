0da397972b350f5bf597ae725dfbcc4c901b5d4d
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-02-25 14:01:23 +0100

    Avoid crash when not starting a render loop on web

--- src/window/canvas.rs
+++ src/window/canvas.rs
@@ -715,108 +715,97 @@ impl Input {
 
 impl Drop for Window {
     fn drop(&mut self) {
-        self.canvas()
-            .unwrap()
-            .remove_event_listener_with_callback(
-                "contextmenu",
-                self.closures_with_event[0].as_ref().unchecked_ref(),
-            )
-            .unwrap();
-        self.closures_with_event.clear();
-        self.canvas()
-            .unwrap()
-            .remove_event_listener_with_callback(
-                "resize",
-                self.closures[0].as_ref().unchecked_ref(),
-            )
-            .unwrap();
-        self.closures.clear();
-        self.canvas()
-            .unwrap()
-            .remove_event_listener_with_callback(
-                "mouseenter",
-                self.closures_with_mouseevent[0].as_ref().unchecked_ref(),
-            )
-            .unwrap();
-        self.canvas()
-            .unwrap()
-            .remove_event_listener_with_callback(
-                "mouseleave",
-                self.closures_with_mouseevent[1].as_ref().unchecked_ref(),
-            )
-            .unwrap();
-        self.canvas()
-            .unwrap()
-            .remove_event_listener_with_callback(
-                "mousedown",
-                self.closures_with_mouseevent[2].as_ref().unchecked_ref(),
-            )
-            .unwrap();
+        if let Some(closure) = self.closures_with_event.pop() {
+            self.canvas()
+                .unwrap()
+                .remove_event_listener_with_callback(
+                    "contextmenu",
+                    closure.as_ref().unchecked_ref(),
+                )
+                .unwrap();
+        }
 
-        self.canvas()
-            .unwrap()
-            .remove_event_listener_with_callback(
-                "mouseup",
-                self.closures_with_mouseevent[3].as_ref().unchecked_ref(),
-            )
-            .unwrap();
-        self.canvas()
-            .unwrap()
-            .remove_event_listener_with_callback(
-                "mousemove",
-                self.closures_with_mouseevent[4].as_ref().unchecked_ref(),
-            )
-            .unwrap();
-        self.closures_with_mouseevent.clear();
-        self.canvas()
-            .unwrap()
-            .remove_event_listener_with_callback(
-                "wheel",
-                self.closures_with_wheelevent[0].as_ref().unchecked_ref(),
-            )
-            .unwrap();
-        self.closures_with_wheelevent.clear();
-        self.canvas()
-            .unwrap()
-            .remove_event_listener_with_callback(
-                "touchstart",
-                self.closures_with_touchevent[0].as_ref().unchecked_ref(),
-            )
-            .unwrap();
-        self.canvas()
-            .unwrap()
-            .remove_event_listener_with_callback(
-                "touchend",
-                self.closures_with_touchevent[1].as_ref().unchecked_ref(),
-            )
-            .unwrap();
-        self.canvas()
-            .unwrap()
-            .remove_event_listener_with_callback(
-                "touchmove",
-                self.closures_with_touchevent[2].as_ref().unchecked_ref(),
-            )
-            .unwrap();
-        self.closures_with_touchevent.clear();
-        web_sys::window()
-            .unwrap()
-            .document()
-            .unwrap()
-            .remove_event_listener_with_callback(
-                "keydown",
-                self.closures_with_keyboardevent[0].as_ref().unchecked_ref(),
-            )
-            .unwrap();
-        web_sys::window()
-            .unwrap()
-            .document()
-            .unwrap()
-            .remove_event_listener_with_callback(
-                "keyup",
-                self.closures_with_keyboardevent[1].as_ref().unchecked_ref(),
-            )
-            .unwrap();
-        self.closures_with_keyboardevent.clear();
+        if let Some(closure) = self.closures.pop() {
+            self.canvas()
+                .unwrap()
+                .remove_event_listener_with_callback("resize", closure.as_ref().unchecked_ref())
+                .unwrap();
+        }
+
+        if let Some(closure) = self.closures_with_mouseevent.pop() {
+            self.canvas()
+                .unwrap()
+                .remove_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())
+                .unwrap();
+        }
+        if let Some(closure) = self.closures_with_mouseevent.pop() {
+            self.canvas()
+                .unwrap()
+                .remove_event_listener_with_callback("mouseup", closure.as_ref().unchecked_ref())
+                .unwrap();
+        }
+        if let Some(closure) = self.closures_with_mouseevent.pop() {
+            self.canvas()
+                .unwrap()
+                .remove_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())
+                .unwrap();
+        }
+        if let Some(closure) = self.closures_with_mouseevent.pop() {
+            self.canvas()
+                .unwrap()
+                .remove_event_listener_with_callback("mouseleave", closure.as_ref().unchecked_ref())
+                .unwrap();
+        }
+        if let Some(closure) = self.closures_with_mouseevent.pop() {
+            self.canvas()
+                .unwrap()
+                .remove_event_listener_with_callback("mouseenter", closure.as_ref().unchecked_ref())
+                .unwrap();
+        }
+
+        if let Some(closure) = self.closures_with_wheelevent.pop() {
+            self.canvas()
+                .unwrap()
+                .remove_event_listener_with_callback("wheel", closure.as_ref().unchecked_ref())
+                .unwrap();
+        }
+
+        if let Some(closure) = self.closures_with_touchevent.pop() {
+            self.canvas()
+                .unwrap()
+                .remove_event_listener_with_callback("touchmove", closure.as_ref().unchecked_ref())
+                .unwrap();
+        }
+        if let Some(closure) = self.closures_with_touchevent.pop() {
+            self.canvas()
+                .unwrap()
+                .remove_event_listener_with_callback("touchend", closure.as_ref().unchecked_ref())
+                .unwrap();
+        }
+        if let Some(closure) = self.closures_with_touchevent.pop() {
+            self.canvas()
+                .as_mut()
+                .unwrap()
+                .remove_event_listener_with_callback("touchstart", closure.as_ref().unchecked_ref())
+                .unwrap();
+        }
+
+        if let Some(closure) = self.closures_with_keyboardevent.pop() {
+            web_sys::window()
+                .unwrap()
+                .document()
+                .unwrap()
+                .remove_event_listener_with_callback("keyup", closure.as_ref().unchecked_ref())
+                .unwrap();
+        }
+        if let Some(closure) = self.closures_with_keyboardevent.pop() {
+            web_sys::window()
+                .unwrap()
+                .document()
+                .unwrap()
+                .remove_event_listener_with_callback("keydown", closure.as_ref().unchecked_ref())
+                .unwrap();
+        }
     }
 }
 
