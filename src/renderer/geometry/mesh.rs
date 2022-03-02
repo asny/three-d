a00e1b6e8603de658f87f61c3ab15724ae4408bb
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-03-02 12:34:34 +0100

    Mesh impl Geometry

--- src/renderer/geometry/mesh.rs
+++ src/renderer/geometry/mesh.rs
@@ -1,4 +1,5 @@
 use crate::core::*;
+use crate::renderer::*;
 
 ///
 /// A triangle mesh where the mesh data is transfered to the GPU.
@@ -16,8 +17,11 @@ pub struct Mesh {
     pub color_buffer: Option<VertexBuffer>,
     /// Buffer with the index data, ie. three contiguous integers define the triangle where each integer is and index into the other vertex buffers.
     pub index_buffer: Option<ElementBuffer>,
-    /// Optional name of the mesh.
-    pub name: String,
+    context: Context,
+    aabb: AxisAlignedBoundingBox,
+    aabb_local: AxisAlignedBoundingBox,
+    transformation: Mat4,
+    texture_transform: Mat3,
 }
 
 impl Mesh {
@@ -59,14 +63,159 @@ impl Mesh {
         } else {
             None
         };
+        let aabb = cpu_mesh.compute_aabb();
         Ok(Self {
+            context: context.clone(),
             position_buffer,
             normal_buffer,
             tangent_buffer,
             index_buffer,
             uv_buffer,
             color_buffer,
-            name: cpu_mesh.name.clone(),
+            aabb,
+            aabb_local: aabb.clone(),
+            transformation: Mat4::identity(),
+            texture_transform: Mat3::identity(),
         })
+    }
+
+    ///
+    /// Get the texture transform applied to the uv coordinates of the model.
+    ///
+    pub fn texture_transform(&mut self) -> &Mat3 {
+        &self.texture_transform
+    }
+
+    ///
+    /// Set the texture transform applied to the uv coordinates of the model.
+    ///
+    pub fn set_texture_transform(&mut self, texture_transform: Mat3) {
+        self.texture_transform = texture_transform;
+    }
+
+    pub(crate) fn vertex_shader_source(fragment_shader_source: &str) -> ThreeDResult<String> {
+        let use_positions = fragment_shader_source.find("in vec3 pos;").is_some();
+        let use_normals = fragment_shader_source.find("in vec3 nor;").is_some();
+        let use_tangents = fragment_shader_source.find("in vec3 tang;").is_some();
+        let use_uvs = fragment_shader_source.find("in vec2 uvs;").is_some();
+        let use_colors = fragment_shader_source.find("in vec4 col;").is_some();
+        Ok(format!(
+            "{}{}{}{}{}{}{}",
+            if use_positions {
+                "#define USE_POSITIONS\n"
+            } else {
+                ""
+            },
+            if use_normals {
+                "#define USE_NORMALS\n"
+            } else {
+                ""
+            },
+            if use_tangents {
+                if fragment_shader_source.find("in vec3 bitang;").is_none() {
+                    Err(CoreError::MissingBitangent)?;
+                }
+                "#define USE_TANGENTS\n"
+            } else {
+                ""
+            },
+            if use_uvs { "#define USE_UVS\n" } else { "" },
+            if use_colors {
+                "#define USE_COLORS\n"
+            } else {
+                ""
+            },
+            include_str!("../../core/shared.frag"),
+            include_str!("shaders/mesh.vert"),
+        ))
+    }
+}
+
+impl Geometry for Mesh {
+    fn aabb(&self) -> AxisAlignedBoundingBox {
+        self.aabb
+    }
+
+    fn transformation(&self) -> Mat4 {
+        self.transformation
+    }
+
+    fn render_with_material(
+        &self,
+        material: &dyn Material,
+        camera: &Camera,
+        lights: &[&dyn Light],
+    ) -> ThreeDResult<()> {
+        let fragment_shader_source =
+            material.fragment_shader_source(self.color_buffer.is_some(), lights);
+        self.context.program(
+            &Self::vertex_shader_source(&fragment_shader_source)?,
+            &fragment_shader_source,
+            |program| {
+                material.use_uniforms(program, camera, lights)?;
+                program.use_uniform_block("Camera", camera.uniform_buffer());
+                program.use_uniform_mat4("modelMatrix", &self.transformation)?;
+
+                if program.requires_attribute("position") {
+                    program.use_attribute_vec3("position", &self.position_buffer)?;
+                }
+                if program.requires_attribute("uv_coordinates") {
+                    program.use_uniform_mat3("textureTransform", &self.texture_transform)?;
+                    let uv_buffer = self
+                        .uv_buffer
+                        .as_ref()
+                        .ok_or(CoreError::MissingMeshBuffer("uv coordinates".to_string()))?;
+                    program.use_attribute_vec2("uv_coordinates", uv_buffer)?;
+                }
+                if program.requires_attribute("normal") {
+                    let normal_buffer = self
+                        .normal_buffer
+                        .as_ref()
+                        .ok_or(CoreError::MissingMeshBuffer("normal".to_string()))?;
+                    program.use_attribute_vec3("normal", normal_buffer)?;
+                    program.use_uniform_mat4(
+                        "normalMatrix",
+                        &self.transformation.invert().unwrap().transpose(),
+                    )?;
+                    if program.requires_attribute("tangent") {
+                        let tangent_buffer = self
+                            .tangent_buffer
+                            .as_ref()
+                            .ok_or(CoreError::MissingMeshBuffer("tangent".to_string()))?;
+                        program.use_attribute_vec4("tangent", tangent_buffer)?;
+                    }
+                }
+                if program.requires_attribute("color") {
+                    let color_buffer = self
+                        .color_buffer
+                        .as_ref()
+                        .ok_or(CoreError::MissingMeshBuffer("color".to_string()))?;
+                    program.use_attribute_vec4("color", color_buffer)?;
+                }
+                if let Some(ref index_buffer) = self.index_buffer {
+                    program.draw_elements(
+                        material.render_states(),
+                        camera.viewport(),
+                        index_buffer,
+                    );
+                } else {
+                    program.draw_arrays(
+                        material.render_states(),
+                        camera.viewport(),
+                        self.position_buffer.count() as u32 / 3,
+                    );
+                }
+                Ok(())
+            },
+        )
+    }
+}
+
+impl GeometryMut for Mesh {
+    fn set_transformation(&mut self, transformation: Mat4) {
+        self.transformation = transformation;
+        let mut aabb = self.aabb_local.clone();
+        aabb.transform(&self.transformation);
+        self.aabb = aabb;
     }
 }
--- src/renderer/object/model.rs
+++ src/renderer/object/model.rs
@@ -1,17 +1,11 @@
 use crate::core::*;
 use crate::renderer::*;
-use std::rc::Rc;
 
 ///
 /// A 3D model consisting of a triangle mesh and any material that implements the `Material` trait.
 ///
 pub struct Model<M: Material> {
-    context: Context,
-    mesh: Rc<Mesh>,
-    aabb: AxisAlignedBoundingBox,
-    aabb_local: AxisAlignedBoundingBox,
-    transformation: Mat4,
-    texture_transform: Mat3,
+    mesh: Mesh,
     /// The material applied to the model
     pub material: M,
 }
@@ -34,31 +28,10 @@ impl<M: Material> Model<M> {
         cpu_mesh: &CpuMesh,
         material: M,
     ) -> ThreeDResult<Self> {
-        let mesh = Rc::new(Mesh::new(context, cpu_mesh)?);
-        let aabb = cpu_mesh.compute_aabb();
         Ok(Self {
-            mesh,
-            aabb,
-            aabb_local: aabb.clone(),
-            transformation: Mat4::identity(),
-            texture_transform: Mat3::identity(),
-            context: context.clone(),
+            mesh: Mesh::new(context, cpu_mesh)?,
             material,
         })
-    }
-
-    ///
-    /// Get the texture transform applied to the uv coordinates of the model.
-    ///
-    pub fn texture_transform(&mut self) -> &Mat3 {
-        &self.texture_transform
-    }
-
-    ///
-    /// Set the texture transform applied to the uv coordinates of the model.
-    ///
-    pub fn set_texture_transform(&mut self, texture_transform: Mat3) {
-        self.texture_transform = texture_transform;
     }
 
     pub(in crate::renderer) fn set_transformation_2d(&mut self, transformation: Mat3) {
@@ -81,52 +54,15 @@ impl<M: Material> Model<M> {
             transformation.z.z,
         ));
     }
-
-    pub(super) fn vertex_shader_source(fragment_shader_source: &str) -> ThreeDResult<String> {
-        let use_positions = fragment_shader_source.find("in vec3 pos;").is_some();
-        let use_normals = fragment_shader_source.find("in vec3 nor;").is_some();
-        let use_tangents = fragment_shader_source.find("in vec3 tang;").is_some();
-        let use_uvs = fragment_shader_source.find("in vec2 uvs;").is_some();
-        let use_colors = fragment_shader_source.find("in vec4 col;").is_some();
-        Ok(format!(
-            "{}{}{}{}{}{}{}",
-            if use_positions {
-                "#define USE_POSITIONS\n"
-            } else {
-                ""
-            },
-            if use_normals {
-                "#define USE_NORMALS\n"
-            } else {
-                ""
-            },
-            if use_tangents {
-                if fragment_shader_source.find("in vec3 bitang;").is_none() {
-                    Err(CoreError::MissingBitangent)?;
-                }
-                "#define USE_TANGENTS\n"
-            } else {
-                ""
-            },
-            if use_uvs { "#define USE_UVS\n" } else { "" },
-            if use_colors {
-                "#define USE_COLORS\n"
-            } else {
-                ""
-            },
-            include_str!("../../core/shared.frag"),
-            include_str!("shaders/mesh.vert"),
-        ))
-    }
 }
 
 impl<M: Material> Geometry for Model<M> {
     fn aabb(&self) -> AxisAlignedBoundingBox {
-        self.aabb
+        self.mesh.aabb()
     }
 
     fn transformation(&self) -> Mat4 {
-        self.transformation
+        self.mesh.transformation()
     }
 
     fn render_with_material(
@@ -135,81 +71,13 @@ impl<M: Material> Geometry for Model<M> {
         camera: &Camera,
         lights: &[&dyn Light],
     ) -> ThreeDResult<()> {
-        let fragment_shader_source =
-            material.fragment_shader_source(self.mesh.color_buffer.is_some(), lights);
-        self.context.program(
-            &Self::vertex_shader_source(&fragment_shader_source)?,
-            &fragment_shader_source,
-            |program| {
-                material.use_uniforms(program, camera, lights)?;
-                program.use_uniform_block("Camera", camera.uniform_buffer());
-                program.use_uniform_mat4("modelMatrix", &self.transformation)?;
-
-                if program.requires_attribute("position") {
-                    program.use_attribute_vec3("position", &self.mesh.position_buffer)?;
-                }
-                if program.requires_attribute("uv_coordinates") {
-                    program.use_uniform_mat3("textureTransform", &self.texture_transform)?;
-                    let uv_buffer = self
-                        .mesh
-                        .uv_buffer
-                        .as_ref()
-                        .ok_or(CoreError::MissingMeshBuffer("uv coordinates".to_string()))?;
-                    program.use_attribute_vec2("uv_coordinates", uv_buffer)?;
-                }
-                if program.requires_attribute("normal") {
-                    let normal_buffer = self
-                        .mesh
-                        .normal_buffer
-                        .as_ref()
-                        .ok_or(CoreError::MissingMeshBuffer("normal".to_string()))?;
-                    program.use_attribute_vec3("normal", normal_buffer)?;
-                    program.use_uniform_mat4(
-                        "normalMatrix",
-                        &self.transformation.invert().unwrap().transpose(),
-                    )?;
-                    if program.requires_attribute("tangent") {
-                        let tangent_buffer = self
-                            .mesh
-                            .tangent_buffer
-                            .as_ref()
-                            .ok_or(CoreError::MissingMeshBuffer("tangent".to_string()))?;
-                        program.use_attribute_vec4("tangent", tangent_buffer)?;
-                    }
-                }
-                if program.requires_attribute("color") {
-                    let color_buffer = self
-                        .mesh
-                        .color_buffer
-                        .as_ref()
-                        .ok_or(CoreError::MissingMeshBuffer("color".to_string()))?;
-                    program.use_attribute_vec4("color", color_buffer)?;
-                }
-                if let Some(ref index_buffer) = self.mesh.index_buffer {
-                    program.draw_elements(
-                        material.render_states(),
-                        camera.viewport(),
-                        index_buffer,
-                    );
-                } else {
-                    program.draw_arrays(
-                        material.render_states(),
-                        camera.viewport(),
-                        self.mesh.position_buffer.count() as u32 / 3,
-                    );
-                }
-                Ok(())
-            },
-        )
+        self.mesh.render_with_material(material, camera, lights)
     }
 }
 
 impl<M: Material> GeometryMut for Model<M> {
     fn set_transformation(&mut self, transformation: Mat4) {
-        self.transformation = transformation;
-        let mut aabb = self.aabb_local.clone();
-        aabb.transform(&self.transformation);
-        self.aabb = aabb;
+        self.mesh.set_transformation(transformation);
     }
 }
 
@@ -222,17 +90,13 @@ impl<M: Material> Object for Model<M> {
         self.material.is_transparent()
     }
 }
-
+/*
 impl<M: Material + Clone> Clone for Model<M> {
     fn clone(&self) -> Self {
         Self {
             context: self.context.clone(),
             mesh: self.mesh.clone(),
-            aabb: self.aabb.clone(),
-            aabb_local: self.aabb_local.clone(),
-            transformation: self.transformation,
-            texture_transform: self.texture_transform,
             material: self.material.clone(),
         }
     }
-}
+}*/
