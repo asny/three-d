1238b36d81a06529ae5ddc619c66c7c9e22f5f8f
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-03-07 10:41:22 +0100

    Made Buffer private

--- src/core/buffer/vertex_buffer.rs
+++ src/core/buffer/vertex_buffer.rs
@@ -30,6 +30,32 @@ impl<T: BufferDataType> VertexBuffer<T> {
     }
 
     ///
+    /// Fills the vertex buffer with the given data. The data should be in the same format as specified in the shader.
+    /// As an example, if specified as `vec3` in the shader it needs to be specified as an array of `Vector3<T>` where `T` is a primitive type that implements [BufferDataType], for example can be f16 or f32.
+    ///
+    pub fn fill(&mut self, data: &[T]) {
+        self.buffer.fill(data);
+    }
+
+    ///
+    /// The number of values in the buffer.
+    ///
+    pub fn count(&self) -> u32 {
+        self.attribute_count() * T::size()
+    }
+
+    ///
+    /// The number of vertex attributes in the buffer.
+    ///
+    pub fn attribute_count(&self) -> u32 {
+        self.buffer.attribute_count()
+    }
+
+    pub(crate) fn bind(&self) {
+        self.buffer.bind();
+    }
+
+    ///
     /// Creates a new vertex buffer and fills it with the given data which must contain between 1 and 4 contiguous values for each vertex.
     /// Use this method instead of [new_with_dynamic](VertexBuffer::new_with_dynamic)
     /// when you do not expect the data to change often.
@@ -48,17 +74,24 @@ impl<T: BufferDataType> VertexBuffer<T> {
     pub fn new_with_dynamic(context: &Context, data: &[T]) -> ThreeDResult<Self> {
         Self::new_with_data(context, data)
     }
-}
 
-impl<T: BufferDataType> std::ops::Deref for VertexBuffer<T> {
-    type Target = Buffer<T>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.buffer
+    ///
+    /// Fills the vertex buffer with the given data which must contain between 1 and 4 contiguous values for each vertex.
+    /// Use this method instead of [fill_with_dynamic](VertexBuffer::fill_with_dynamic)
+    /// when you do not expect the data to change often.
+    ///
+    #[deprecated = "use fill() and specify the data in the same format as in the shader (for example an array of Vec3 instead of f32)"]
+    pub fn fill_with_static(&mut self, data: &[T]) {
+        self.fill(data)
     }
-}
-impl<T: BufferDataType> std::ops::DerefMut for VertexBuffer<T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.buffer
+
+    ///
+    /// Fills the vertex buffer with the given data which must contain between 1 and 4 contiguous values for each vertex.
+    /// Use this method instead of [fill_with_static](VertexBuffer::fill_with_static)
+    /// when you expect the data to change often.
+    ///
+    #[deprecated = "use fill() and specify the data in the same format as in the shader (for example an array of Vec3 instead of f32)"]
+    pub fn fill_with_dynamic(&mut self, data: &[T]) {
+        self.fill(data)
     }
 }
