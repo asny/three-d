f19a5d9bad45b5fd2e08cfc7c29390be61c5c81b
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-03-11 08:20:23 +0100

    Added volume example

---
+++ examples/volume/src/main.rs
@@ -0,0 +1,85 @@
+// Entry point for non-wasm
+#[cfg(not(target_arch = "wasm32"))]
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    run(args.get(1).map(|a| std::path::PathBuf::from(a))).await;
+}
+
+use three_d::*;
+
+pub async fn run(screenshot: Option<std::path::PathBuf>) {
+    let window = Window::new(WindowSettings {
+        title: "Volume!".to_string(),
+        max_size: Some((1280, 720)),
+        ..Default::default()
+    })
+    .unwrap();
+    let context = window.gl().unwrap();
+
+    let mut camera = Camera::new_perspective(
+        &context,
+        window.viewport().unwrap(),
+        vec3(4.0, 0.5, 4.0),
+        vec3(0.0, 0.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        degrees(45.0),
+        0.1,
+        1000.0,
+    )
+    .unwrap();
+    let mut control = OrbitControl::new(*camera.target(), 1.0, 100.0);
+
+    let bytes = include_bytes!("../../assets/C60Small.vol");
+    let volume = Volume::new(
+        &context,
+        &CpuTexture3D {
+            data: bytes[28..].to_vec(),
+            width: 64,
+            height: 64,
+            depth: 64,
+            format: Format::R,
+            ..Default::default()
+        },
+    )
+    .unwrap();
+
+    let ambient = AmbientLight::new(&context, 0.4, Color::WHITE).unwrap();
+    let directional =
+        DirectionalLight::new(&context, 2.0, Color::WHITE, &vec3(0.0, -1.0, -1.0)).unwrap();
+
+    // main loop
+    window
+        .render_loop(move |mut frame_input| {
+            let mut redraw = frame_input.first_frame;
+            redraw |= camera.set_viewport(frame_input.viewport).unwrap();
+            redraw |= control
+                .handle_events(&mut camera, &mut frame_input.events)
+                .unwrap();
+
+            // draw
+            if redraw {
+                Screen::write(
+                    &context,
+                    ClearState::color_and_depth(0.5, 0.5, 0.5, 1.0, 1.0),
+                    || render_pass(&camera, &[&volume], &[&ambient, &directional]),
+                )
+                .unwrap();
+            }
+
+            if let Some(ref screenshot) = screenshot {
+                // To automatically generate screenshots of the examples, can safely be ignored.
+                FrameOutput {
+                    screenshot: Some(screenshot.clone()),
+                    exit: true,
+                    ..Default::default()
+                }
+            } else {
+                FrameOutput {
+                    swap_buffers: redraw,
+                    ..Default::default()
+                }
+            }
+        })
+        .unwrap();
+}
