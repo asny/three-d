9a0403544f4e5bb1b42bdea3f33af095467cef04
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-03-19 08:30:48 +0100

    More primitive data types supported for UniformDataType

--- src/core/uniform.rs
+++ src/core/uniform.rs
@@ -3,9 +3,13 @@ use crate::core::*;
 ///
 /// Possible types that can be send as a uniform to a shader (a variable that is uniformly available when processing all vertices and fragments).
 ///
-pub trait UniformDataType: std::fmt::Debug + crate::core::internal::DataType {}
+pub trait UniformDataType: crate::core::internal::DataType {}
 
+impl UniformDataType for u8 {}
+impl UniformDataType for u16 {}
+impl UniformDataType for u32 {}
 impl UniformDataType for i32 {}
+impl UniformDataType for f16 {}
 impl UniformDataType for f32 {}
 
 impl<T: UniformDataType + crate::core::internal::PrimitiveDataType> UniformDataType for Vector2<T> {}
