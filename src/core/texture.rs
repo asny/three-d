9f8e88811ed43a37bc953ca0cfc924bbdcbe92dd
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-03-19 18:45:02 +0100

    More data types for buffer/uniform

--- src/core/texture.rs
+++ src/core/texture.rs
@@ -60,12 +60,12 @@ impl TextureDataType for f32 {}
 impl<T: TextureDataType + crate::core::internal::PrimitiveDataType> TextureDataType for Vector2<T> {}
 impl<T: TextureDataType + crate::core::internal::PrimitiveDataType> TextureDataType for Vector3<T> {}
 impl<T: TextureDataType + crate::core::internal::PrimitiveDataType> TextureDataType for Vector4<T> {}
-
 impl<T: TextureDataType + crate::core::internal::PrimitiveDataType> TextureDataType for [T; 2] {}
 impl<T: TextureDataType + crate::core::internal::PrimitiveDataType> TextureDataType for [T; 3] {}
 impl<T: TextureDataType + crate::core::internal::PrimitiveDataType> TextureDataType for [T; 4] {}
 
 impl TextureDataType for Color {}
+impl TextureDataType for Quat {}
 
 impl<T: TextureDataType + ?Sized> TextureDataType for &T {}
 
--- src/core/uniform.rs
+++ src/core/uniform.rs
@@ -8,6 +8,8 @@ pub trait UniformDataType: crate::core::internal::DataType {}
 impl UniformDataType for u8 {}
 impl UniformDataType for u16 {}
 impl UniformDataType for u32 {}
+impl UniformDataType for i8 {}
+impl UniformDataType for i16 {}
 impl UniformDataType for i32 {}
 impl UniformDataType for f16 {}
 impl UniformDataType for f32 {}
@@ -15,13 +17,12 @@ impl UniformDataType for f32 {}
 impl<T: UniformDataType + crate::core::internal::PrimitiveDataType> UniformDataType for Vector2<T> {}
 impl<T: UniformDataType + crate::core::internal::PrimitiveDataType> UniformDataType for Vector3<T> {}
 impl<T: UniformDataType + crate::core::internal::PrimitiveDataType> UniformDataType for Vector4<T> {}
-
-impl UniformDataType for Color {}
-impl UniformDataType for Quat {}
-
 impl<T: UniformDataType + crate::core::internal::PrimitiveDataType> UniformDataType for [T; 2] {}
 impl<T: UniformDataType + crate::core::internal::PrimitiveDataType> UniformDataType for [T; 3] {}
 impl<T: UniformDataType + crate::core::internal::PrimitiveDataType> UniformDataType for [T; 4] {}
+
+impl UniformDataType for Color {}
+impl UniformDataType for Quat {}
 
 impl<T: UniformDataType + crate::core::internal::PrimitiveDataType> UniformDataType for Matrix2<T> {}
 impl<T: UniformDataType + crate::core::internal::PrimitiveDataType> UniformDataType for Matrix3<T> {}
