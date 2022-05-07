189110dbb579327c02cacf76b7ecbe4cdb439aae
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-05-07 07:37:47 +0200

    Update io.rs

--- src/io.rs
+++ src/io.rs
@@ -3,22 +3,43 @@
 //! Also includes functionality to save data which is limited to desktop.
 //!
 
+#![allow(missing_docs)]
 #![deprecated = "moved to the `three-d-io` crate"]
+
+use std::path::Path;
+use three_d_asset::Result;
 
 ///
 /// Functionality for loading any type of resource runtime on both desktop and web.
 ///
-pub type Loader = three_d_asset::Loader;
+pub struct Loader {}
 
-///
-/// Contains the resources loaded using the [Loader](crate::Loader) and/or manually inserted using the [insert_bytes](Self::insert_bytes) method.
-/// Use the [remove_bytes](crate::Loaded::remove_bytes) or [get_bytes](crate::Loaded::get_bytes) function to extract the raw byte array for the loaded resource
-/// or one of the other methods to both extract and deserialize a loaded resource.
-///
-pub type Loaded = three_d_asset::Loaded;
+impl Loader {
+    pub fn load(paths: &[impl AsRef<Path>], on_done: impl 'static + FnOnce(Result<Loaded>)) {
+        three_d_asset::io::load(paths, on_done)
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    pub fn load_blocking(paths: &[impl AsRef<Path>]) -> Result<Loaded> {
+        three_d_asset::io::load_blocking(paths)
+    }
+
+    pub async fn load_async(paths: &[impl AsRef<Path>]) -> Result<Loaded> {
+        three_d_asset::io::load_async(paths).await
+    }
+}
+
+pub type Loaded = three_d_asset::io::Loaded;
 
 ///
 /// Functionality for saving resources. Only available on desktop at the moment.
 ///
 #[cfg(not(target_arch = "wasm32"))]
-pub type Saver = three_d_asset::Saver;
+pub struct Saver {}
+
+#[cfg(not(target_arch = "wasm32"))]
+impl Saver {
+    pub fn save(path: impl AsRef<Path>, bytes: &[u8]) -> Result<()> {
+        three_d_asset::io::save(path, bytes)
+    }
+}
