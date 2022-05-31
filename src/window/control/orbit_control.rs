ef5b68e7fccd9f193b9a099b08e2313a39f03a14
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-05-31 14:57:35 +0200

    Fix #231

--- src/window/control/orbit_control.rs
+++ src/window/control/orbit_control.rs
@@ -32,9 +32,21 @@ impl OrbitControl {
         camera: &mut Camera,
         events: &mut [Event],
     ) -> ThreeDResult<bool> {
-        if let CameraAction::Zoom { speed, target, .. } = &mut self.control.scroll_horizontal {
-            *speed = 0.1 / target.distance(*camera.position());
+        if let CameraAction::Zoom {
+            speed,
+            target,
+            min,
+            max,
+        } = &mut self.control.scroll_vertical
+        {
+            let x = target.distance(*camera.position());
+            *speed = 0.5 * smoothstep(*min, *max, x) + 0.001;
         }
         self.control.handle_events(camera, events)
     }
+}
+
+fn smoothstep(edge0: f32, edge1: f32, x: f32) -> f32 {
+    let t = ((x - edge0) / (edge1 - edge0)).max(0.0).min(1.0);
+    t * t * (3.0 - 2.0 * t)
 }
