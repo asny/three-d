53f47b575fce60d2534cf34a87455a791fdb4d7c
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-08-03 09:57:08 +0200

    egui-gui feature is no longer default

--- src/gui.rs
+++ src/gui.rs
@@ -2,9 +2,9 @@
 //! Graphical User Interface support.
 //!
 
-#[cfg(feature = "gui")]
-#[cfg_attr(docsrs, doc(cfg(feature = "gui")))]
+#[cfg(feature = "egui-gui")]
+#[cfg_attr(docsrs, doc(cfg(feature = "egui-gui")))]
 mod egui_gui;
 #[doc(inline)]
-#[cfg(feature = "gui")]
+#[cfg(feature = "egui-gui")]
 pub use egui_gui::*;
