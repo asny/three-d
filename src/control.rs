9b88614fbbd8d022774e4e6e16820a0768e1d380
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-08-08 11:05:01 +0200

    FrameInput:: from_egui_input

--- src/control.rs
+++ src/control.rs
@@ -258,18 +258,110 @@ pub use egui_conversions::*;
 #[cfg(feature = "egui")]
 mod egui_conversions {
     use super::*;
-    impl From<egui::RawInput> for FrameInput {
-        fn from(frame_input: egui::RawInput) -> Self {
-            frame_input.into()
+    impl FrameInput {
+        pub fn from_egui_input(
+            context: &Context,
+            first_frame: bool,
+            input: egui::RawInput,
+        ) -> Self {
+            let modifiers = input.modifiers.into();
+            let mut events = Vec::new();
+            /*egui::Event::ModifiersChange { modifiers } => {
+                modifiers = modifiers;
+                events.push(Event::ModifiersChange { modifiers })
+            }*/
+
+            for event in input.events.into_iter() {
+                match event {
+                    egui::Event::Key {
+                        key,
+                        modifiers,
+                        pressed,
+                    } => {
+                        events.push(if pressed {
+                            Event::KeyPress {
+                                kind: key.into(),
+                                modifiers: modifiers.into(),
+                                handled: false,
+                            }
+                        } else {
+                            Event::KeyRelease {
+                                kind: key.into(),
+                                modifiers: modifiers.into(),
+                                handled: false,
+                            }
+                        });
+                    }
+                    egui::Event::PointerButton {
+                        button,
+                        pos,
+                        modifiers,
+                        pressed,
+                    } => events.push(if pressed {
+                        Event::MousePress {
+                            position: (pos.x as f64, pos.y as f64),
+                            button: button.into(),
+                            modifiers: modifiers.into(),
+                            handled: false,
+                        }
+                    } else {
+                        Event::MouseRelease {
+                            position: (pos.x as f64, pos.y as f64),
+                            button: button.into(),
+                            modifiers: modifiers.into(),
+                            handled: false,
+                        }
+                    }),
+                    egui::Event::PointerMoved(pos) => events.push(Event::MouseMotion {
+                        modifiers,
+                        delta: (0.0, 0.0), // TODO
+                        button: None,      // TODO
+                        position: (pos.x as f64, pos.y as f64),
+                        handled: false,
+                    }),
+                    egui::Event::Text(text) => events.push(Event::Text(text)),
+                    egui::Event::PointerGone => events.push(Event::MouseLeave),
+                    egui::Event::Scroll(delta) => events.push(Event::MouseWheel {
+                        delta: (delta.x as f64, delta.y as f64),
+                        handled: false,
+                        modifiers,
+                        position: (0.0, 0.0), // TODO
+                    }),
+                    _ => {}
+                };
+            }
+            let device_pixel_ratio = input.pixels_per_point.map(|v| v as f64).unwrap_or(1.0);
+            let window_width = input.screen_rect.map(|r| r.max.x as f64).unwrap_or(4096.0)
+                - input.screen_rect.map(|r| r.min.x as f64).unwrap_or(0.0);
+            let window_height = input.screen_rect.map(|r| r.max.y as f64).unwrap_or(4096.0)
+                - input.screen_rect.map(|r| r.min.y as f64).unwrap_or(0.0);
+            Self {
+                context: context.clone(),
+                viewport: Viewport {
+                    x: (input.screen_rect.map(|r| r.min.x as f64).unwrap_or(0.0)
+                        * device_pixel_ratio) as i32,
+                    y: (input.screen_rect.map(|r| r.min.x as f64).unwrap_or(0.0)
+                        * device_pixel_ratio) as i32,
+                    width: (window_width * device_pixel_ratio) as u32,
+                    height: (window_height * device_pixel_ratio) as u32,
+                },
+                window_width: window_width as u32,
+                window_height: window_height as u32,
+                device_pixel_ratio,
+                accumulated_time: input.time.unwrap_or(0.0) * 1000.0,
+                events,
+                first_frame,
+                elapsed_time: input.predicted_dt as f64,
+            }
         }
     }
 
-    impl From<FrameInput> for egui::RawInput {
-        fn from(frame_input: FrameInput) -> Self {
+    impl From<&FrameInput> for egui::RawInput {
+        fn from(frame_input: &FrameInput) -> Self {
             let mut egui_modifiers = egui::Modifiers::default();
             let events = frame_input
                 .events
-                .into_iter()
+                .iter()
                 .filter_map(|event| match event {
                     Event::KeyPress {
                         kind,
@@ -313,11 +405,7 @@ mod egui_conversions {
                                     x: position.0 as f32,
                                     y: position.1 as f32,
                                 },
-                                button: match button {
-                                    MouseButton::Left => egui::PointerButton::Primary,
-                                    MouseButton::Right => egui::PointerButton::Secondary,
-                                    MouseButton::Middle => egui::PointerButton::Middle,
-                                },
+                                button: button.into(),
                                 pressed: true,
                                 modifiers: modifiers.into(),
                             })
@@ -337,11 +425,7 @@ mod egui_conversions {
                                     x: position.0 as f32,
                                     y: position.1 as f32,
                                 },
-                                button: match button {
-                                    MouseButton::Left => egui::PointerButton::Primary,
-                                    MouseButton::Right => egui::PointerButton::Secondary,
-                                    MouseButton::Middle => egui::PointerButton::Middle,
-                                },
+                                button: button.into(),
                                 pressed: false,
                                 modifiers: modifiers.into(),
                             })
@@ -456,8 +540,8 @@ mod egui_conversions {
         }
     }
 
-    impl From<Key> for egui::Key {
-        fn from(key: Key) -> Self {
+    impl From<&Key> for egui::Key {
+        fn from(key: &Key) -> Self {
             use super::Key::*;
             use egui::Key;
             match key {
@@ -527,14 +611,34 @@ mod egui_conversions {
         }
     }
 
-    impl From<Modifiers> for egui::Modifiers {
-        fn from(modifiers: Modifiers) -> Self {
+    impl From<&Modifiers> for egui::Modifiers {
+        fn from(modifiers: &Modifiers) -> Self {
             Self {
                 alt: modifiers.alt,
                 ctrl: modifiers.ctrl,
                 shift: modifiers.shift,
                 command: modifiers.command,
                 mac_cmd: cfg!(target_os = "macos") && modifiers.command,
+            }
+        }
+    }
+
+    impl From<egui::PointerButton> for MouseButton {
+        fn from(button: egui::PointerButton) -> Self {
+            match button {
+                egui::PointerButton::Primary => MouseButton::Left,
+                egui::PointerButton::Secondary => MouseButton::Right,
+                egui::PointerButton::Middle => MouseButton::Middle,
+            }
+        }
+    }
+
+    impl From<&MouseButton> for egui::PointerButton {
+        fn from(button: &MouseButton) -> Self {
+            match button {
+                MouseButton::Left => egui::PointerButton::Primary,
+                MouseButton::Right => egui::PointerButton::Secondary,
+                MouseButton::Middle => egui::PointerButton::Middle,
             }
         }
     }
