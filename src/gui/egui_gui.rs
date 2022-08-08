0839b85d1cec934ebe169463415ead6dcce2034e
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-08-08 09:01:04 +0200

    Move FrameInput from window module to control

--- src/gui/egui_gui.rs
+++ src/gui/egui_gui.rs
@@ -42,27 +42,16 @@ impl GUI {
     /// Construct the GUI (Add panels, widgets etc.) using the [egui::Context] in the callback function.
     /// This function returns whether or not the GUI has changed, ie. if it consumes any events, and therefore needs to be rendered again.
     ///
-    #[cfg(not(feature = "window"))]
     pub fn update(
         &mut self,
-        frame_input: egui::RawInput,
+        frame_input: &mut FrameInput,
         callback: impl FnOnce(&egui::Context),
     ) -> bool {
-        self.update_internal(frame_input, callback)
-    }
+        self.width = frame_input.window_width;
+        self.height = frame_input.window_height;
+        self.egui_context.begin_frame(frame_input.clone().into());
+        callback(&self.egui_context);
 
-    ///
-    /// Initialises a new frame of the GUI and handles events.
-    /// Construct the GUI (Add panels, widgets etc.) using the [egui::Context] in the callback function.
-    /// This function returns whether or not the GUI has changed, ie. if it consumes any events, and therefore needs to be rendered again.
-    ///
-    #[cfg(feature = "window")]
-    pub fn update(
-        &mut self,
-        frame_input: &mut crate::window::FrameInput,
-        callback: impl FnOnce(&egui::Context),
-    ) -> bool {
-        let change = self.update_internal(frame_input.clone().into(), callback);
         for event in frame_input.events.iter_mut() {
             if self.egui_context.wants_pointer_input() {
                 match event {
@@ -106,18 +95,6 @@ impl GUI {
                 }
             }
         }
-        change
-    }
-
-    fn update_internal(
-        &mut self,
-        frame_input: egui::RawInput,
-        callback: impl FnOnce(&egui::Context),
-    ) -> bool {
-        self.width = frame_input.screen_rect.unwrap().max.x as u32;
-        self.height = frame_input.screen_rect.unwrap().max.y as u32;
-        self.egui_context.begin_frame(frame_input);
-        callback(&self.egui_context);
         self.egui_context.wants_pointer_input() || self.egui_context.wants_keyboard_input()
     }
 
@@ -143,200 +120,5 @@ impl GUI {
             use glow::HasContext as _;
             self.painter.gl().disable(glow::FRAMEBUFFER_SRGB);
         }
-    }
-}
-
-#[cfg(feature = "window")]
-impl From<crate::window::FrameInput> for egui::RawInput {
-    fn from(frame_input: crate::window::FrameInput) -> Self {
-        let mut egui_modifiers = egui::Modifiers::default();
-        let mut egui_events = Vec::new();
-        for event in frame_input.events {
-            match event {
-                Event::KeyPress {
-                    kind,
-                    modifiers,
-                    handled,
-                } => {
-                    if !handled {
-                        egui_events.push(egui::Event::Key {
-                            key: translate_to_egui_key_code(kind),
-                            pressed: true,
-                            modifiers: map_modifiers(modifiers),
-                        });
-                    }
-                }
-                Event::KeyRelease {
-                    kind,
-                    modifiers,
-                    handled,
-                } => {
-                    if !handled {
-                        egui_events.push(egui::Event::Key {
-                            key: translate_to_egui_key_code(kind),
-                            pressed: false,
-                            modifiers: map_modifiers(modifiers),
-                        });
-                    }
-                }
-                Event::MousePress {
-                    button,
-                    position,
-                    modifiers,
-                    handled,
-                } => {
-                    if !handled {
-                        egui_events.push(egui::Event::PointerButton {
-                            pos: egui::Pos2 {
-                                x: position.0 as f32,
-                                y: position.1 as f32,
-                            },
-                            button: match button {
-                                MouseButton::Left => egui::PointerButton::Primary,
-                                MouseButton::Right => egui::PointerButton::Secondary,
-                                MouseButton::Middle => egui::PointerButton::Middle,
-                            },
-                            pressed: true,
-                            modifiers: map_modifiers(modifiers),
-                        });
-                    }
-                }
-                Event::MouseRelease {
-                    button,
-                    position,
-                    modifiers,
-                    handled,
-                } => {
-                    if !handled {
-                        egui_events.push(egui::Event::PointerButton {
-                            pos: egui::Pos2 {
-                                x: position.0 as f32,
-                                y: position.1 as f32,
-                            },
-                            button: match button {
-                                MouseButton::Left => egui::PointerButton::Primary,
-                                MouseButton::Right => egui::PointerButton::Secondary,
-                                MouseButton::Middle => egui::PointerButton::Middle,
-                            },
-                            pressed: false,
-                            modifiers: map_modifiers(modifiers),
-                        });
-                    }
-                }
-                Event::MouseMotion {
-                    position, handled, ..
-                } => {
-                    if !handled {
-                        egui_events.push(egui::Event::PointerMoved(egui::Pos2 {
-                            x: position.0 as f32,
-                            y: position.1 as f32,
-                        }));
-                    }
-                }
-                Event::Text(text) => {
-                    egui_events.push(egui::Event::Text(text.clone()));
-                }
-                Event::MouseLeave => {
-                    egui_events.push(egui::Event::PointerGone);
-                }
-                Event::MouseWheel { delta, handled, .. } => {
-                    if !handled {
-                        egui_events.push(egui::Event::Scroll(egui::Vec2::new(
-                            delta.0 as f32,
-                            delta.1 as f32,
-                        )));
-                    }
-                }
-                Event::ModifiersChange { modifiers } => egui_modifiers = map_modifiers(modifiers),
-                _ => (),
-            }
-        }
-        egui::RawInput {
-            screen_rect: Some(egui::Rect::from_min_size(
-                Default::default(),
-                egui::Vec2 {
-                    x: frame_input.window_width as f32,
-                    y: frame_input.window_height as f32,
-                },
-            )),
-            pixels_per_point: Some(frame_input.device_pixel_ratio as f32),
-            time: Some(frame_input.accumulated_time * 0.001),
-            modifiers: egui_modifiers,
-            events: egui_events,
-            ..Default::default()
-        }
-    }
-}
-
-fn translate_to_egui_key_code(key: crate::Key) -> egui::Key {
-    use crate::Key::*;
-    use egui::Key;
-
-    match key {
-        ArrowDown => Key::ArrowDown,
-        ArrowLeft => Key::ArrowLeft,
-        ArrowRight => Key::ArrowRight,
-        ArrowUp => Key::ArrowUp,
-
-        Escape => Key::Escape,
-        Tab => Key::Tab,
-        Backspace => Key::Backspace,
-        Enter => Key::Enter,
-        Space => Key::Space,
-
-        Insert => Key::Insert,
-        Delete => Key::Delete,
-        Home => Key::Home,
-        End => Key::End,
-        PageUp => Key::PageUp,
-        PageDown => Key::PageDown,
-
-        Num0 => Key::Num0,
-        Num1 => Key::Num1,
-        Num2 => Key::Num2,
-        Num3 => Key::Num3,
-        Num4 => Key::Num4,
-        Num5 => Key::Num5,
-        Num6 => Key::Num6,
-        Num7 => Key::Num7,
-        Num8 => Key::Num8,
-        Num9 => Key::Num9,
-
-        A => Key::A,
-        B => Key::B,
-        C => Key::C,
-        D => Key::D,
-        E => Key::E,
-        F => Key::F,
-        G => Key::G,
-        H => Key::H,
-        I => Key::I,
-        J => Key::J,
-        K => Key::K,
-        L => Key::L,
-        M => Key::M,
-        N => Key::N,
-        O => Key::O,
-        P => Key::P,
-        Q => Key::Q,
-        R => Key::R,
-        S => Key::S,
-        T => Key::T,
-        U => Key::U,
-        V => Key::V,
-        W => Key::W,
-        X => Key::X,
-        Y => Key::Y,
-        Z => Key::Z,
-    }
-}
-
-fn map_modifiers(modifiers: Modifiers) -> egui::Modifiers {
-    egui::Modifiers {
-        alt: modifiers.alt,
-        ctrl: modifiers.ctrl,
-        shift: modifiers.shift,
-        command: modifiers.command,
-        mac_cmd: cfg!(target_os = "macos") && modifiers.command,
     }
 }
--- src/window.rs
+++ src/window.rs
@@ -72,53 +72,6 @@ use crate::control::*;
 use crate::core::*;
 
 ///
-/// Input from the window to the rendering (and whatever else needs it) each frame.
-///
-#[derive(Clone, Debug)]
-pub struct FrameInput {
-    /// A list of [events](crate::Event) which has occurred since last frame.
-    pub events: Vec<Event>,
-
-    /// Milliseconds since last frame.
-    pub elapsed_time: f64,
-
-    /// Milliseconds accumulated time since start.
-    pub accumulated_time: f64,
-
-    /// Viewport of the window in physical pixels (the size of the screen [RenderTarget] which is returned from [FrameInput::screen]).
-    pub viewport: Viewport,
-
-    /// Width of the window in logical pixels.
-    pub window_width: u32,
-
-    /// Height of the window in logical pixels.
-    pub window_height: u32,
-
-    /// Number of physical pixels for each logical pixel.
-    pub device_pixel_ratio: f64,
-
-    /// Whether or not this is the first frame.
-    pub first_frame: bool,
-
-    /// The graphics context for the window.
-    pub context: Context,
-}
-
-impl FrameInput {
-    ///
-    /// Returns the screen render target, which is used for drawing to the screen, for this window.
-    /// Same as
-    ///
-    /// ```notrust
-    /// RenderTarget::screen(&frame_input.context, frame_input.viewport.width, frame_input.viewport.height)
-    /// ```
-    ///
-    pub fn screen(&self) -> RenderTarget {
-        RenderTarget::screen(&self.context, self.viewport.width, self.viewport.height)
-    }
-}
-
-///
 /// Output from the rendering to the window each frame.
 ///
 #[derive(Clone, Debug)]
