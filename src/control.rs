37cfaaf1c52ffa19f9f4d73267fd36d2cb9b4a91
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-08-08 09:10:21 +0200

    clean-up

--- src/control.rs
+++ src/control.rs
@@ -253,205 +253,204 @@ pub struct Modifiers {
 }
 
 #[cfg(feature = "egui")]
-impl From<egui::RawInput> for FrameInput {
-    fn from(frame_input: egui::RawInput) -> Self {
-        frame_input.into()
-    }
-}
+pub use egui_conversions::*;
 
 #[cfg(feature = "egui")]
-impl From<FrameInput> for egui::RawInput {
-    fn from(frame_input: FrameInput) -> Self {
-        let mut egui_modifiers = egui::Modifiers::default();
-        let mut egui_events = Vec::new();
-        for event in frame_input.events {
-            match event {
-                Event::KeyPress {
-                    kind,
-                    modifiers,
-                    handled,
-                } => {
-                    if !handled {
-                        egui_events.push(egui::Event::Key {
-                            key: translate_to_egui_key_code(kind),
-                            pressed: true,
-                            modifiers: map_modifiers(modifiers),
-                        });
+mod egui_conversions {
+    use super::*;
+    impl From<egui::RawInput> for FrameInput {
+        fn from(frame_input: egui::RawInput) -> Self {
+            frame_input.into()
+        }
+    }
+
+    impl From<FrameInput> for egui::RawInput {
+        fn from(frame_input: FrameInput) -> Self {
+            let mut egui_modifiers = egui::Modifiers::default();
+            let mut egui_events = Vec::new();
+            for event in frame_input.events {
+                match event {
+                    Event::KeyPress {
+                        kind,
+                        modifiers,
+                        handled,
+                    } => {
+                        if !handled {
+                            egui_events.push(egui::Event::Key {
+                                key: translate_to_egui_key_code(kind),
+                                pressed: true,
+                                modifiers: map_modifiers(modifiers),
+                            });
+                        }
                     }
-                }
-                Event::KeyRelease {
-                    kind,
-                    modifiers,
-                    handled,
-                } => {
-                    if !handled {
-                        egui_events.push(egui::Event::Key {
-                            key: translate_to_egui_key_code(kind),
-                            pressed: false,
-                            modifiers: map_modifiers(modifiers),
-                        });
+                    Event::KeyRelease {
+                        kind,
+                        modifiers,
+                        handled,
+                    } => {
+                        if !handled {
+                            egui_events.push(egui::Event::Key {
+                                key: translate_to_egui_key_code(kind),
+                                pressed: false,
+                                modifiers: map_modifiers(modifiers),
+                            });
+                        }
                     }
-                }
-                Event::MousePress {
-                    button,
-                    position,
-                    modifiers,
-                    handled,
-                } => {
-                    if !handled {
-                        egui_events.push(egui::Event::PointerButton {
-                            pos: egui::Pos2 {
+                    Event::MousePress {
+                        button,
+                        position,
+                        modifiers,
+                        handled,
+                    } => {
+                        if !handled {
+                            egui_events.push(egui::Event::PointerButton {
+                                pos: egui::Pos2 {
+                                    x: position.0 as f32,
+                                    y: position.1 as f32,
+                                },
+                                button: match button {
+                                    MouseButton::Left => egui::PointerButton::Primary,
+                                    MouseButton::Right => egui::PointerButton::Secondary,
+                                    MouseButton::Middle => egui::PointerButton::Middle,
+                                },
+                                pressed: true,
+                                modifiers: map_modifiers(modifiers),
+                            });
+                        }
+                    }
+                    Event::MouseRelease {
+                        button,
+                        position,
+                        modifiers,
+                        handled,
+                    } => {
+                        if !handled {
+                            egui_events.push(egui::Event::PointerButton {
+                                pos: egui::Pos2 {
+                                    x: position.0 as f32,
+                                    y: position.1 as f32,
+                                },
+                                button: match button {
+                                    MouseButton::Left => egui::PointerButton::Primary,
+                                    MouseButton::Right => egui::PointerButton::Secondary,
+                                    MouseButton::Middle => egui::PointerButton::Middle,
+                                },
+                                pressed: false,
+                                modifiers: map_modifiers(modifiers),
+                            });
+                        }
+                    }
+                    Event::MouseMotion {
+                        position, handled, ..
+                    } => {
+                        if !handled {
+                            egui_events.push(egui::Event::PointerMoved(egui::Pos2 {
                                 x: position.0 as f32,
                                 y: position.1 as f32,
-                            },
-                            button: match button {
-                                MouseButton::Left => egui::PointerButton::Primary,
-                                MouseButton::Right => egui::PointerButton::Secondary,
-                                MouseButton::Middle => egui::PointerButton::Middle,
-                            },
-                            pressed: true,
-                            modifiers: map_modifiers(modifiers),
-                        });
+                            }));
+                        }
                     }
-                }
-                Event::MouseRelease {
-                    button,
-                    position,
-                    modifiers,
-                    handled,
-                } => {
-                    if !handled {
-                        egui_events.push(egui::Event::PointerButton {
-                            pos: egui::Pos2 {
-                                x: position.0 as f32,
-                                y: position.1 as f32,
-                            },
-                            button: match button {
-                                MouseButton::Left => egui::PointerButton::Primary,
-                                MouseButton::Right => egui::PointerButton::Secondary,
-                                MouseButton::Middle => egui::PointerButton::Middle,
-                            },
-                            pressed: false,
-                            modifiers: map_modifiers(modifiers),
-                        });
+                    Event::Text(text) => {
+                        egui_events.push(egui::Event::Text(text.clone()));
                     }
-                }
-                Event::MouseMotion {
-                    position, handled, ..
-                } => {
-                    if !handled {
-                        egui_events.push(egui::Event::PointerMoved(egui::Pos2 {
-                            x: position.0 as f32,
-                            y: position.1 as f32,
-                        }));
+                    Event::MouseLeave => {
+                        egui_events.push(egui::Event::PointerGone);
                     }
-                }
-                Event::Text(text) => {
-                    egui_events.push(egui::Event::Text(text.clone()));
-                }
-                Event::MouseLeave => {
-                    egui_events.push(egui::Event::PointerGone);
-                }
-                Event::MouseWheel { delta, handled, .. } => {
-                    if !handled {
-                        egui_events.push(egui::Event::Scroll(egui::Vec2::new(
-                            delta.0 as f32,
-                            delta.1 as f32,
-                        )));
+                    Event::MouseWheel { delta, handled, .. } => {
+                        if !handled {
+                            egui_events.push(egui::Event::Scroll(egui::Vec2::new(
+                                delta.0 as f32,
+                                delta.1 as f32,
+                            )));
+                        }
                     }
+                    Event::ModifiersChange { modifiers } => {
+                        egui_modifiers = map_modifiers(modifiers)
+                    }
+                    _ => (),
                 }
-                Event::ModifiersChange { modifiers } => egui_modifiers = map_modifiers(modifiers),
-                _ => (),
+            }
+            egui::RawInput {
+                screen_rect: Some(egui::Rect::from_min_size(
+                    Default::default(),
+                    egui::Vec2 {
+                        x: frame_input.window_width as f32,
+                        y: frame_input.window_height as f32,
+                    },
+                )),
+                pixels_per_point: Some(frame_input.device_pixel_ratio as f32),
+                time: Some(frame_input.accumulated_time * 0.001),
+                modifiers: egui_modifiers,
+                events: egui_events,
+                ..Default::default()
             }
         }
-        egui::RawInput {
-            screen_rect: Some(egui::Rect::from_min_size(
-                Default::default(),
-                egui::Vec2 {
-                    x: frame_input.window_width as f32,
-                    y: frame_input.window_height as f32,
-                },
-            )),
-            pixels_per_point: Some(frame_input.device_pixel_ratio as f32),
-            time: Some(frame_input.accumulated_time * 0.001),
-            modifiers: egui_modifiers,
-            events: egui_events,
-            ..Default::default()
+    }
+    fn translate_to_egui_key_code(key: crate::Key) -> egui::Key {
+        use crate::Key::*;
+        use egui::Key;
+        match key {
+            ArrowDown => Key::ArrowDown,
+            ArrowLeft => Key::ArrowLeft,
+            ArrowRight => Key::ArrowRight,
+            ArrowUp => Key::ArrowUp,
+            Escape => Key::Escape,
+            Tab => Key::Tab,
+            Backspace => Key::Backspace,
+            Enter => Key::Enter,
+            Space => Key::Space,
+            Insert => Key::Insert,
+            Delete => Key::Delete,
+            Home => Key::Home,
+            End => Key::End,
+            PageUp => Key::PageUp,
+            PageDown => Key::PageDown,
+            Num0 => Key::Num0,
+            Num1 => Key::Num1,
+            Num2 => Key::Num2,
+            Num3 => Key::Num3,
+            Num4 => Key::Num4,
+            Num5 => Key::Num5,
+            Num6 => Key::Num6,
+            Num7 => Key::Num7,
+            Num8 => Key::Num8,
+            Num9 => Key::Num9,
+            A => Key::A,
+            B => Key::B,
+            C => Key::C,
+            D => Key::D,
+            E => Key::E,
+            F => Key::F,
+            G => Key::G,
+            H => Key::H,
+            I => Key::I,
+            J => Key::J,
+            K => Key::K,
+            L => Key::L,
+            M => Key::M,
+            N => Key::N,
+            O => Key::O,
+            P => Key::P,
+            Q => Key::Q,
+            R => Key::R,
+            S => Key::S,
+            T => Key::T,
+            U => Key::U,
+            V => Key::V,
+            W => Key::W,
+            X => Key::X,
+            Y => Key::Y,
+            Z => Key::Z,
         }
     }
-}
 
-#[cfg(feature = "egui")]
-fn translate_to_egui_key_code(key: crate::Key) -> egui::Key {
-    use crate::Key::*;
-    use egui::Key;
-
-    match key {
-        ArrowDown => Key::ArrowDown,
-        ArrowLeft => Key::ArrowLeft,
-        ArrowRight => Key::ArrowRight,
-        ArrowUp => Key::ArrowUp,
-
-        Escape => Key::Escape,
-        Tab => Key::Tab,
-        Backspace => Key::Backspace,
-        Enter => Key::Enter,
-        Space => Key::Space,
-
-        Insert => Key::Insert,
-        Delete => Key::Delete,
-        Home => Key::Home,
-        End => Key::End,
-        PageUp => Key::PageUp,
-        PageDown => Key::PageDown,
-
-        Num0 => Key::Num0,
-        Num1 => Key::Num1,
-        Num2 => Key::Num2,
-        Num3 => Key::Num3,
-        Num4 => Key::Num4,
-        Num5 => Key::Num5,
-        Num6 => Key::Num6,
-        Num7 => Key::Num7,
-        Num8 => Key::Num8,
-        Num9 => Key::Num9,
-
-        A => Key::A,
-        B => Key::B,
-        C => Key::C,
-        D => Key::D,
-        E => Key::E,
-        F => Key::F,
-        G => Key::G,
-        H => Key::H,
-        I => Key::I,
-        J => Key::J,
-        K => Key::K,
-        L => Key::L,
-        M => Key::M,
-        N => Key::N,
-        O => Key::O,
-        P => Key::P,
-        Q => Key::Q,
-        R => Key::R,
-        S => Key::S,
-        T => Key::T,
-        U => Key::U,
-        V => Key::V,
-        W => Key::W,
-        X => Key::X,
-        Y => Key::Y,
-        Z => Key::Z,
-    }
-}
-
-#[cfg(feature = "egui")]
-fn map_modifiers(modifiers: Modifiers) -> egui::Modifiers {
-    egui::Modifiers {
-        alt: modifiers.alt,
-        ctrl: modifiers.ctrl,
-        shift: modifiers.shift,
-        command: modifiers.command,
-        mac_cmd: cfg!(target_os = "macos") && modifiers.command,
+    fn map_modifiers(modifiers: Modifiers) -> egui::Modifiers {
+        egui::Modifiers {
+            alt: modifiers.alt,
+            ctrl: modifiers.ctrl,
+            shift: modifiers.shift,
+            command: modifiers.command,
+            mac_cmd: cfg!(target_os = "macos") && modifiers.command,
+        }
     }
 }
