4a61044646e98fd7b3e3fb41df774dc407b5e07e
Asger Nyman Christiansen <asgernyman@gmail.com>
2022-08-17 21:13:49 +0200

    Point cloud example is working

--- examples/point_cloud/src/main.rs
+++ examples/point_cloud/src/main.rs
@@ -15,19 +15,17 @@ pub async fn run() {
         ..Default::default()
     })
     .unwrap();
-    let context = window.gl().unwrap();
+    let context = window.gl();
 
     let mut camera = Camera::new_perspective(
-        &context,
-        window.viewport().unwrap(),
+        window.viewport(),
         vec3(0.25, -0.5, -2.0),
         vec3(0.0, 0.0, 0.0),
         vec3(0.0, 1.0, 0.0),
         degrees(45.0),
         0.1,
         100.0,
-    )
-    .unwrap();
+    );
     let mut control = OrbitControl::new(*camera.target(), 0.1, 100.0);
 
     let point_material = PhysicalMaterial {
@@ -41,50 +39,40 @@ pub async fn run() {
         ..Default::default()
     };
 
-    let mut loaded = Loader::load_async(&["examples/assets/hand.pcd"])
+    let mut loaded = three_d_asset::io::load_async(&["examples/assets/hand.pcd"])
         .await
         .unwrap();
-    let cpu_point_cloud = loaded.pcd("examples/assets/hand.pcd", true, true).unwrap();
-
-    // let cpu_point_cloud = CpuPointCloud::random(250_000);
+    let cpu_point_cloud = loaded.deserialize("hand.pcd").unwrap();
 
     let point_cloud = Gm {
-        geometry: PointCloud::new(&context, cpu_point_cloud).unwrap(),
+        geometry: PointCloud::new(&context, cpu_point_cloud),
         material: point_material,
     };
 
-    let ambient = AmbientLight::new(&context, 0.4, Color::WHITE).unwrap();
-    let directional1 =
-        DirectionalLight::new(&context, 2.0, Color::WHITE, &vec3(-1.0, -1.0, -1.0)).unwrap();
-    let directional2 =
-        DirectionalLight::new(&context, 2.0, Color::WHITE, &vec3(1.0, 1.0, 1.0)).unwrap();
+    let ambient = AmbientLight::new(&context, 0.4, Color::WHITE);
+    let directional1 = DirectionalLight::new(&context, 2.0, Color::WHITE, &vec3(-1.0, -1.0, -1.0));
+    let directional2 = DirectionalLight::new(&context, 2.0, Color::WHITE, &vec3(1.0, 1.0, 1.0));
 
     // main loop
-    window
-        .render_loop(move |mut frame_input| {
-            let mut redraw = frame_input.first_frame;
-            redraw |= camera.set_viewport(frame_input.viewport).unwrap();
-            redraw |= control
-                .handle_events(&mut camera, &mut frame_input.events)
-                .unwrap();
+    window.render_loop(move |mut frame_input| {
+        let mut redraw = frame_input.first_frame;
+        redraw |= camera.set_viewport(frame_input.viewport);
+        redraw |= control.handle_events(&mut camera, &mut frame_input.events);
 
-            if redraw {
-                frame_input
-                    .screen()
-                    .clear(ClearState::color_and_depth(1.0, 1.0, 1.0, 1.0, 1.0))
-                    .unwrap()
-                    .render(
-                        &camera,
-                        &[&point_cloud],
-                        &[&ambient, &directional1, &directional2],
-                    )
-                    .unwrap();
-            }
+        if redraw {
+            frame_input
+                .screen()
+                .clear(ClearState::color_and_depth(1.0, 1.0, 1.0, 1.0, 1.0))
+                .render(
+                    &camera,
+                    &[&point_cloud],
+                    &[&ambient, &directional1, &directional2],
+                );
+        }
 
-            FrameOutput {
-                swap_buffers: redraw,
-                ..Default::default()
-            }
-        })
-        .unwrap();
+        FrameOutput {
+            swap_buffers: redraw,
+            ..Default::default()
+        }
+    });
 }
--- src/renderer/geometry/point_cloud.rs
+++ src/renderer/geometry/point_cloud.rs
@@ -1,6 +1,8 @@
 use crate::core::*;
 use crate::renderer::*;
 
+pub use three_d_asset::PointCloud as CpuPointCloud;
+
 ///
 /// Wraps [InstancedMesh] to render many instances of the same point mesh (Sphere) efficiently.
 ///
@@ -11,7 +13,7 @@ impl PointCloud {
     /// Creates a new PointCloud from the given [CpuPointCloud].
     /// All data in the [CpuPointCloud] is transfered to the GPU, so make sure to remove all unnecessary data from the [CpuPointCloud] before calling this method.
     ///
-    pub fn new(context: &Context, cpu_point_cloud: CpuPointCloud) -> ThreeDResult<Self> {
+    pub fn new(context: &Context, cpu_point_cloud: CpuPointCloud) -> Self {
         let instances = Instances {
             translations: cpu_point_cloud.positions.to_f32(),
             colors: cpu_point_cloud.colors,
@@ -19,9 +21,9 @@ impl PointCloud {
         };
 
         let mut point = CpuMesh::cube();
-        point.transform(&Mat4::from_scale(0.001))?;
+        point.transform(&Mat4::from_scale(0.001)).unwrap();
 
-        Ok(Self(InstancedMesh::new(context, &instances, &point)?))
+        Self(InstancedMesh::new(context, &instances, &point))
     }
 }
 
@@ -35,7 +37,7 @@ impl Geometry for PointCloud {
         material: &dyn Material,
         camera: &Camera,
         lights: &[&dyn Light],
-    ) -> ThreeDResult<()> {
+    ) {
         self.0.render_with_material(material, camera, lights)
     }
 }
