f533fe10f42e537ac9213bf8319b5f8d8391a971
Brend Wanders <brend@13w.nl>
2023-01-14 12:14:42 +0100

    Switch to macros that understand some generic args.

--- src/core/render_target.rs
+++ src/core/render_target.rs
@@ -487,29 +487,54 @@ fn new_framebuffer(context: &Context) -> crate::context::Framebuffer {
     }
 }
 
-macro_rules! impl_render_target_core_extensions {
-    ($t:ty) => {
-        impl $t {
-            ///
-            /// Returns the scissor box that encloses the entire target.
-            ///
-            pub fn scissor_box(&self) -> ScissorBox {
-                ScissorBox::new_at_origo(self.width(), self.height())
-            }
+macro_rules! impl_render_target_core_extensions_body {
+    () => {
+        ///
+        /// Returns the scissor box that encloses the entire target.
+        ///
+        pub fn scissor_box(&self) -> ScissorBox {
+            ScissorBox::new_at_origo(self.width(), self.height())
+        }
 
-            ///
-            /// Returns the viewport that encloses the entire target.
-            ///
-            pub fn viewport(&self) -> Viewport {
-                Viewport::new_at_origo(self.width(), self.height())
-            }
+        ///
+        /// Returns the viewport that encloses the entire target.
+        ///
+        pub fn viewport(&self) -> Viewport {
+            Viewport::new_at_origo(self.width(), self.height())
+        }
+    }
+}
+
+macro_rules! impl_render_target_core_extensions {
+    // 2 generic arguments with bounds
+    ($name:ident < $a:ident : $ta:tt , $b:ident : $tb:tt >) => {
+        impl<$a: $ta, $b: $tb> $name<$a, $b> {
+            impl_render_target_core_extensions_body!();
+        }
+    };
+    // 1 generic argument with bound
+    ($name:ident < $a:ident : $ta:tt >) => {
+        impl<$a: $ta> $name<$a> {
+            impl_render_target_core_extensions_body!();
+        }
+    };
+    // 1 liftetime argument
+    ($name:ident < $lt:lifetime >) => {
+        impl<$lt> $name<$lt> {
+            impl_render_target_core_extensions_body!();
+        }
+    };
+    // without any arguments
+    ($name:ty) => {
+        impl $name {
+            impl_render_target_core_extensions_body!();
         }
     };
 }
 
-impl_render_target_core_extensions!(RenderTarget<'_>);
-impl_render_target_core_extensions!(ColorTarget<'_>);
-impl_render_target_core_extensions!(DepthTarget<'_>);
+impl_render_target_core_extensions!(RenderTarget<'a>);
+impl_render_target_core_extensions!(ColorTarget<'a>);
+impl_render_target_core_extensions!(DepthTarget<'a>);
 impl_render_target_core_extensions!(RenderTargetMultisample);
 impl_render_target_core_extensions!(ColorTargetMultisample);
 impl_render_target_core_extensions!(DepthTargetMultisample);
