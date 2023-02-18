1a5c136b193e4fcd90f296245781052225401d52
Asger Nyman Christiansen <asgernyman@gmail.com>
2023-02-18 20:14:00 +0100

    Docs

--- src/renderer/material.rs
+++ src/renderer/material.rs
@@ -136,9 +136,12 @@ impl FragmentAttributes {
     };
 }
 
+/// Description of a fragment shader
 #[derive(Debug, Clone)]
 pub struct FragmentShader {
+    /// The fragment shader source code
     pub source: String,
+    /// The attributes used by this fragment shader, ie. the input from the vertex shader.
     pub attributes: FragmentAttributes,
 }
 
