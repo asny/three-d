c3f4e18ef49909b54c6a5f453718fdd7b13d1c5d
Asger Nyman Christiansen <asgernyman@gmail.com>
2023-03-31 14:57:25 +0200

    Update winit window example

--- examples/winit_window/src/main.rs
+++ examples/winit_window/src/main.rs
@@ -8,32 +8,24 @@ pub fn main() {
     // Create camera
     let mut camera = Camera::new_perspective(
         Viewport::new_at_origo(1, 1),
-        vec3(0.0, 0.0, 2.0),
+        vec3(0.0, 2.0, 4.0),
         vec3(0.0, 0.0, 0.0),
         vec3(0.0, 1.0, 0.0),
         degrees(45.0),
         0.1,
         10.0,
     );
+    let mut control = OrbitControl::new(*camera.target(), 1.0, 100.0);
 
-    // Create triangle model
-    let positions = vec![
-        vec3(0.5, -0.5, 0.0),  // bottom right
-        vec3(-0.5, -0.5, 0.0), // bottom left
-        vec3(0.0, 0.5, 0.0),   // top
-    ];
-    let colors = vec![
-        Color::new(255, 0, 0, 255), // bottom right
-        Color::new(0, 255, 0, 255), // bottom left
-        Color::new(0, 0, 255, 255), // top
-    ];
-    let cpu_mesh = CpuMesh {
-        positions: Positions::F32(positions),
-        colors: Some(colors),
-        ..Default::default()
-    };
-    let mut model = Gm::new(Mesh::new(&context, &cpu_mesh), ColorMaterial::default());
-    model.set_animation(|time| Mat4::from_angle_y(radians(time * 0.005)));
+    // Create model
+    let mut model = Gm::new(
+        Mesh::new(&context, &CpuMesh::cube()),
+        ColorMaterial {
+            color: Color::GREEN,
+            ..Default::default()
+        },
+    );
+    model.set_animation(|time| Mat4::from_angle_y(radians(time * 0.0005)));
 
     // Event loop
     let mut event_handler = EventHandler::new();
@@ -45,8 +37,9 @@ pub fn main() {
                 window.request_redraw();
             }
             winit::event::Event::RedrawRequested(_) => {
-                let frame_input = event_handler.resolve(&context);
+                let mut frame_input = event_handler.resolve(&context);
 
+                control.handle_events(&mut camera, &mut frame_input.events);
                 camera.set_viewport(frame_input.viewport);
                 model.animate(frame_input.accumulated_time as f32);
                 frame_input
