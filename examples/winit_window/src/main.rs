74331f2b8087b5e28a9db9bc2b24855778372c7f
Asger Nyman Christiansen <asgernyman@gmail.com>
2023-03-31 14:48:56 +0200

    winit window example

---
+++ examples/winit_window/src/main.rs
@@ -0,0 +1,76 @@
+use three_d::{renderer::*, EventHandler, SurfaceSettings, WindowedContext};
+
+pub fn main() {
+    let event_loop = winit::event_loop::EventLoop::new();
+    let window = winit::window::Window::new(&event_loop).unwrap();
+    let context = WindowedContext::from_winit_window(&window, SurfaceSettings::default()).unwrap();
+
+    // Create camera
+    let mut camera = Camera::new_perspective(
+        Viewport::new_at_origo(1, 1),
+        vec3(0.0, 0.0, 2.0),
+        vec3(0.0, 0.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        degrees(45.0),
+        0.1,
+        10.0,
+    );
+
+    // Create triangle model
+    let positions = vec![
+        vec3(0.5, -0.5, 0.0),  // bottom right
+        vec3(-0.5, -0.5, 0.0), // bottom left
+        vec3(0.0, 0.5, 0.0),   // top
+    ];
+    let colors = vec![
+        Color::new(255, 0, 0, 255), // bottom right
+        Color::new(0, 255, 0, 255), // bottom left
+        Color::new(0, 0, 255, 255), // top
+    ];
+    let cpu_mesh = CpuMesh {
+        positions: Positions::F32(positions),
+        colors: Some(colors),
+        ..Default::default()
+    };
+    let mut model = Gm::new(Mesh::new(&context, &cpu_mesh), ColorMaterial::default());
+    model.set_animation(|time| Mat4::from_angle_y(radians(time * 0.005)));
+
+    // Event loop
+    let mut event_handler = EventHandler::new();
+    event_loop.run(move |event, _, control_flow| {
+        event_handler.handle_event(&event);
+
+        match event {
+            winit::event::Event::MainEventsCleared => {
+                window.request_redraw();
+            }
+            winit::event::Event::RedrawRequested(_) => {
+                let frame_input = event_handler.resolve(&context);
+
+                camera.set_viewport(frame_input.viewport);
+                model.animate(frame_input.accumulated_time as f32);
+                frame_input
+                    .screen()
+                    .clear(ClearState::color_and_depth(0.8, 0.8, 0.8, 1.0, 1.0))
+                    .render(&camera, &model, &[]);
+
+                context.swap_buffers().unwrap();
+                control_flow.set_poll();
+                window.request_redraw();
+            }
+            winit::event::Event::WindowEvent { ref event, .. } => match event {
+                winit::event::WindowEvent::Resized(physical_size) => {
+                    context.resize(*physical_size);
+                }
+                winit::event::WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                    context.resize(**new_inner_size);
+                }
+                winit::event::WindowEvent::CloseRequested => {
+                    control_flow.set_exit();
+                }
+                _ => (),
+            },
+            _ => {}
+        }
+    });
+}
