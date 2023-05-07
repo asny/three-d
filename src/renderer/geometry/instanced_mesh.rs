c8c1d7d24fd4cda7f2a52ef7e719bdb867690436
Asger Nyman Christiansen <asgernyman@gmail.com>
2023-05-07 20:46:05 +0200

    Remove InstancedMesh::set_instance_count

--- src/renderer/geometry/instanced_mesh.rs
+++ src/renderer/geometry/instanced_mesh.rs
@@ -17,7 +17,6 @@ pub struct InstancedMesh {
     transformation: Mat4,
     current_transformation: Mat4,
     animation: Option<Box<dyn Fn(f32) -> Mat4 + Send + Sync>>,
-    instance_count: u32,
     instances: Instances,
 }
 
@@ -38,7 +37,6 @@ impl InstancedMesh {
             transformation: Mat4::identity(),
             current_transformation: Mat4::identity(),
             animation: None,
-            instance_count: 0,
             instances: instances.clone(),
         };
         instanced_mesh.set_instances(instances);
@@ -72,15 +70,7 @@ impl InstancedMesh {
 
     /// Returns the number of instances that is rendered.
     pub fn instance_count(&self) -> u32 {
-        self.instance_count
-    }
-
-    /// Use this if you only want to render instance 0 through to instance `instance_count`.
-    /// This is the same as changing the instances using `set_instances`, except that it is faster since it doesn't update any buffers.
-    /// `instance_count` will be set to the number of instances when they are defined by `set_instances`, so all instanced are rendered by default.
-    pub fn set_instance_count(&mut self, instance_count: u32) {
-        self.instance_count = instance_count.min(self.instances.transformations.len() as u32);
-        self.update_aabb();
+        self.instances.count()
     }
 
     ///
@@ -89,79 +79,20 @@ impl InstancedMesh {
     pub fn set_instances(&mut self, instances: &Instances) {
         #[cfg(debug_assertions)]
         instances.validate().expect("invalid instances");
-        self.instance_count = instances.count();
         self.instances = instances.clone();
         self.update_aabb();
 
-        {
-            let mut s = self
-                .instance_buffers
-                .write()
-                .expect("failed acquiring write accesss");
-            s.0.clear();
-        }
+        self.instance_buffers.write().unwrap().0.clear();
     }
 
     fn update_aabb(&mut self) {
         let mut aabb = AxisAlignedBoundingBox::EMPTY;
-        for i in 0..self.instance_count as usize {
+        for transformation in self.instances.transformations.iter() {
             let mut aabb2 = self.aabb_local;
-            aabb2.transform(&(self.instances.transformations[i] * self.transformation));
+            aabb2.transform(&(transformation * self.transformation));
             aabb.expand_with_aabb(&aabb2);
         }
         self.aabb = aabb;
-    }
-
-    /// Update the instance buffers, if depth_ordering_pose is populated depth ordering is performed
-    /// using this position.
-    fn update_instance_buffers(&self, depth_ordering_pose: Option<Vec3>) {
-        let needs_update = {
-            let s = self
-                .instance_buffers
-                .read()
-                .expect("failed acquiring read accesss");
-
-            // Check if we need a reorder, this only applies to transparent materials.
-            let reorder_needed = if let Some(ref ordering_pose) = depth_ordering_pose {
-                let camera_changed = *ordering_pose != s.1;
-                let instance_count_changed = if let Some(v) = s.0.values().next() {
-                    v.instance_count() != self.instance_count
-                } else {
-                    false
-                };
-
-                camera_changed || instance_count_changed
-            } else {
-                false
-            };
-
-            // Update is always needed if the instance buffers is empty; Opaque materials only.
-            // Or, for transparent materials, if the camera moved or if the instance count changed.
-            s.0.is_empty() || reorder_needed
-        };
-
-        if needs_update {
-            let mut s = self
-                .instance_buffers
-                .write()
-                .expect("failed acquiring mutable access");
-            s.0 = self.create_instance_buffers(depth_ordering_pose);
-            if let Some(ordering_pose) = depth_ordering_pose {
-                s.1 = ordering_pose;
-            }
-        }
-    }
-
-    /// Sort function to order the indices up to instance count by their distance, back to front.
-    fn ordered_indices_back_to_front(instance_count: usize, distances: &[f32]) -> Vec<usize> {
-        // Then, we can sort the indices based on those distances.
-        let mut indices = (0..instance_count).collect::<Vec<usize>>();
-        indices.sort_by(|a, b| {
-            distances[*b]
-                .partial_cmp(&distances[*a])
-                .unwrap_or(std::cmp::Ordering::Equal)
-        });
-        indices
     }
 
     ///
@@ -179,7 +110,13 @@ impl InstancedMesh {
                 .iter()
                 .map(|m| (self.transformation * m).w.truncate().distance2(position))
                 .collect::<Vec<_>>();
-            Self::ordered_indices_back_to_front(self.instance_count as usize, &distances)
+            let mut indices = (0..self.instance_count() as usize).collect::<Vec<usize>>();
+            indices.sort_by(|a, b| {
+                distances[*b]
+                    .partial_cmp(&distances[*a])
+                    .unwrap_or(std::cmp::Ordering::Equal)
+            });
+            indices
         } else {
             // No need to order, just return the indices as is.
             (0..self.instances.transformations.len()).collect::<Vec<usize>>()
@@ -291,7 +228,35 @@ impl Geometry for InstancedMesh {
             None
         };
 
-        self.update_instance_buffers(update_pose);
+        let needs_update = {
+            let s = self.instance_buffers.read().unwrap();
+
+            // Check if we need a reorder, this only applies to transparent materials.
+            let reorder_needed = if let Some(ref ordering_pose) = update_pose {
+                let camera_changed = *ordering_pose != s.1;
+                let instance_count_changed = if let Some(v) = s.0.values().next() {
+                    v.instance_count() != self.instance_count()
+                } else {
+                    false
+                };
+
+                camera_changed || instance_count_changed
+            } else {
+                false
+            };
+
+            // Update is always needed if the instance buffers is empty; Opaque materials only.
+            // Or, for transparent materials, if the camera moved or if the instance count changed.
+            s.0.is_empty() || reorder_needed
+        };
+
+        if needs_update {
+            let mut s = self.instance_buffers.write().unwrap();
+            s.0 = self.create_instance_buffers(update_pose);
+            if let Some(ordering_pose) = update_pose {
+                s.1 = ordering_pose;
+            }
+        }
 
         let instance_buffers = &self
             .instance_buffers
@@ -331,7 +296,7 @@ impl Geometry for InstancedMesh {
             render_states,
             camera,
             attributes,
-            self.instance_count,
+            self.instance_count(),
         );
     }
 
@@ -520,16 +485,5 @@ impl From<PointCloud> for Instances {
             colors: points.colors,
             ..Default::default()
         }
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    #[test]
-    fn test_ordered_indices_back_to_front() {
-        let distances = vec![10.0, 5.0, 5.1, 3.0, 1.0];
-        let res = InstancedMesh::ordered_indices_back_to_front(distances.len(), &distances);
-        assert_eq!(res, vec![0, 2, 1, 3, 4]);
     }
 }
