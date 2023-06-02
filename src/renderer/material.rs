e15b7ed656fcf09c97ad9f92e813bcbe89864086
Asger Nyman Christiansen <asgernyman@gmail.com>
2023-06-02 17:48:32 +0200

    Fix docs

--- src/renderer/material.rs
+++ src/renderer/material.rs
@@ -120,7 +120,7 @@ pub enum MaterialType {
 
 ///
 /// Describes the set of attributes provided by a [geometry] and consumed by a [Material], ie. calculated in the vertex shader and then sent to the fragment shader.
-/// To use an attribute for a material, add the relevant shader code to the fragment shader source (documented for each attribute) and return this struct from [Material::fragment_shader] with the relevant attribute set to true.
+/// To use an attribute for a material, add the relevant shader code to the fragment shader source (documented for each attribute) and return this struct from [Material::fragment_attributes] with the relevant attribute set to true.
 ///
 #[derive(Clone, Copy, Debug)]
 pub struct FragmentAttributes {
