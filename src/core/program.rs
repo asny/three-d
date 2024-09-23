e52e1da6f61c030fa44c7819c21862f5b1c86378
Asger Nyman Christiansen <asgernyman@gmail.com>
2024-09-23 17:02:13 +0200

    Return error instead of panic if program compilation failed for unknown reasons

--- src/core/program.rs
+++ src/core/program.rs
@@ -84,7 +84,7 @@ impl Program {
                 if !log.is_empty() {
                     Err(CoreError::ShaderLink(log))?;
                 }
-                unreachable!();
+                Err(CoreError::ShaderCompilerError)?;
             }
 
             context.detach_shader(id, vert_shader);
