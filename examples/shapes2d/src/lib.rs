7355d51243ef86ca68a1bba25f7d7bb6bf5dc708
Ivor Wanders <ivor@iwanders.net>
2025-11-03 20:31:12 -0500

    Add a textured rectangle to shapes2d.
    This shows how to draw textures with transparancy on the screen.

--- examples/shapes2d/src/lib.rs
+++ examples/shapes2d/src/lib.rs
@@ -7,13 +7,13 @@ use wasm_bindgen::prelude::*;
 
 #[cfg(target_arch = "wasm32")]
 #[wasm_bindgen(start)]
-pub fn start() -> Result<(), JsValue> {
+pub async fn start() -> Result<(), JsValue> {
     console_log::init_with_level(log::Level::Debug).unwrap();
 
     use log::info;
     info!("Logging works!");
 
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-    main::main();
+    main::run().await;
     Ok(())
 }
