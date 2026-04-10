632d75894f6c23dd34ed49e94e6225a1e8fa77e3
Asger Nyman Christiansen <asgernyman@gmail.com>
2026-04-10 14:07:12 +0200

    Use new wireframe object in wireframe example

--- examples/wireframe/src/main.rs
+++ examples/wireframe/src/main.rs
@@ -37,42 +37,18 @@ pub async fn run() {
     cpu_mesh
         .transform(Mat4::from_translation(vec3(0.0, 2.0, 0.0)))
         .unwrap();
-    let mut model_material = PhysicalMaterial::new_opaque(
+    let model_material = PhysicalMaterial::new_opaque(
         &context,
         &CpuMaterial {
-            albedo: Srgba::new_opaque(50, 50, 50),
+            albedo: Srgba::new_opaque(150, 150, 150),
             roughness: 0.7,
             metallic: 0.8,
             ..Default::default()
         },
     );
-    model_material.render_states.cull = Cull::Back;
     let model = Gm::new(Mesh::new(&context, &cpu_mesh), model_material);
-    let mut wireframe_material = PhysicalMaterial::new_opaque(
-        &context,
-        &CpuMaterial {
-            albedo: Srgba::new_opaque(220, 50, 50),
-            roughness: 0.7,
-            metallic: 0.8,
-            ..Default::default()
-        },
-    );
-    wireframe_material.render_states.cull = Cull::Back;
-    let mut cylinder = CpuMesh::cylinder(10);
-    cylinder
-        .transform(Mat4::from_nonuniform_scale(1.0, 0.007, 0.007))
-        .unwrap();
-    let edges = Gm::new(
-        InstancedMesh::new(&context, &edge_transformations(&cpu_mesh), &cylinder),
-        wireframe_material.clone(),
-    );
 
-    let mut sphere = CpuMesh::sphere(8);
-    sphere.transform(Mat4::from_scale(0.015)).unwrap();
-    let vertices = Gm::new(
-        InstancedMesh::new(&context, &vertex_transformations(&cpu_mesh), &sphere),
-        wireframe_material,
-    );
+    let wireframe = Wireframe::new(&context, &cpu_mesh, 1.0, Srgba::RED);
 
     let ambient = AmbientLight::new(&context, 0.7, Srgba::WHITE);
     let directional0 = DirectionalLight::new(&context, 2.0, Srgba::WHITE, vec3(-1.0, -1.0, -1.0));
@@ -90,7 +66,7 @@ pub async fn run() {
                 .clear(ClearState::color_and_depth(1.0, 1.0, 1.0, 1.0, 1.0))
                 .render(
                     &camera,
-                    model.into_iter().chain(&vertices).chain(&edges),
+                    model.into_iter().chain(&wireframe),
                     &[&ambient, &directional0, &directional1],
                 );
         }
@@ -100,51 +76,4 @@ pub async fn run() {
             ..Default::default()
         }
     });
-}
-
-fn vertex_transformations(cpu_mesh: &CpuMesh) -> Instances {
-    Instances {
-        transformations: cpu_mesh
-            .positions
-            .to_f32()
-            .into_iter()
-            .map(Mat4::from_translation)
-            .collect(),
-        ..Default::default()
-    }
-}
-
-fn edge_transformations(cpu_mesh: &CpuMesh) -> Instances {
-    let indices = cpu_mesh.indices.to_u32().unwrap();
-    let positions = cpu_mesh.positions.to_f32();
-    let mut transformations = Vec::new();
-    for f in 0..indices.len() / 3 {
-        let i1 = indices[3 * f] as usize;
-        let i2 = indices[3 * f + 1] as usize;
-        let i3 = indices[3 * f + 2] as usize;
-
-        if i1 < i2 {
-            transformations.push(edge_transform(positions[i1], positions[i2]));
-        }
-        if i2 < i3 {
-            transformations.push(edge_transform(positions[i2], positions[i3]));
-        }
-        if i3 < i1 {
-            transformations.push(edge_transform(positions[i3], positions[i1]));
-        }
-    }
-    Instances {
-        transformations,
-        ..Default::default()
-    }
-}
-
-fn edge_transform(p1: Vec3, p2: Vec3) -> Mat4 {
-    Mat4::from_translation(p1)
-        * Into::<Mat4>::into(Quat::from_arc(
-            vec3(1.0, 0.0, 0.0),
-            (p2 - p1).normalize(),
-            None,
-        ))
-        * Mat4::from_nonuniform_scale((p1 - p2).magnitude(), 1.0, 1.0)
 }
